005faf85b975223bf946978aff92db2d939e62eb
Begona Fernandez <boadicea1979@yahoo.es>
2018-12-03 22:26:59 +0000

    added switch loop to decide what operator to use

--- begonafernandez/calculator/app.js
+++ begonafernandez/calculator/app.js
@@ -1,43 +1,40 @@
 var userImput
-var operation
+var operator
 var firstNumber
 var secondNumber
+var result
 
 //What operation does the user want to do?
-while(!operation) {
+while(!operator) {
   userImput = prompt('What operation do you wish? Sum, Substraction, Division or Multiplication')
-  switch(userImput.toLowerCase()) {
-    case 'sum':
-      operation = '+'
-      break
-    case 'substraction':
-      operation = '-'
-      break
-    case 'division':
-      operation = '/'
-      break
-    case 'Multiplication':
-      operation = '*'
-      break
-  }
-  console.log(operation)
+  operator = userImput.toLowerCase()
 }
 
 // what is the first number?
 while(isNaN(firstNumber)) {
   userImput = prompt('What is the first number?')
   firstNumber = parseFloat(userImput)
-  console.log(firstNumber)
 }
 // what is the second number?
 while(isNaN(secondNumber)) {
   userImput = prompt('What is the second number?')
   secondNumber = parseFloat(userImput)
-  console.log(secondNumber)
 }
 
-// Add the three inputs together and show box with the answer and option to do another operation
-
-// if yes start again
-
-// if not close
+// Add the three inputs together and show box with the answer
+switch(operator.toLowerCase()) {
+  case 'sum':
+    result = firstNumber + secondNumber
+    console.log(result)
+    break
+  case 'substraction':
+    result = firstNumber - secondNumber
+    break
+  case 'division':
+    result = firstNumber / secondNumber
+    break
+  case 'multiplication':
+    result = firstNumber * secondNumber
+    break
+}
+confirm(result)
