2bdafc88de82b895a3a112de2a26348336b5cf08
laceswingybethler <beth.swingler@gmail.com>
2018-12-03 21:52:26 +0000

    Added the if null condition to break out of loop.

--- laceswingybethler/jsES6-calculator/app.js
+++ laceswingybethler/jsES6-calculator/app.js
@@ -3,82 +3,47 @@ console.log('working')
 //PSEUDO METHOD 1
 //ask user for input 1 - var desiredCalc (m d a or s)
 var solution
+
 var calculating = true
 while(calculating)  {
   var desiredCalc = prompt('What calculation are you after, pal? (m)ultiply, (d)ivide, (s)ubtract or (a)dd?')
   //if none of the options, alert and loop back to start
+  if (desiredCalc === null) {
+    break
+  }
   if (desiredCalc !=='m' && desiredCalc !=='d' && desiredCalc!=='a' && desiredCalc!=='s') {
     alert('Lo siento, me no entiendo. Try again.')
-    calculating=false
+
+  } else {
+
+    //while validCalc = false {ask again and then update validCalc}
+
+    //ask user for input 2 - firstNum
+    var firstNum = prompt('Using what number to start?')
+    //ask user for input 3 - secondNum
+    var secondNum = prompt('...with what number?')
+    //if desiredCalc === 'm' then solution = (firstNum*secondNum)
+    if (desiredCalc === 'm') {
+      solution=(firstNum*secondNum)
+
+    //else if desiredCalc === 'd' then solution = (firstNum/secondNum)
+    } else if (desiredCalc === 'd'){
+      solution=(firstNum/secondNum)
+
+    // else if desiredCalc === 'p' then solution = (firstNum+secondNum)
+    } else if (desiredCalc === 'p') {
+      solution=(firstNum+secondNum)
+
+    //else if desiredCalc === 's' then solution = (firstNum-secondNum)
+    } else if (desiredCalc === 's') {
+      solution=(firstNum-secondNum)
+    }
+    //display 'The answer is ' + solution and ask the user if they want to do another calculation (loop)
+    // console.log('The answer is ' + solution)
+    calculating = confirm('The answer is ' + solution + '. Play again?')
+    //   }
   }
-  // else {
-
-  //ask user for input 2 - firstNum
-  var firstNum = prompt('Using what number to start?')
-  //ask user for input 3 - secondNum
-  var secondNum = prompt('...with what number?')
-  //if desiredCalc === 'm' then solution = (firstNum*secondNum)
-  if (desiredCalc === 'm') {
-    solution=(firstNum*secondNum)
-
-  //else if desiredCalc === 'd' then solution = (firstNum/secondNum)
-  } else if (desiredCalc === 'd'){
-    solution=(firstNum/secondNum)
-
-  // else if desiredCalc === 'p' then solution = (firstNum+secondNum)
-  } else if (desiredCalc === 'p') {
-    solution=(firstNum+secondNum)
-
-  //else if desiredCalc === 's' then solution = (firstNum-secondNum)
-  } else if (desiredCalc === 's') {
-    solution=(firstNum-secondNum)
-  }
-  //display 'The answer is ' + solution and ask the user if they want to do another calculation (loop)
-  // console.log('The answer is ' + solution)
-  calculating = confirm('The answer is ' + solution + '. Play again?')
-  //   }
 }
-
-
-
-// helper code
-// var playing = true
-// while(playing)  {
-//   var randomNum = Math.ceil(Nath,random() * 10)
-//   var userInput
-//   //inner loop - AT FIRST THE VAR USERINPUT JUST HAS NO VALUE
-//     while {userInput !== randomNum {
-//       userInput = parseInt(prompt'Enter a number 1-10')
-//     }
-//     //end of inner loop
-//     playing = confirm('Well done. Play again?')
-//   }
-//   console.log ('thanks for playing')
-// }var playing = true
-// while(playing)  {
-//   var randomNum = Math.ceil(Nath,random() * 10)
-//   var userInput
-//   //inner loop - AT FIRST THE VAR USERINPUT JUST HAS NO VALUE
-//     while {userInput !== randomNum {
-//       userInput = parseInt(prompt'Enter a number 1-10')
-//     }
-//     //end of inner loop
-//     playing = confirm('Well done. Play again?')
-//   }
-//   console.log ('thanks for playing')
-// }
-
-
-// var answer = prompt('What is 5 multiplied by 2?')
-//
-// if (answer === 10) {
-//   alert('Wow! Correct!')
-// } else {
-//   alert('Wrong!')
-// }
-
-
-
 
 
 
