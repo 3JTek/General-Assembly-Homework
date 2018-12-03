fd2d201cbc2e233da291f30500e2c54ab5c77a7a
Gessy90 <gessicasantoro90@hotmail.it>
2018-12-03 19:15:45 +0000

    final final

--- Gessy90/js-calculator/calc.js
+++ Gessy90/js-calculator/calc.js
@@ -1,7 +1,7 @@
 alert('Here is G Calculator')
 
 while(true){
-  var type = prompt('Type adding(a), multiplying(m), subtracting(s), divide (d)')
+  var type = prompt('Type adding(a), multiplying(m), subtracting(s), divide (d),')
   if(type === 'a'){
     var firstNumber = parseFloat(prompt('what is your first number?'))
     var secondNumber = parseFloat(prompt('what is your second number?'))
@@ -9,16 +9,16 @@ while(true){
   } else if (type === 'm'){
     var firstMul = parseFloat(prompt('what is your first number?'))
     var secondMul = parseFloat(prompt('what is your second number?'))
-    alert(firstMul+secondMul)
+    alert(firstMul*secondMul)
   } else if(type === 's'){
     var firstSub = parseFloat(prompt('what is your first number?'))
     var secondSub = parseFloat(prompt('what is your second number?'))
-    alert(firstSub+secondSub)
+    alert(firstSub-secondSub)
   } else if (type === 'd'){
     var firstDiv = parseFloat(prompt('what is your first number?'))
     var secondDiv = parseFloat(prompt('what is your second number?'))
   }
-  alert(firstDiv+secondDiv)
+  alert(firstDiv/secondDiv)
   if(confirm('Play again?') === false){
     break
   }
