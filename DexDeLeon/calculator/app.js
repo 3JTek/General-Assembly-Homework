aab5c4537243d2c9db66f7c9f42e3f6abf0ac943
Dexter De Leon <dexdeleon@gmail.com>
2018-12-03 22:11:44 +0000

    Corrected mortgage calculator to take yearly interest rate in percentage and convert for calculation.

--- DexDeLeon/calculator/app.js
+++ DexDeLeon/calculator/app.js
@@ -6,7 +6,6 @@ while(calc){
   var num1 = NaN
   var num2 = NaN
   var num3 = NaN
-  var num4 = NaN
   var result = 0
 
   // prompt the user to select basic or advanced calculator
@@ -27,7 +26,7 @@ while(calc){
     while (isNaN(num2)){
       num2 = prompt('Enter the second number')
     }
-    // given the operator selected, calculate the appropriate result
+    // given the operator selected, calculate the appropriate result and store
     result = eval(num1 + op + num2)
 
   }else {
@@ -36,33 +35,44 @@ while(calc){
       op = prompt('Select an operation: (p)ower, (s)quare-root or (m)ortgage')
     }
     switch(op){
+      // if power is selected
       case 'p':
+        // prompt the user for base and exponent numbers
         while(isNaN(num1)){
           num1 = parseFloat(prompt('Please enter the base number'))
         }
         while(isNaN(num2)){
           num2 = parseFloat(prompt('Please enter the exponent'))
         }
+        // calculate the power and store in result
         result = Math.pow(num1, num2)
         break
 
+      // if square root is selected
       case 's':
+        // prompt the user for the number to be calculated
         while(isNaN(num1)){
           num1 = parseFloat(prompt('Please enter a number (radicant)'))
         }
+        // store the square root in result
         result = Math.sqrt(num1)
         break
 
+      // if mortgage is selected
       case 'm':
         while(isNaN(num1)){
+          // prompt user for principal loan
           num1 = parseFloat(prompt('Please enter the principal of the loan'))
         }
         while(isNaN(num2)){
-          num2 = parseFloat(prompt('Please enter the interest rate'))
+          // prompt user for yearly interest rate in % and divide by 1200 for the monthly rate as a decimal
+          num2 = parseFloat(prompt('Please enter the yearly interest rate in %')/1200)
         }
         while(isNaN(num3)){
+          // prompt user for number of payments
           num3 = parseFloat(prompt('Please enter the number of payments'))
         }
+        // calculate monthly payment and store in result
         result = num1*((num2*Math.pow(1+num2, num3))/(Math.pow(1+num2, num3)-1))
         break
     }
