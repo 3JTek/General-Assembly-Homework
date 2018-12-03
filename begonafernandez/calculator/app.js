5e3918b69f7e43ee1acb2de528738b77d86ee6b1
Begona Fernandez <boadicea1979@yahoo.es>
2018-12-03 22:56:25 +0000

    added option for basic, advanced and invalid type of calculator

--- begonafernandez/calculator/app.js
+++ begonafernandez/calculator/app.js
@@ -1,49 +1,52 @@
 
-// var typeOfCalculator
+var typeOfCalculator
 var operator
 var firstNumber
 var secondNumber
 var result
 
 // Does use want basic or advanced calculator
-// userImput = prompt('Do you want the advanced calculator(a) or the basic calculator(b)')
-// console.log(typeOfCalculator)
-// // if basic
-// if()
+typeOfCalculator = prompt('Do you want the advanced calculator(a) or the basic calculator(b)')
+console.log(typeOfCalculator)
+// if basic
+if(typeOfCalculator === 'a') {
+  //What operation does the user want to do?
+  while(!operator) {
+    operator = prompt('What operation do you wish? Sum, Substraction, Division or Multiplication').toLowerCase()
+  }
 
-// if advanced
+  // what is the first number?
+  while(isNaN(firstNumber)) {
+    firstNumber = parseFloat(prompt('What is the first number?'))
+    // firstNumber = parseFloat(userImput)
+  }
+  // what is the second number?
+  while(isNaN(secondNumber)) {
+    secondNumber = parseFloat(prompt('What is the second number?'))
+  }
 
-//What operation does the user want to do?
-while(!operator) {
-  operator = prompt('What operation do you wish? Sum, Substraction, Division or Multiplication').toLowerCase()
+  // Add the three inputs together and show box with the answer
+  switch(operator.toLowerCase()) {
+    case 'sum':
+      result = firstNumber + secondNumber
+      console.log(result)
+      break
+    case 'substraction':
+      result = firstNumber - secondNumber
+      break
+    case 'division':
+      result = firstNumber / secondNumber
+      break
+    case 'multiplication':
+      result = firstNumber * secondNumber
+      break
+    default:
+      result = 'not a valid math operation'
+  }
+  confirm(result)
+} else if (typeOfCalculator === 'b') {
+  // if advanced
+  confirm('advanced')
+} else {
+  confirm('not valid type of calculator')
 }
-
-// what is the first number?
-while(isNaN(firstNumber)) {
-  firstNumber = parseFloat(prompt('What is the first number?'))
-  // firstNumber = parseFloat(userImput)
-}
-// what is the second number?
-while(isNaN(secondNumber)) {
-  secondNumber = parseFloat(prompt('What is the second number?'))
-}
-
-// Add the three inputs together and show box with the answer
-switch(operator.toLowerCase()) {
-  case 'sum':
-    result = firstNumber + secondNumber
-    console.log(result)
-    break
-  case 'substraction':
-    result = firstNumber - secondNumber
-    break
-  case 'division':
-    result = firstNumber / secondNumber
-    break
-  case 'multiplication':
-    result = firstNumber * secondNumber
-    break
-  default:
-    result = 'not a valid math operation'
-}
-confirm(result)
