ef257a6e30a533412a53b2687849e90a56128971
SirAbbott <tom@iynk.com>
2018-12-03 18:39:21 +0000

    refactored

--- SirAbbott/js-calculator/app.js
+++ SirAbbott/js-calculator/app.js
@@ -1,35 +1,32 @@
 // Create a calculator that takes input from the browser. The calculator should display the results using alert.
 
-// make a loop
-var calc
+var calculating = true
 var num1
 var num2
-while (calc !== 0) {
-  // make a prompt where the user can decide what operator they want to use to calculate -> store the answer
-  var typeOfCalc = prompt('Would you like a (b)asic or (a)dvanced calculator')
-  if (typeOfCalc === 'b') {
-    calc = prompt('Would you like to (a)dd, (s)ubtract, (m)ultiply or (d)ivide')
-    num1 = parseInt(prompt('What is your first number'))
-    num2 = parseInt(prompt('Enter the second number'))
-    // Second prompt once the first is selected
-    //Use stored value to alert an answer
-    if (calc === 'a') {
-      alert('The sum is ' + (num1 + num2))
-    } else if (calc === 's') {
-      alert('The diferrence is ' + (num1 - num2))
-    } else if (calc === 'm') {
-      alert('The diferrence is ' + (num1 * num2))
-    } else if (calc === 'd') {
-      alert('The diferrence is ' + (num1 / num2))
-    }
-  } else if (typeOfCalc === 'a') {
-    calc = prompt('Would you like (p)ower or (s)quare root ')
+var operator
+
+while (calculating) {
+
+  var calcType = prompt('(a)dvanced or (b)asic')
+
+  if (calcType === 'b') {
+
+    operator = prompt('What would you like to do? +, -, *, / ')
+    var firstNumber = parseInt(prompt('Enter the first number you would like to use'))
+    var secondNumber = parseInt(prompt('Enter the second number you would like to use'))
+    var answer = eval((firstNumber + operator + secondNumber))
+
+    alert('Your answer is ' + answer)
+  } else if (calcType === 'a') {
+
+    operator = prompt('(s)quare root or (p)ower')
     num1 = parseInt(prompt('What is your first number'))
   }
-  if (calc === 'p') {
-    num2 = parseInt(prompt('Enter the second number'))
-    alert('The sum is ' + Math.pow(num1, num2))
-  } else if (calc === 's') {
-    alert('The sum is ' + Math.sqrt(num1))
+  if (operator === 's') {
+    alert(Math.sqrt(num1))
+  } else if (operator === 'p') {
+    num2 = parseInt(prompt('What is your second number'))
   }
+  alert(Math.pow(num1, num2))
+
 }
