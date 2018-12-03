6ef51cf1fc405bc5493d759f44ca477ae5e7ee51
Alessandro Lepri <alex.lepri84@gmail.com>
2018-12-03 21:06:05 +0000

    Changed all syntax due to line error

--- alexlepri/js-calculator/app.js
+++ alexlepri/js-calculator/app.js
@@ -1,23 +1,25 @@
-alert("Here is your Calculator")
+alert('Here is Greg, your Calculator')
 
-while(true) {
-  var type = prompt('Type adding(a), multiply(m), subtracting(s), divide(d)')
-  if(type === 'a')
-  var firstNumber = parse(prompt('What is your first number?'))
-  var seconsNumber = parseFloat(prompt('What is your second number'))
-  alert(firstNumber+secondNumber)
-} else if (type === 's') {
-  var divNumber1 = parseFloat(prompt('What is your first number?'))
-  var divNumber2 = parseFloat(prompt('What is your second number?'))
-  alert('divNumber1 / divNumber2')
-} else if (type === 'm') {
-  var multiNumber = parseFloat (prompt('What is your first number?'))
-  alert('multiNumber * multiNumber2')
-} else if (type === 's') {
-  var subNum = parseFloat (prompt('What is your first number?'))
-  var subNum2 = parseFloat (prompt('What is your second number?'))
-  alert(divNum - divNum2)
-  if(confirm('Play again?')) === false {
+while(true){
+  var type = prompt('Type adding(a), multiplying(m), subtracting(s), divide(d)')
+  if(type === 'a'){
+    var addFirst = parseFloat(prompt('What is your first number?'))
+    var addSecond = parseFloat(prompt('What is your second number?'))
+    alert(addFirst + addSecond)
+  } else if (type === 'm'){
+    var mulFirst = parseFloat(prompt('What is your first number?'))
+    var mulSecond = parseFloat(prompt('What is your second number?'))
+    alert(mulFirst * mulSecond)
+  } else if(type === 's'){
+    var subFirst = parseFloat(prompt('What is your first number?'))
+    var subSecond = parseFloat(prompt('What is your second number?'))
+    alert(subFirst - subSecond)
+  } else if (type === 'd'){
+    var divFirst = parseFloat(prompt('What is your first number?'))
+    var divSecond = parseFloat(prompt('What is your second number?'))
+  }
+  alert(divFirst / divSecond)
+  if(confirm('Math again?') === false){
     break
   }
 }
