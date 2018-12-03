b4e8dd5cfdcc32af3bfc573761b8c716501a5f09
Alessandro Lepri <alex.lepri84@gmail.com>
2018-12-03 19:37:01 +0000

    Added first numbers syntax

--- alexlepri/js-calculator/app.js
+++ alexlepri/js-calculator/app.js
@@ -2,4 +2,8 @@ alert("Here is your Calculator")
 
 while(true) {
   var type = prompt('Type adding(a), multiply(m), subtracting(s), divide(d)')
+  if(type === 'a')
+  var firstNumber = parse(prompt('What is your number?'))
+  var seconsNumber = parseFloat(prompt('What is your second number'))
+  alert(firstNumber+secondNumber)
 }
