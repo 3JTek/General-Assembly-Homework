568f85485962b49c3d951f04bd808efb647fb5c2
James Benson <you@example.com>
2018-12-03 16:49:13 +0000

    Pseudo-code

---
+++ jjbenson/js-calculator/app.js
@@ -0,0 +1,5 @@
+//User Chooses ADD, SUBTRACT, MULTIPLY, DIVIDE
+//User Enters FIRST Number
+//User Enters Second Number
+//Calculate Result
+//User is shown the result and asked if they want to start again
