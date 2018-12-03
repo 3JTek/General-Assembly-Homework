6a92870212987f6bc407fda2204d15795f4850ae
Michael Simmonds <michaelsimmonds92@hotmail.com>
2018-12-03 23:53:30 +0000

    Added error message and additional prompt if number input is not valid

--- michaelsimmonds/js-calculator/app.js
+++ michaelsimmonds/js-calculator/app.js
@@ -1,41 +1,51 @@
 console.log('working')
 
 // prompt user to enter a number
+// if not number, prompt user to input valid number
 // prompt user to enter an action indicator of plus, minus, multiply or divide
+// if not valid action indicator, promt user to input valid action indictor
 // prompt user to enter a second number
+// if not number, prompt user to input valid number
 // generate an alert displaying value of calculation
 
 var action
+var firstValue
+var secondValue
 
-var firstValue = parseFloat(prompt('Please enter an initial number'))
-console.log(firstValue)
+while (true) {
+  firstValue = parseFloat(prompt('Please enter an initial number'))
+  console.log(firstValue)
+  if (isNaN(firstValue) != true) {
+    break
+  } else {
+    alert('Please enter a valid number')
+  }
+}
 
-while(true) {
+while (true) {
   action = prompt('Please enter a modifier of +, *, - or /')
   console.log(action)
   if (action === '+' || action === '*' || action === '-' || action === '/') {
     break
-  } else{
-    alert("Please enter a valid function")
+  } else {
+    alert('Please enter a valid function')
   }
 }
 
-
-
-
-// while (action !== '+' || action !== '*' || action !== '-' || action !== '/') {
-//   alert('Input not valid')
-//   break
-// }
-
-var secondValue = parseFloat(prompt('Please enter a second number'))
-console.log(secondValue)
-
+while (true) {
+  secondValue = parseFloat(prompt('Please enter a second number'))
+  console.log(firstValue)
+  if (isNaN(secondValue) != true) {
+    break
+  } else {
+    alert('Please enter a valid number')
+  }
+}
 
 switch (action) {
-  case "+": alert(firstValue + secondValue)
-  case "-": alert(firstValue - secondValue)
-  case "*": alert(firstValue * secondValue)
-  case "/": alert(firstValue / secondValue)
+  case '+': alert(firstValue + secondValue)
+  case '-': alert(firstValue - secondValue)
+  case '*': alert(firstValue * secondValue)
+  case '/': alert(firstValue / secondValue)
   default: alert('Not a correct operation')
 }
