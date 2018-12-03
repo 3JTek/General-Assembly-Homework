a3dc748a345513b9948e0964b5a7d43ef108f932
Nawal00 <nawalgurung80@gmail.com>
2018-12-03 22:27:27 +0000

    added square root & pow logic

--- nawalgurung/js-calculator/app.js
+++ nawalgurung/js-calculator/app.js
@@ -14,20 +14,33 @@ Create a calculator that takes input from the browser. The calculator should dis
 var calcAgain = true
 
 while(calcAgain){
-  var input = prompt('what would you like to do(m)Multiply, (d)Divide, (a)Addition, (s)Subtraction?')
-  var num1 = parseInt(prompt('Please enter the first number'))
-  var num2 = parseInt(prompt('Please enter the first number'))
 
-  if(input==='m'){
+  var input = prompt('what would you like to do (m)Multiply, (d)Divide, (a)Addition, (s)Subtraction, (sqr)Square Root, (pow)Power?')
+
+  //check if input is pow & prompt for baseNum/expoNum
+  if(input==='pow'){
+    var baseNum = parseInt(prompt('Please enter the base number'))
+    var expoNum = parseInt(prompt('Please enter the exponent number'))
+    alert(Math.pow(baseNum, expoNum))
+  //check if input is not sqr & pow and prompt for first num & second num
+  }if(input !== 'sqr' && input !=='pow'){
+    var num1 = parseInt(prompt('Please enter the first number'))
+    var num2 = parseInt(prompt('Please enter the second number'))
+  //if input = sqr then prompt for only 1 num
+  }if(input==='sqr'){
+    var squareNum = parseInt(prompt('Please enter the number you want to square'))
+    alert(Math.sqrt(squareNum))
+  } else if(input==='m'){
     alert(num1 * num2)
-  }else if(input==='d'){
+  } else if(input==='d'){
     alert(num1 / num2)
-  }else if(input==='a'){
+  } else if(input==='a'){
     alert(num1 + num2)
-  }else if(input==='s'){
+  } else if(input==='s'){
     alert(num1 - num2)
-  } else{
-    alert('please choose m, d, a or s')
   }
+  //  else{
+  //   alert('please choose m, d, a, s, sqr or pow')
+  // }
   calcAgain = confirm('do you want to calcuate again?')
 }
