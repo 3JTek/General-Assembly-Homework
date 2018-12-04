5245e187b8527c5984bb9e1ac26d286ffa42ed6d
joshuaking06 <joshua.king0692@gmail.com>
2018-12-04 15:53:23 +0000

    first problem done

--- jjking1/js-problems-101/problems.js
+++ jjking1/js-problems-101/problems.js
@@ -2,8 +2,8 @@
 
 // write a function that returns "Hello World!" if no argument is given, or "Hello <argument>!" otherwise
 // eg: hello() => "Hello World!"; hello("Mike") => "Hello Mike!"
-function hello(string) {
-
+function hello(string='World') {
+  return (`Hello ${string}!`)
 }
 
 // write a function that will calculate the area of a circle, given the radius
