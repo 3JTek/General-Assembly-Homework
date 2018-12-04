02084a792e3f60b443cb84ecb4253aa446e29463
laceswingybethler <beth.swingler@gmail.com>
2018-12-04 21:37:07 +0000

    Complete up to numOfOccurences.

--- laceswingybethler/js-problems-101/problems.js
+++ laceswingybethler/js-problems-101/problems.js
@@ -35,19 +35,42 @@ function palindromeCheck(string) {
 
 // write a function that returns the letters of a word or phrase in alphabetical order case insensitive
 // eg. orderStringAlphabetically('javascript is cool') => 'aacciijlooprsstv'
-function orderStringAlphabetically(string) {
 
+function orderStringAlphabetically(string) {
+  return string.toLowerCase().split(' ').join('').split('').sort().join('')
 }
+console.log(orderStringAlphabetically('Javascript is cool'))
 
 // write a function to return the number of occurances of each letter of a string in an object case insensitive
 // eg. numOfOccurances('This is great') => { t: 2, h: 1, i: 2, s: 2, g: 1, r: 1, e: 1, a: 1 }
-// convert string into an Array
-// order
+// convert string into an Array? An object?
+
 // count?
+// convert to an object?
+//for every character in the object [i] count?
+
+//.includes for the Array
+// Objects.keys(nameOfObject)  <-- consults the object and checks if there. then can loop through this.
+
 function numOfOccurances(string) {
-  return
+  objectForCount = {}
+  // make string into an Array
+  const myStringArray = string.toLowerCase().split(' ').join('').split('')
+  //for each item in the array, create a property
+  for (var i = 0; i < myStringArray.length; i++) {
+    const letter = myStringArray[i]
+    // if the item is not new increase the value by 1
+    if (letter in objectForCount) {
+      objectForCount[letter] +=1
+      //if item is new, set value of the property to 1
+    }else {
+      objectForCount[letter] =1
+    }
+  }
+  //return the answer
+  return objectForCount
 }
-console.log(numOfOccurances('hhaatty'))
+console.log(numOfOccurances('This is great'))
 
 // write a function that capitalizes the first letter of each word
 // eg. titleCase('the lord of the rings') => 'The Lord Of The Rings'
