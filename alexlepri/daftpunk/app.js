1a83e636b99caa548e186ecb56a9c9086f14f963
Alessandro Lepri <alex.lepri84@gmail.com>
2018-12-06 18:57:16 +0000

    Added JS

--- alexlepri/daftpunk/app.js
+++ alexlepri/daftpunk/app.js
@@ -1,14 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const audio = document.querySelector('audio')
-  const btn = document.querySelector('button')
+  const audio = document.querySelectorAll('audio')
+  const buttons = document.querySelectorAll('button')
+  console.log(audio, buttons)
 
-  btn.addEventListener('click', () => {
+  function playAudioOnClick(audio){
     if(audio.paused) {
       audio.play()
     } else {
       audio.pause()
       audio.currentTime = 0
     }
+  }
+
+  buttons.forEach(function(button, index){
+    console.log(index)
+    console.log(audio[index])
+    button.addEventListener('click', () => {
+      playAudioOnClick(audio[index])
+    })
   })
 
 })
