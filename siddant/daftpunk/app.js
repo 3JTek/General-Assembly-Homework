e03259f497f8c6910e1b4c1094aa1e1fe321328e
Siddant <siddantgurung@hotmail.co.uk>
2018-12-06 22:24:10 +0000

    completed the task

--- siddant/daftpunk/app.js
+++ siddant/daftpunk/app.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const musicButton = document.querySelectorAll('.btn')
   const sound = document.querySelector('#sound')
+  const rand = document.querySelector('#random')
   let previousValue =''
 
   musicButton.forEach(musicButton => {
@@ -15,6 +16,8 @@ document.addEventListener('DOMContentLoaded', () => {
       previousValue = e.target.textContent
       e.target.textContent = ''
       e.target.style.backgroundImage = 'url(\'gif/'+e.target.value+'.gif\')'
+      e.target.style.backgroundRepeat= 'no-repeat'
+
       e.target.style.backgroundSize = 'contain'
     })
 
@@ -25,7 +28,12 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(previousValue)
       e.target.textContent =  previousValue
     })
-
   })
+
+  rand.addEventListener('click', () => {
+    sound.setAttribute('src','sounds/'+musicButton[Math.floor(Math.random() * 16)].value+'.wav')
+    sound.play()
+  }
+  )
 
 })
