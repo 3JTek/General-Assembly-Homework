000f38670a7b9856ef9cede7a423d14c87263a63
Dexter De Leon <dexdeleon@gmail.com>
2018-12-06 10:01:05 +0000

    Homework solutions added.

--- DexDeLeon/js-problems-102/problems.js
+++ DexDeLeon/js-problems-102/problems.js
@@ -79,13 +79,13 @@ function fibonacci(n) {
   const fibStart = [0, 1]
 
   // if n is less than 2, return appropriate slice of the starting array
-  if (n < 2) return fibStart.slice(0, n)
+  if (n <= 2) return fibStart.slice(0, n)
   // otherwise, starting at the 3rd term in the sequence, until the nth term in the sequence (index[2] until index[n-1])...
   else for(let i=2;i<n;i++){
     // ...push a new number which is the sum of the previous 2 in sequence
     fibStart.push(fibStart[i-1] + fibStart[i-2])
   }
-
+  
   // return the completed array
   return fibStart
 }
@@ -94,7 +94,7 @@ function fibonacci(n) {
 // it should not return negative numbers
 // eg: daysBetween("2017-01-01", "2017-02-01") => 31; daysBetween("2017-02-01", "2017-01-01") => 31
 function daysBetween(date1, date2) {
-  
+
 }
 
 // write a function which returns the number of seconds between two times (in the same day)
