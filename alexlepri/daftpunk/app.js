200cd57e461824f77221b42aa63d1e148b19593b
Alessandro Lepri <alex.lepri84@gmail.com>
2018-12-06 22:30:31 +0000

    Review style

--- alexlepri/daftpunk/app.js
+++ alexlepri/daftpunk/app.js
@@ -3,7 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const buttons = document.querySelectorAll('button')
   console.log(audio, buttons)
 
-  function playAudioOnClick(audio){
+  const styleButton = (e) => {
+    e.target.style.fontWeight = 'bold'
+    e.target.style.color = 'white'
+    e.target.style.backgroundColor = 'red'
+  }
+
+
+  function play(audio){
     if(audio.paused) {
       audio.play()
     } else {
@@ -16,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log(index)
     console.log(audio[index])
     button.addEventListener('click', () => {
-      playAudioOnClick(audio[index])
+      play(audio[index])
     })
   })
 
