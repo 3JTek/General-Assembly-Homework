ffd04cc6253a89bd63a7cbbfa2eb5c0264070dc9
joshuaking06 <joshua.king0692@gmail.com>
2018-12-06 18:27:46 +0000

    added random button

--- jjking1/daftpunk/app.js
+++ jjking1/daftpunk/app.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   const audio = document.querySelector('audio')
-  const buttons = document.querySelectorAll('button')
-
+  const buttons = document.querySelectorAll('.btn')
+  const randomBtn = document.querySelector('#randomBtn')
   buttons.forEach((btn) => {
     btn.addEventListener('click', (e) => {
       audio.setAttribute('src', './sounds/'+e.target.value+'.wav')
@@ -9,7 +9,15 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   })
 
+  const randomElement = (array => {
+    return (array[Math.floor(Math.random()*array.length-1)]).value
+  })
 
+  console.log(randomElement(buttons))
 
+  randomBtn.addEventListener('click', (e) => {
+    audio.setAttribute('src', './sounds/'+randomElement(buttons)+'.wav')
+    audio.play()
+  })
 
 })
