e277ee62196a86c7df5e34b2a7652acca11d6fa9
Begona Fernandez <boadicea1979@yahoo.es>
2018-12-06 22:22:17 +0000

    removed extra image

--- begonafernandez/daftpunk/app.js
+++ begonafernandez/daftpunk/app.js
@@ -2,11 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
   const audios = document.querySelectorAll('audio')
   const buttons = document.querySelectorAll('button')
 
-  const Img = document.createElement('img')
-  Img.setAttribute('src', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu5UfoUD86vrvcm5btkfbEzw9UgnOQzn3yOouCg7a4aVhvS1MP')
-  Img.setAttribute('alt', 'na')
-  document.body.appendChild(Img)
-
   for(let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
       audios[i].play()
