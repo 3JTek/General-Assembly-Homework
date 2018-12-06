0a87e1b92209948a77240294e6edca3b0d75789c
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2018-12-06 21:05:36 +0000

    added comments for my own understanding

--- charlottemorgan/daftpunk/app.js
+++ charlottemorgan/daftpunk/app.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('js loaded')
-  //find the squares
+  //find the squares that in the grid
   const buttons = document.querySelectorAll('button')
-  // play the sound with the corresponding id
+  // play the sound with the corresponding id.
+  // This function finds the audio tag in the html and then gives the source to the button using its id. First we are creating a variable song to find the tag in the html. Then we are telling it the source using the id which matches the song name in the html. Then we are telling it to play the song
   function music (e) {
     const song = document.querySelector('audio')
     song.src = './sounds/' + e.target.id + '.wav'
     song.play()
   }
-  // loop through all of the buttons
-  // add an eventListener to each button
+  // loop through all of the buttons that we have created. We create a variable i which is equal to zero. while this variables index is less than the number of buttons(length),which is 16, then add one and run it again.
   for (let i = 0; i < buttons.length; i++){
-    // When you click on a button, run a function
+    // add an eventListener to each button
+    // When you click on the button, run the function music
     buttons[i].addEventListener('click', music)
-    // find the id of the element that was clicked on
   }
 
 })
