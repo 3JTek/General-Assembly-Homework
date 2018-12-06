5607a4fb5e0a4ba5bf97c9273b9b85cbf7eaaa9b
edwardlea <leaedward87@gmail.com>
2018-12-06 18:55:19 +0000

    Audio link added to div

---
+++ edwardlea/daftpunk/app.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const object = {
+    'After': 'sounds/after.wav',
+    'Better': 'sounds/better.wav',
+    'Do It': 'sounds/do_it.wav',
+    'Ever': 'sounds/ever.wav',
+    Faster: 'sounds/faster.wav',
+    Harder: 'sounds/harder.wav',
+    Hour: 'sounds/hour.wav',
+    'Make It': 'sounds/make_it.wav',
+    'Makes Us': 'sounds/makes_us.wav',
+    'More Than': 'sounds/more_than.wav',
+    Never: 'sounds/never.wav',
+    Our: 'sounds/our.wav',
+    Over: 'sounds/over.wav',
+    Stronger: 'sounds/stronger.wav',
+    'Work Is': 'sounds/work_is.wav',
+    'Work It': 'sounds/work_it.wav'
+
+  }
+
+
+
+  const audio = document.querySelector('audio')
+  const sounder = document.querySelectorAll('.column')
+
+
+  sounder.forEach(button => button.addEventListener('click', (e) => {
+    // const track = e.target.outerText
+    // console.log(object[track])
+    audio.setAttribute('src', object[e.target.outerText])
+    audio.play()
+    // audioFile.play()
+    // if(audio[e.childElementCount].paused()) {
+    //   audio[e.childElementCount].play()
+    // } else {
+    //   audio[e.childElementCount].pause()
+    //   audio[e.childElementCount].currentTime = 0
+    // }
+  }))
+
+
+
+
+
+})
