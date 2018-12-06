6af7a824d874b4c96b69ea202ec2d99092919906
Ben Lander <ben.l.lander@googlemail.com>
2018-12-06 08:54:05 +0000

    Amended Fibonacci

--- BenLeo/js-problems-102/js-problems-102/problems.js
+++ BenLeo/js-problems-102/js-problems-102/problems.js
@@ -85,10 +85,10 @@ function fibonacci(n) {
   // }
 
   const sequence = [0,1]
-  for (let i = 2; i <= n; i++) {
+  if (n<=2) return sequence.slice(0, n)
+  else for (let i = 2; i < n; i++) {
     sequence.push(sequence[i-1] + sequence[i-2])
   }
-  sequence.pop()
   return sequence
 
 }
