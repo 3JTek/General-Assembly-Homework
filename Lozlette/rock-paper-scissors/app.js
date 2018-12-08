65294670700c26c3b177e028dcfe4361c043d5d7
Lauren Bell <laurengilbert0@gmail.com>
2018-12-08 15:46:20 +0000

    html and css done. Starting JS.

---
+++ Lozlette/rock-paper-scissors/app.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const rock = document.querySelector('.rock')
+
+  //set event listener for button click on rock paper or scissors buttons
+  rock.addEventListener('click', () => {
+    console.log('rock')
+
+  })
+
+})
