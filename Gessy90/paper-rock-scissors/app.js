e1c58ef047f848b71b565019033f723d70010732
Gessy90 <gessicasantoro90@hotmail.it>
2018-12-08 16:41:43 +0000

    final

---
+++ Gessy90/paper-rock-scissors/app.js
@@ -0,0 +1,16 @@
+//winner button no live
+//user has a go
+//when user chooses "paper" the paper button needs to be pressed
+//computer goes
+//when computer chooses "rock" the rock button will be pressed
+//one winning button will appear
+//the right winning button will light up to the right User
+// game Reset
+
+
+
+// const players = 2
+// function getMyUserId() {
+//   return prompt('User', 'Computer')
+// }
+document.addEventListener('DOMContentLoaded', () => {
