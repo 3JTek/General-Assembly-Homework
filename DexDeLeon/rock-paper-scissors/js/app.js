d02558e9c76b58a8948f53f4c6b23e0f79b62db3
Dexter De Leon <dexdeleon@gmail.com>
2018-12-08 20:44:49 +0000

    Basic HTML set up. JS creating buttons from array of button objects. Basic game logic working.

--- DexDeLeon/rock-paper-scissors/js/app.js
+++ DexDeLeon/rock-paper-scissors/js/app.js
@@ -1,25 +1,51 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  let oppChoice = Math.floor(Math.random()*3)
-
-  const win = (choice) => this.beats === choices[choice.name] ? 1 : 0
-
+  const gameBoard = document.getElementById('gameBoard')
   const choices = [
     {
       name: 'rock',
       beats: 'scissors',
-      check: win(oppChoice)
+      check(choice){
+        return this.beats === choices[choice].name
+      }
     },
     {
       name: 'paper',
       beats: 'rock',
-      check: win(oppChoice)
+      check(choice){
+        return this.beats === choices[choice].name
+      }
     },
     {
       name: 'scissors',
       beats: 'paper',
-      check: win(oppChoice)
+      check(choice){
+        return this.beats === choices[choice].name
+      }
     }
   ]
 
+  choices.forEach((choice, i) => {
+    const button = document.createElement('button')
+    button.innerText = `${choice.name}`.toUpperCase()
+    button.setAttribute('value', choice.name)
+    button.dataset.id = i
+
+    // Each time the button is clicked
+    button.addEventListener('click', function(){
+
+      // The computer makes a random choice
+      const oppChoice = Math.floor(Math.random()*3)
+
+      // Choices logged
+      console.log(`The computer chose ${choices[oppChoice].name}`)
+      console.log(`You chose ${choices[i].name}`)
+
+      // If the choices are the same, then the game is a tie...
+      choices[i].name === choices[oppChoice].name ? console.log('It\'s a tie!') :
+        // ...otherwise use the object's check method to see if it is the winning choice
+        choices[i].check(oppChoice) ? console.log('You win') : console.log('You lose')
+    })
+    gameBoard.append(button)
+  })
 })
