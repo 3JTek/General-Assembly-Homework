646488c55c8fea173411a0a209f4f0803fd65ea5
Begona Fernandez <boadicea1979@yahoo.es>
2018-12-08 22:49:30 +0000

    added function for computer to choose an option

--- begonafernandez/rockPaperScissors/app.js
+++ begonafernandez/rockPaperScissors/app.js
@@ -5,12 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
   const notification = document.querySelector('.notification')
 
 
+  const initialPlayerText = 'choose an option'
+  player1.innerText = initialPlayerText
+  player2.innerText = initialPlayerText
+
+  function computerChoice() {
+    const choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * (3 - 1) + 1)]
+  }
 
 
   buttons.forEach(button => {
     button.addEventListener('click', (e) => {
       player1.innerText = e.target.value
-
+      player2.innerText = computerChoice()
+      
     })
   })
 })
