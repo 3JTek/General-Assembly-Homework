a2c4ae786b1cd6faa0b223d3f196773108a1378d
Siddant <siddantgurung@hotmail.co.uk>
2018-12-09 15:58:12 +0000

    game propely work, html and js comibed propely

--- siddant/rock-paper-sissor/js/app.js
+++ siddant/rock-paper-sissor/js/app.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const option = document.querySelectorAll('.btn')
   const reset = document.querySelectorAll('#reset')[0]
+  const content = document.querySelectorAll('p')
+
 
   let rand = 0, total =0, playerWon=0, computerWon=0, winnerIndex=0, userIndex =0
-  const arrayOption = ['Rock', 'Paper','Scissor','Lizard','Spock']
+  const arrayOption = ['Rock', 'Paper','Scissor','Lizard','Spock',' ']
   //Game Rule: Winning Condition
   //rock beat siccior  = [0,2], rock beats lizard = [0, 3]
   //paper beats rock = [1,0] , paper beats spock = [1,4]
@@ -11,29 +13,32 @@ document.addEventListener('DOMContentLoaded', () => {
   //lizard beats spock = [3,4], lizard beats paper = [3,1]
   //spock beats Rock = [4,0], spock beats scissor = [4,2]
   const computerCondition = [[0,2],[0,3],[1,0],[1,4],[2,3],[2,1],[3,4],[3,1],[4,0],[4,2]]
-  const explanation = ['smashes','crushes','covers','disproves','decapitates','cuts','poisons','eats','vaporizes','smashes']
+  const explanation = ['smashes','crushes','covers','disproves','decapitates','cuts','poisons','eats','vaporizes','smashes', ' ']
 
   //game display the Winner and explanation
   const display = function(e,outcome){
-    if(outcome ==='draw'){
-      console.log(`It's a draw both picked ${arrayOption[e]}`)
+    if(outcome ==='It\'s a draw both picked' || outcome ===' '){
+      content[1].innerText = arrayOption[e]
+      content[4].innerText = arrayOption[e]
+      content[2].innerText = outcome +` ${arrayOption[e]}`
     }else{
       computerCondition.forEach((element,index) => {
         if(element.toString() === e.toString()){
           winnerIndex = index
         }
       })
-      console.log(`${outcome} Won, ${arrayOption[e[0]]} ${explanation[winnerIndex]} ${arrayOption[e[1]]}`)
+      content[1].innerText = arrayOption[userIndex]
+      content[4].innerText = arrayOption[rand]
+      content[2].innerText = `${outcome} Won, ${arrayOption[e[0]]} ${explanation[winnerIndex]} ${arrayOption[e[1]]}`
     }
   }
 
   //display the score board
   const displayScore =function(){
-    console.log(`The total outcome is You have won ${playerWon} Games`)
-    console.log(`The Computer have won ${computerWon} Games`)
-    console.log(`The total game played is ${total}`)
-    console.log(`The computer index ${rand}`)
-    console.log(`human index  ${userIndex}`)
+    content[6].innerText = playerWon
+    content[8].innerText = computerWon
+    content[10].innerText = total
+
 
   }
 
@@ -42,9 +47,8 @@ document.addEventListener('DOMContentLoaded', () => {
     total++
     rand = Math.floor(Math.random()*5)
     userIndex = parseInt(e.target.id)
-    console.log(`You Picked ${arrayOption[userIndex]} Computer Picked ${arrayOption[rand]} `)
     if(rand === userIndex){
-      display(rand, 'draw')
+      display(rand, 'It\'s a draw both picked')
     }else if(computerCondition.some(elem => elem.toString() === [rand, userIndex].toString())){
       computerWon++
       display([rand, userIndex], 'Computer')
@@ -56,13 +60,19 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   //Event Listner
-  option.forEach(option => {
+  option.forEach((option,index) => {
     option.addEventListener('click', logic)
+    console.log(index)
   })
 
   reset.addEventListener('click', ()=> {
-    console.log(`computer won ${computerWon}, you Won ${playerWon}, ${total} `)
     total =0, playerWon=0, computerWon=0, winnerIndex=0
+    display(5,' ')
+    displayScore()
   })
+
+
+  display(5,' ')
+  displayScore()
 
 })
