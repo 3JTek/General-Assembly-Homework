1c465536ca315cecb33f278f7146a65a960a2efa
Alessandro Lepri <alex.lepri84@gmail.com>
2018-12-09 15:52:58 +0000

    Try added JS

--- alexlepri/childrens-game/app.js
+++ alexlepri/childrens-game/app.js
@@ -1,4 +1,112 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const rock = document.querySelector('.choice Rock')
+  const paper = document.querySelector('.choice Paper')
+  const scissors = document.querySelector('.choice Scissors')
+  const lizzard = document.querySelector('.choice Lizzard')
+  const spock = document.querySelector('.choice Spock')
+  const player = document.querySelector('.yourChoice')
+  const computer = document.querySelector('.computerChoice')
+  const theWinner = document.querySelector('.result winner')
+  const tray = [1,2,3,4,5]
 
-  
+  rock.addEventListener('click', () => {
+    player.innerHTML = 'Player choose ROCK'
+    const randomChoice = computer[Math.floor(Math.random()*tray.lenght -1)]
+    if (randomChoice === 1) {
+      computer.innerHTML = 'Computer choose Rock'
+      theWinner.innerHTML = 'It\'s a Tie'
+    } else if (randomChoice === 2) {
+      computer.innerHTML = 'Computer choose Paper'
+      theWinner.innerHTML = 'You Lost'
+    } else if (randomChoice === 3) {
+      computer.innerHTML = 'Computer choose Scissors'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else if (randomChoice === 4) {
+      computer.innerHTML = 'Computer choose Lizzard'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else
+    computer.innerHTML = 'Computer choose Spock'
+    theWinner.innerHTML = 'You Lost!!!'
+  },
+
+  scissors.addEventListener('click', () => {
+    player.innerHTML = 'Player choose Scissors'
+    const randomChoice = computer[Math.floor(Math.random()*computer.lenght -1)]
+    if (randomChoice === 1) {
+      computer.innerHTML = 'Computer choose Rock'
+      theWinner.innerHTML = 'It\'s a Tie'
+    } else if (randomChoice === 2) {
+      computer.innerHTML = 'Computer choose Paper'
+      theWinner.innerHTML = 'You Lost'
+    } else if (randomChoice === 3) {
+      computer.innerHTML = 'Computer choose Scissors'
+      theWinner.innerHTML = 'It\'s a Tie'
+    } else if (randomChoice === 4) {
+      computer.innerHTML = 'Computer choose Lizzard'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else
+    computer.innerHTML = 'Computer choose Spock'
+    theWinner.innerHTML = 'You Lost!!!'
+  },
+
+  paper.addEventListener('click', () => {
+    player.innerHTML = 'Player choose ROCK'
+    const randomChoice = computer[Math.floor(Math.random()*computer.lenght -1)]
+    if (randomChoice === 1) {
+      computer.innerHTML = 'Computer choose Rock'
+      theWinner.innerHTML = 'It\'s a Tie'
+    } else if (randomChoice === 2) {
+      computer.innerHTML = 'Computer choose Paper'
+      theWinner.innerHTML = 'It\'s a Tie'
+    } else if (randomChoice === 3) {
+      computer.innerHTML = 'Computer choose Scissors'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else if (randomChoice === 4) {
+      computer.innerHTML = 'Computer choose Lizzard'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else
+    computer.innerHTML = 'Computer choose Spock'
+    theWinner.innerHTML = 'You Lost!!!'
+  },
+
+  lizzard.addEventListener('click', () => {
+    player.innerHTML = 'Player choose ROCK'
+    const randomChoice = computer[Math.floor(Math.random()*computer.lenght -1)]
+    if (randomChoice === 1) {
+      computer.innerHTML = 'Computer choose Rock'
+      theWinner.innerHTML = 'You Lost!!!'
+    } else if (randomChoice === 2) {
+      computer.innerHTML = 'Computer choose Paper'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else if (randomChoice === 3) {
+      computer.innerHTML = 'Computer choose Scissors'
+      theWinner.innerHTML = 'You Lost!!!'
+    } else if (randomChoice === 4) {
+      computer.innerHTML = 'Computer choose Lizzard'
+      theWinner.innerHTML = 'It\'s a Tie'
+    } else
+    computer.innerHTML = 'Computer choose Spock'
+    theWinner.innerHTML = 'You Lost!!!'
+  },
+
+  spock.addEventListener('click', () => {
+    player.innerHTML = 'Player choose ROCK'
+    const randomChoice = computer[Math.floor(Math.random()*computer.lenght -1)]
+    if (randomChoice === 1) {
+      computer.innerHTML = 'Computer choose Rock'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else if (randomChoice === 2) {
+      computer.innerHTML = 'Computer choose Paper'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else if (randomChoice === 3) {
+      computer.innerHTML = 'Computer choose Scissors'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else if (randomChoice === 4) {
+      computer.innerHTML = 'Computer choose Lizzard'
+      theWinner.innerHTML = 'YOU WIN!!!'
+    } else {
+      computer.innerHTML = 'Computer choose Spock'
+      theWinner.innerHTML = 'It\'s a Tie'
+    }
+  }
 }
