ec7a5e44640a80105c91022c4a698f520e605282
Dexter De Leon <dexdeleon@gmail.com>
2018-12-09 18:19:07 +0000

    Some refactoring done to include Lizard and Spock options. Some basic styling added.

--- DexDeLeon/rock-paper-scissors/js/app.js
+++ DexDeLeon/rock-paper-scissors/js/app.js
@@ -1,33 +1,76 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const message = document.getElementById('message')
-  const gameBoard = document.getElementById('gameBoard')
-  const choices = [
-    {
-      name: 'rock',
-      beats: 'scissors',
-      image: `images/${this.name}.png`,
-      check(choice){
-        return this.beats === choices[choice].name
-      }
+  const message = document.getElementById('message'),
+    gameBoard = document.getElementById('gameBoard'),
+    reset = document.createElement('button'),
+    reloadPage = () => window.location.reload(),
+    playerAction = document.querySelector('h3.player.choiceLabel'),
+    playerChoice = document.querySelector('div.player.choice img'),
+    computerAction = document.querySelector('h3.computer.choiceLabel'),
+    computerChoice = document.querySelector('div.computer.choice img'),
+    actionMessage = document.querySelector('#actionMessage'),
+    sprEf = '\nIt\'s super effective!',
+
+    check = function(choice, object, choices, action){
+      if (object.name === choices[choice].name) object.action = action[0]
+      else if (this.beats.includes(choices[choice].name)) object.action = action[1]
+      return
     },
-    {
-      name: 'paper',
-      beats: 'rock',
-      image: `images/${this.name}.png`,
-      check(choice){
-        return this.beats === choices[choice].name
+
+    choices = [
+      {
+        name: 'rock',
+        beats: ['scissors', 'lizard'],
+        image: 'images/rock.png',
+        action: [
+          `The rock used body slam on the scissors.${sprEf}`,
+          `The rock used crush on the lizard.${sprEf}`,
+          'The rocks smash against each other, but nothing happens...'
+        ]
+      },
+      {
+        name: 'paper',
+        beats: ['rock', 'spock'],
+        image: 'images/paper.png',
+        action: [
+          `The paper used cover on the rock.${sprEf}`,
+          `The paper used disprove on Spock.${sprEf}`,
+          'The two sheets of paper lie awkwardly on top of each other, but nothing happens...'
+        ]
+      },
+      {
+        name: 'scissors',
+        beats: ['paper', 'lizard'],
+        image: 'images/scissors.png',
+        action: [
+          `The scissors used cut on the paper.${sprEf}`,
+          `The scissors used decapitate on the lizard.${sprEf}`,
+          'The scissors snip angrily at each other, but nothing happens...'
+        ]
+      },
+      {
+        name: 'lizard',
+        beats: ['paper', 'spock'],
+        image: 'images/scissors.png',
+        action: [
+          `The lizard used devour on the paper.${sprEf}`,
+          `The lizard used poison on Spock.${sprEf}`,
+          'The lizards seem cool with each other; I think they might be friends now...'
+        ]
+      },
+      {
+        name: 'spock',
+        beats: ['rock', 'scissors'],
+        image: 'images/scissors.png',
+        action: [
+          `Spock used disintigrate on the rock.${sprEf}`,
+          `Spock used smash on the scissors.${sprEf}`,
+          'The two Spocks exchange their ideas and logical observations...'
+        ]
       }
-    },
-    {
-      name: 'scissors',
-      beats: 'paper',
-      image: `images/${this.name}.png`,
-      check(choice){
-        return this.beats === choices[choice].name
-      }
-    }
-  ]
+    ]
+
+  let oppChoice = 0
 
   choices.forEach((choice, i) => {
     const button = document.createElement('button')
@@ -39,17 +82,36 @@ document.addEventListener('DOMContentLoaded', () => {
     button.addEventListener('click', function(){
 
       // The computer makes a random choice
-      const oppChoice = Math.floor(Math.random()*3)
+      oppChoice = Math.floor(Math.random()*3)
 
-      // Choices logged
-      console.log(`The computer chose ${choices[oppChoice].name}`)
-      console.log(`You chose ${choices[i].name}`)
+      // Choices displayed
+      computerAction.innerText = `Computer chose ${choices[oppChoice].name.toUpperCase()}`
+      computerChoice.setAttribute('src', choices[oppChoice].image)
+      computerChoice.style.display = 'block'
+      console.log(choices[oppChoice].image)
+      playerAction.innerText = `You chose ${choices[i].name.toUpperCase()}`
+      playerChoice.setAttribute('src', choices[i].image)
+      playerChoice.style.display = 'block'
 
-      // If the choices are the same, then the game is a tie...
-      choices[i].name === choices[oppChoice].name ? message.innerText = 'It\'s a tie!' :
-        // ...otherwise use the object's check method to see if it is the winning choice
-        choices[i].check(oppChoice) ? message.innerText = 'You win' : message.innerText = 'You lose'
+      // choices[oppChoice].check(i)
+      // ...otherwise use the object's check method to see if it is the winning choice
+      if (check(oppChoice)) {
+        message.innerText = 'You win 😄'
+        actionMessage.innerText = choices[i].action
+      } else if (choices[oppChoice].check(i)){
+        message.innerText = 'You lose ☹️'
+        actionMessage.innerText = choices[oppChoice].action
+
+        // If the choices are the same, then the game is a tie...
+      } else if (choices[i].name === choices[oppChoice].name){
+        message.innerText = 'It\'s a tie! 😕'
+        actionMessage.innerText = choices[i].action
+      }
     })
     gameBoard.append(button)
+    reset.innerText = 'Reset'
+    reset.setAttribute('id', 'resetButton')
+    reset.addEventListener('click', reloadPage)
+    gameBoard.append(reset)
   })
 })
