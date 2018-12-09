a30480288f35330fe9a3d1cc64180de3bbf1dd7b
Michael Simmonds <michaelsimmonds92@hotmail.com>
2018-12-09 14:06:38 +0000

    Made small change to styling of buttons and reversed display of computer/user selection

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-const buttonArray = document.querySelectorAll('button')
-const computerResultArea = document.getElementById('computer-picked')
-const userResultArea = document.getElementById('user-picked')
-const outcomeArea = document.getElementById('result')
+  const buttonArray = document.querySelectorAll('button')
+  const computerResultArea = document.getElementById('computer-picked')
+  const userResultArea = document.getElementById('user-picked')
+  const outcomeArea = document.getElementById('result')
 
-const logic = function(userChoice,computerChoice) {
+  const logic = function(userChoice,computerChoice) {
     if (userChoice === computerChoice) {
       outcomeArea.innerHTML = 'You Drew...'
     } else if ((userChoice-computerChoice + 3) % 3 === 1) {
@@ -18,28 +18,28 @@ const logic = function(userChoice,computerChoice) {
   buttonArray.forEach(button => {
     button.addEventListener('click', (e) => {
       userResultArea.innerHTML = e.target.className
-      let userChoice = parseInt(e.target.value)
+      const userChoice = parseInt(e.target.value)
       // reset button
       if (userChoice === 3){
         computerResultArea.innerHTML = ''
         userResultArea.innerHTML = ''
         outcomeArea.innerHTML = ''
       } else {
-        let randomNumber = Math.random()
+        const randomNumber = Math.random()
         // outcome if computer selects rock
         if (randomNumber < 0.33) {
           computerResultArea.innerHTML = 'Rock'
-          let computerChoice = 0
+          const computerChoice = 0
           logic(userChoice,computerChoice)
           // outcome if computer selects scissors
         } else if (randomNumber > 0.66) {
           computerResultArea.innerHTML = 'Scissors'
-          let computerChoice = 2
+          const computerChoice = 2
           logic(userChoice,computerChoice)
           // outcome if computer selects paper
         } else {
           computerResultArea.innerHTML = 'Paper'
-          let computerChoice = 1
+          const computerChoice = 1
           logic(userChoice,computerChoice)
         }
       }
