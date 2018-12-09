02c84f0c86ac089731697e8219c5cf4977977f05
3jtechno <3j.techno@gmail.com>
2018-12-09 19:34:34 +0000

    Add Lizard and Spoke options

--- 3jtechno/rockPaperSicors/app.js
+++ 3jtechno/rockPaperSicors/app.js
@@ -1,20 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const choices = [{name: 'rock', beat: 'scisors'},{name: 'paper', beat: 'rock'},{name: 'scisors', beat: 'paper'}]
-  const displayResults = document.querySelectorAll('span')
+  const choices =
+  [
+    {name: 'rock', beats: ['scisors','lizard']},
+    {name: 'paper', beats: ['rock','spock']},
+    {name: 'scisors', beats: ['paper','lizard']},
+    {name: 'lizard', beats: ['paper','spock']},
+    {name: 'spoke', beats: ['scissors', 'rock']}
+  ]
+
+  const displayResults = document.querySelectorAll('.right h2')
+  const displayChoicePlayer1 = displayResults[0]
+  const displayChoicePlayer2 = displayResults[1]
+  const displayWhoWin = displayResults[2]
   const buttons = document.querySelectorAll('button')
 
-  function play(e){
-    const player2Play = choices[Math.floor(Math.random()*3)]
-    const player1Play = choices[e.target.id]
-    displayResults[0].innerText = player1Play.name
-    displayResults[1].innerText = player2Play.name
-    if(player1Play.name === player2Play.name){
-      displayResults[2].innerText = 'Ties'
-    } else if(player1Play.beat === player2Play.name){
-      displayResults[2].innerText = 'Player 1 wins'
+  function findWinner(player1, player2){
+    console.log(player1.beats + player2.name);
+
+    if(player1.name === player2.name){
+      displayWhoWin.innerText = 'Ties'
+    } else if(player1.beats.includes(player2.name)){
+      displayWhoWin.innerText = 'Player 1 wins'
     } else{
-      displayResults[2].innerText = 'Player 2 wins'
+      displayWhoWin.innerText = 'Player 2 wins'
     }
+  }
+  function play(e){
+    const player2 = choices[Math.floor(Math.random()*5)]
+    const player1 = choices[e.target.id]
+    displayChoicePlayer1.innerText = player1.name
+    displayChoicePlayer2.innerText = player2.name
+    findWinner(player1, player2)
   }
 
   buttons.forEach(button => {
