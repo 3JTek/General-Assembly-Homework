bb44a40ef2210134fc92e40f8f4c8a02f39236cf
Ben Lander <ben.l.lander@googlemail.com>
2018-12-09 15:29:36 +0000

    Game logic in place

--- BenLeo/rock-paper-scissors/app.js
+++ BenLeo/rock-paper-scissors/app.js
@@ -1,6 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JS Loaded')
-  const choices = ['rock', 'paper', 'scissors']
-  console.log(choices[1])
 
+  const choices = ['rock', 'paper', 'scissors']
+  const cpuChoice = choices[Math.floor((Math.random() * choices.length) + 0)]
+  console.log(cpuChoice)
+
+  let userChoice
+
+  const buttons = document.querySelectorAll('button')
+
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', () => {
+      userChoice = buttons[i].value
+      compare (userChoice, cpuChoice)
+    })
+  }
+
+  const compare = function(userChoice, cpuChoice) {
+    if(userChoice===cpuChoice) { alert('You chose ' + userChoice + ' and your opponent chose ' + cpuChoice + '. The result is a tie!')
+    } else {
+      switch(userChoice+cpuChoice){
+        case 'rockscissors': case 'scissorsrock':
+          return alert('You chose ' + userChoice + ' and your opponent chose ' + cpuChoice + '. Rock wins!')
+        case 'rockpaper': case 'paperrock':
+          return alert('You chose ' + userChoice + ' and your opponent chose ' + cpuChoice + '. Paper wins!')
+        default: return alert('You chose ' + userChoice + ' and your opponent chose ' + cpuChoice + '. Scissors wins!')
+      }
+    }
+}
+
+// compare(userChoice, cpuChoice)
 })
