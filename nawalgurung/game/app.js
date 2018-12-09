63110b9d002a494372b92b428517dc04fb91f716
Nawal00 <nawalgurung80@gmail.com>
2018-12-09 11:50:52 +0000

    random selector option

--- nawalgurung/game/app.js
+++ nawalgurung/game/app.js
@@ -4,8 +4,23 @@ document.addEventListener('DOMContentLoaded', () => {
   const userRock = document.querySelector('#rock')
   const userPaper = document.querySelector('#paper')
   const userScissors = document.querySelector('#scissors')
+  const computerBtn = document.querySelector('#computerBtn')
 
   //create function to generate Random rock, paper, scissors
+computerBtn.addEventListener('click', () => {
+    let computerChoice = Math.floor(Math.random()*3)
+    if(computerChoice === 0){
+      computerChoice = 'rock'
+    } if(computerChoice === 1){
+      computerChoice = 'paper'
+    } else{
+      computerChoice = 'scissors'
+    }
+  })
 
+  //create function to compare the user Choice with random selected rock paper scissors
+function compare(computerChoice, userChoice){
+
+}
 
 })
