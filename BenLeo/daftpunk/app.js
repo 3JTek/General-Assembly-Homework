b07d0e733f6ac04f8ad074058dde3d6e3ed1e62b
Ben Lander <ben.l.lander@googlemail.com>
2018-12-09 12:20:51 +0000

    Initial Commit

--- BenLeo/daftpunk/app.js
+++ BenLeo/daftpunk/app.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const buttons = document.getElementsByTagName('button')
+  const buttons = document.querySelectorAll('button')
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', () => {
       (buttons[i].parentElement.children[1]).play()
---
+++ BenLeo/rock-paper-scissors/app.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JS Loaded')
+  const choices = ['rock', 'paper', 'scissors']
+  console.log(choices[1])
+
+})
