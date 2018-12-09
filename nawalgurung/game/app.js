219d2b0bf88e0b77db4dc535f0c3e1683fc67ca1
Nawal00 <nawalgurung80@gmail.com>
2018-12-09 10:52:58 +0000

    initial commit

---
+++ nawalgurung/game/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  //create event listerners for btns
+  const userRock = document.querySelector('#rock')
+  const userPaper = document.querySelector('#paper')
+  const userScissors = document.querySelector('#scissors')
+
+  //create function to generate Random rock, paper, scissors
+
+
+})
