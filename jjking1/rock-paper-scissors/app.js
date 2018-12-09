450939d97b76abdf62b01efae069ec733a5ddc21
joshuaking06 <joshua.king0692@gmail.com>
2018-12-09 16:47:36 +0000

    finished game

--- jjking1/rock-paper-scissors/app.js
+++ jjking1/rock-paper-scissors/app.js
@@ -1,32 +1,44 @@
 const rock = document.getElementById('rock')
 const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
-const choices = ['rock', 'paper', 'scissors']
+const choices = ['Rock', 'Paper', 'Scissors']
 const winnerDisplay = document.querySelector('.result')
 const resetBtn = document.querySelector('#reset')
 const userScoreDisplay = document.querySelector('#user-score')
+const userChoiceDisplay = document.querySelector('#user-choice-display')
 const cpuScoreDisplay = document.querySelector('#cpu-score')
+const cpuChoiceDisplay = document.querySelector('#cpu-choice-display')
 let userScore = 0, cpuScore =0
 
 // win conditions
 rock.addEventListener('click', () => {
+  // cpu chooses
   const cpuChoice = cpuRandom()
-  if (cpuChoice === 'paper') youLose()
-  else if (cpuChoice ==='scissors') youWin()
+  // updating chosentext on board
+  userChoiceDisplay.textContent = 'Rock'
+  cpuChoiceDisplay.textContent = cpuChoice
+  // calculating winner
+  if (cpuChoice === 'Paper') youLose()
+  else if (cpuChoice ==='Scissors') youWin()
   else resultIsDraw()
+
 })
 
 paper.addEventListener('click', () => {
   const cpuChoice = cpuRandom()
-  if (cpuChoice === 'scissors') youLose()
-  else if (cpuChoice ==='rock') youWin()
+  userChoiceDisplay.textContent = 'Paper'
+  cpuChoiceDisplay.textContent = cpuChoice
+  if (cpuChoice === 'Scissors') youLose()
+  else if (cpuChoice ==='Rock') youWin()
   else resultIsDraw()
 })
 
 scissors.addEventListener('click', () => {
   const cpuChoice = cpuRandom()
-  if (cpuChoice === 'rock') youLose()
-  else if (cpuChoice ==='paper') youWin()
+  userChoiceDisplay.textContent = 'Scissors'
+  cpuChoiceDisplay.textContent = cpuChoice
+  if (cpuChoice === 'Rock') youLose()
+  else if (cpuChoice ==='Paper') youWin()
   else resultIsDraw()
 })
 
@@ -49,9 +61,11 @@ const youWin = () => {
   userScoreDisplay.textContent = userScore
 }
 
-const resultIsDraw = () => winnerDisplay.innerHTML = 'Draw! Nobody wins. Try Again'
+const resultIsDraw = () => winnerDisplay.innerHTML = 'Draw! Try Again.'
 
 const reset = () => {
+  userChoiceDisplay.textContent = ''
+  cpuChoiceDisplay.textContent = ''
   userScore = 0,
   cpuScore = 0,
   userScoreDisplay.textContent = userScore
