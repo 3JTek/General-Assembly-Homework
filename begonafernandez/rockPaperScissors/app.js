1ad9fdf584a0e6f8bf957b03e9dc24458e6e8752
Begona Fernandez <boadicea1979@yahoo.es>
2018-12-09 21:07:04 +0000

    added reset function

--- begonafernandez/rockPaperScissors/app.js
+++ begonafernandez/rockPaperScissors/app.js
@@ -2,16 +2,28 @@ document.addEventListener('DOMContentLoaded', () => {
   const player1 = document.querySelector('.player1').querySelector('span')
   const player2 = document.querySelector('.player2').querySelector('span')
   const buttons = document.querySelectorAll('.button.option')
+  const resetButton = document.querySelector('.button.reset')
   const notification = document.querySelector('.notification')
 
 
-  const initialPlayerText = 'choose an option'
-  player1.innerText = initialPlayerText
-  player2.innerText = initialPlayerText
+  function initialPlayerFields() {
+    const initialPlayerText = 'choose an option'
+    player1.innerText = initialPlayerText
+    player2.innerText = initialPlayerText
+  }
+
+  function initialGameStatus() {
+    notification.innerText= 'No winner'
+  }
 
   function computerChoice() {
     const choices = ['rock', 'paper', 'scissors']
     return choices[Math.floor(Math.random() * (3))]
+  }
+
+  function reset() {
+    initialPlayerFields()
+    initialGameStatus()
   }
 
   function winner(play1Choice, player2Choice) {
@@ -26,6 +38,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
+  reset()
+
   buttons.forEach(button => {
     button.addEventListener('click', (e) => {
       player1.innerText = e.target.value
@@ -33,4 +47,6 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(winner(player1.innerText, player2.innerText))
     })
   })
+
+  resetButton.addEventListener('click', reset)
 })
