86df21a079ed12ec510212349de73e49d02bcf27
joshuaking06 <joshua.king0692@gmail.com>
2018-12-09 15:21:45 +0000

    began styling. set up fonts and container

--- jjking1/rock-paper-scissors/app.js
+++ jjking1/rock-paper-scissors/app.js
@@ -2,14 +2,13 @@ const rock = document.getElementById('rock')
 const paper = document.getElementById('paper')
 const scissors = document.getElementById('scissors')
 const choices = ['rock', 'paper', 'scissors']
-const winnerDisplay = document.querySelector('.winner-display')
+const winnerDisplay = document.querySelector('.result')
 const resetBtn = document.querySelector('#reset')
 
 
-
+// win conditions
 rock.addEventListener('click', () => {
   const cpuChoice = cpuRandom()
-  console.log(cpuChoice)
   if (cpuChoice === 'paper') youLose()
   else if (cpuChoice ==='scissors') youWin()
   else resultIsDraw()
@@ -17,7 +16,6 @@ rock.addEventListener('click', () => {
 
 paper.addEventListener('click', () => {
   const cpuChoice = cpuRandom()
-  console.log(cpuChoice)
   if (cpuChoice === 'scissors') youLose()
   else if (cpuChoice ==='rock') youWin()
   else resultIsDraw()
@@ -25,28 +23,23 @@ paper.addEventListener('click', () => {
 
 scissors.addEventListener('click', () => {
   const cpuChoice = cpuRandom()
-  console.log(cpuChoice)
   if (cpuChoice === 'rock') youLose()
   else if (cpuChoice ==='paper') youWin()
   else resultIsDraw()
 })
 
+// reset button
 resetBtn.addEventListener('click', () => reset())
 
 
 
 
 
-
+// result functions
 const youLose = () => winnerDisplay.innerHTML ='Cpu Wins'
 const youWin = () => winnerDisplay.innerHTML = 'You Win'
 const resultIsDraw = () => winnerDisplay.innerHTML = 'Draw! Nobody wins. Try Again'
-
 const reset = () => winnerDisplay.innerHTML = 'Please choose Rock, Paper, or Scissors'
-
-
-
-
 const cpuRandom =() => {
   return choices[Math.ceil(Math.random()*choices.length-1)]
 }
