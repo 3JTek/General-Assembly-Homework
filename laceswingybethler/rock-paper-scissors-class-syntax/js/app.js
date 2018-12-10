f1abdef6932ae36dda48b9d12f13b5e4e7dde892
laceswingybethler <beth.swingler@gmail.com>
2018-12-10 16:55:15 +0000

    Basic translation done. Need to now do reset btn...

---
+++ laceswingybethler/rock-paper-scissors-class-syntax/js/app.js
@@ -0,0 +1,60 @@
+
+class RPS {
+  constructor() {
+    // GETTING DOM ELEMENTS
+    this.buttons = document.querySelectorAll('button.choice')
+    this.player1 = document.querySelector('.player1')
+    this.player2 = document.querySelector('.player2')
+    this.result = document.querySelector('.result')
+    this.resetBtn = document.querySelector('button.reset')
+
+    this.winConditions = {
+      rock: 'scissors',
+      paper: 'rock',
+      scissors: 'paper'
+    }
+
+    this.choices = Object.keys(this.winConditions)
+
+    this.play = this.play.bind(this)
+
+    // INITIALIZING THE EVENT LISTENERS
+    this.init()
+  }
+
+  // ASSIGN EVENT LISTENERS to when the player clicks, initialise RPS
+  init() {
+    this.buttons.forEach(button => button.addEventListener('click', this.play))
+    this.resetBtn.addEventListener('click', this.reset)
+  }
+
+
+  makeChoice() {
+    return this.choices[Math.floor(Math.random() * this.choices.length)]
+  }
+
+  findWinner(player1Choice, player2Choice) {
+    if(player1Choice === player2Choice) return 'Tie'
+    if(this.winConditions[player1Choice] === player2Choice) return 'You win'
+    return 'You lose'
+  }
+
+  play(e) {
+    const player1Choice = e.target.textContent
+    const player2Choice = this.makeChoice()
+
+    this.player1.textContent = player1Choice
+    this.player2.textContent = player2Choice
+
+    this.result.textContent = this.findWinner(player1Choice, player2Choice)
+  }
+
+  reset() {
+    this.layer1.textContent = ''
+    this.layer2.textContent = ''
+    this.result.textContent = ''
+  }
+}
+
+
+window.addEventListener('DOMContentLoaded', () => new RPS())
