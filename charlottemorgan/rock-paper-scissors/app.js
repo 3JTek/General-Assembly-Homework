5a29bac071f30c422e563f97446fc6e6a5ec5100
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2018-12-10 17:28:05 +0000

    completed bonus task and added buttons using JS

--- charlottemorgan/rock-paper-scissors/app.js
+++ charlottemorgan/rock-paper-scissors/app.js
@@ -2,22 +2,17 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('JS loaded')
   // find the buttons from the html
   const buttons = document.querySelectorAll('.main-buttons')
-  console.log('buttons')
   //find the form for the computer input from the html
   const playerOne = document.getElementById('Player-1')
-  console.log('playerOne')
   //find the form for the user input from the html
   const playerTwo = document.getElementById('Player-2')
-  console.log('playerTwo')
   //find the result input from the html
   const resultValue = document.getElementById('result')
   //find the reset button in the html
   const resetButton = document.querySelector('.reset-button')
-  console.log('resetButton')
+  //array with possible options
+  const options = ['Rock', 'Paper', 'Scissors']
 
-
-  // // add event listener for reset button
-  // resetButton.addEventListener('click', reset)
   //when button is clicked run function to clear values in the forms
   function reset() {
     // make the value of playerOne empty
@@ -26,16 +21,11 @@ document.addEventListener('DOMContentLoaded', () => {
     playerTwo.value = ''
     // make the result empty
     resultValue.value = ''
-
   }
-
+  // add event listener for reset button
   resetButton.addEventListener('click', reset)
 
-
-  //array with possible options
-  const options = ['Rock', 'Paper', 'Scissors']
-
-  // get the button.value to fill player one form when clicked.
+  // get the button to fill player one form when clicked.
   function compareValues(option1, option2) {
     if (option1 === option2) {
       return resultValue.value = 'You have tied!'
@@ -72,10 +62,9 @@ document.addEventListener('DOMContentLoaded', () => {
   buttons.forEach(button => {
     //Add click event to each button
     button.addEventListener('click', e => {
-      console.log(e.target.value)
       // make the value of playerOne equal to the value of the button
       playerOne.value = e.target.value
-      // make the value of playerTwo equal to a random string in the outcomes array
+      // make the value of playerTwo equal to a random string in the options array
       playerTwo.value = options[Math.floor(Math.random() * options.length)]
       // call the funtion
       compareValues(playerOne.value, playerTwo.value)
