487f2bfa2202f4ba2e3cc230200b5e189c935c4d
3jtechno <3j.techno@gmail.com>
2018-12-10 17:35:45 +0000

    Create button within the class

--- 3jtechno/rock-paper-scissors-class-syntax/js/app.js
+++ 3jtechno/rock-paper-scissors-class-syntax/js/app.js
@@ -1,6 +1,23 @@
 class RockPaperScissors{
   constructor(){
-    this.buttons = document.querySelectorAll('button.choice')
+    const div = document.querySelector('.game')
+    this.button1 = document.createElement('button')
+    this.button1.className = 'choice'
+    this.button1.innerText = 'rock'
+    div.insertBefore(this.button1, div.childNodes[6])
+
+    this.button2 = document.createElement('button')
+    this.button2.className = 'choice'
+    this.button2.innerText = 'paper'
+    div.insertBefore(this.button2, div.childNodes[7])
+
+    this.button3 = document.createElement('button')
+    this.button3.className = 'choice'
+    this.button3.innerText = 'scissors'
+    div.insertBefore(this.button3, div.childNodes[8])
+
+
+
     this.player1 = document.querySelector('.player1')
     this.player2 = document.querySelector('.player2')
     this.result = document.querySelector('.result')
@@ -61,60 +78,3 @@ class RockPaperScissors{
 document.addEventListener('DOMContentLoaded', () => {
   new RockPaperScissors()
 })
-
-// // VARIABLES
-// let buttons
-// let player1
-// let player2
-// let result
-// let resetBtn
-//
-// const winConditions = {
-//   rock: 'scissors',
-//   paper: 'rock',
-//   scissors: 'paper'
-// }
-// // const choices = Object.keys(winConditions)
-//
-// // FUNCTIONS
-// function makeChoice() {
-//   return choices[Math.floor(Math.random() * choices.length)]
-// }
-//
-// function findWinner(player1Choice, player2Choice) {
-//   if(player1Choice === player2Choice) return 'Tie'
-//   if(winConditions[player1Choice] === player2Choice) return 'You win'
-//   return 'You lose'
-// }
-//
-// function play(e) {
-//
-//   const player1Choice = e.target.textContent
-//   const player2Choice = makeChoice()
-//
-//   player1.textContent = player1Choice
-//   player2.textContent = player2Choice
-//
-//   result.textContent = findWinner(player1Choice, player2Choice)
-// }
-//
-// function reset() {
-//   player1.textContent = ''
-//   player2.textContent = ''
-//   result.textContent = ''
-// }
-//
-// function init() {
-//   // DOM ELEMENTS
-//   buttons = document.querySelectorAll('button.choice')
-//   player1 = document.querySelector('.player1')
-//   player2 = document.querySelector('.player2')
-//   result = document.querySelector('.result')
-//   resetBtn = document.querySelector('button.reset')
-//
-//   // EVENT LISTENERS
-//   buttons.forEach(button => button.addEventListener('click', play))
-//   resetBtn.addEventListener('click', reset)
-// }
-//
-// window.addEventListener('DOMContentLoaded', init)
