305f2284c84fe01d0e3216b91ae60cac07a74ffb
Siddant <siddantgurung@hotmail.co.uk>
2018-12-10 19:14:24 +0000

    bonus completed

--- siddant/rock-paper-scissors-class-syntax/js/app.js
+++ siddant/rock-paper-scissors-class-syntax/js/app.js
@@ -1,23 +1,33 @@
 // VARIABLES
 
-class Game{
+class RPS{
   constructor(){
+    this.winConditions = {
+      rock: ['scissors','lizard'],
+      paper: ['rock', 'spock'],
+      scissors: ['paper','lizard'],
+      lizard: ['paper','spock'],
+      spock: ['rock', 'scissor']
+    }
+    this.choices = Object.keys(this.winConditions)
+
+    const game = document.querySelector('.game')
+    this.resetBtn = document.querySelector('button.reset')
+
+    this.choices.forEach(element  => {
+      const button = document.createElement('button')
+      button.textContent = element
+      button.setAttribute('class', 'choice')
+      game.insertBefore(button,this.resetBtn)
+    })
+
     this.buttons = document.querySelectorAll('button.choice')
     this.player1 = document.querySelector('.player1')
     this.player2 = document.querySelector('.player2')
     this.result = document.querySelector('.result')
-    this.resetBtn = document.querySelector('button.reset')
-    this.winConditions = {
-      rock: 'scissors',
-      paper: 'rock',
-      scissors: 'paper'
-    }
-    this.choices = Object.keys(this.winConditions)
-
     this.reset = this.reset.bind(this)
     this.play = this.play.bind(this)
     this.init()
-
 
   }
   //function
@@ -40,20 +50,17 @@ class Game{
   play(e) {
     const player1Choice = e.target.textContent
     const player2Choice = this.makeChoice()
-
-
     this.player1.textContent = player1Choice
     this.player2.textContent = player2Choice
-
     this.result.textContent = this.findWinner(player1Choice, player2Choice)
   }
+
   findWinner(player1Choice, player2Choice) {
     if(player1Choice === player2Choice) return 'Tie'
-    if(this.winConditions[player1Choice] === player2Choice) return 'You win'
+    if(this.winConditions[player1Choice][0] === player2Choice || this.winConditions[player1Choice][1] === player2Choice) return 'You win'
     return 'You lose'
   }
-
 }
 
 
-window.addEventListener('DOMContentLoaded', () => new Game())
+window.addEventListener('DOMContentLoaded', () => new RPS())
