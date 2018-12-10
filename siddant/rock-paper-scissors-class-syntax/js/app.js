a3d4055171176f8bb34e12ad6ca288ad92116119
Siddant <siddantgurung@hotmail.co.uk>
2018-12-10 16:48:54 +0000

    basic done

--- siddant/rock-paper-scissors-class-syntax/js/app.js
+++ siddant/rock-paper-scissors-class-syntax/js/app.js
@@ -13,54 +13,47 @@ class Game{
       scissors: 'paper'
     }
     this.choices = Object.keys(this.winConditions)
-    this.init()
-  }
-  init(){
-    // EVENT LISTENERS
-    //this.buttons.forEach(button => button.addEventListener('click', play))
-    this.resetBtn.addEventListener('click', this.makeChoice)
-  }
 
+    this.reset = this.reset.bind(this)
+    this.play = this.play.bind(this)
+    this.init()
+
+
+  }
   //function
   makeChoice() {
-    console.log( this.choices[Math.floor(Math.random() * this.choices.length)])
+    return this.choices[Math.floor(Math.random() * this.choices.length)]
   }
 
+  init(){
+    // EVENT LISTENERS
+    this.buttons.forEach(button => button.addEventListener('click', this.play))
+    this.resetBtn.addEventListener('click', this.reset)
+  }
+
+  reset() {
+    this.player1.textContent = ''
+    this.player2.textContent = ''
+    this.result.textContent = ''
+  }
+
+  play(e) {
+    const player1Choice = e.target.textContent
+    const player2Choice = this.makeChoice()
+
+
+    this.player1.textContent = player1Choice
+    this.player2.textContent = player2Choice
+
+    this.result.textContent = this.findWinner(player1Choice, player2Choice)
+  }
+  findWinner(player1Choice, player2Choice) {
+    if(player1Choice === player2Choice) return 'Tie'
+    if(this.winConditions[player1Choice] === player2Choice) return 'You win'
+    return 'You lose'
+  }
 
 }
 
-
-
-//
-// // FUNCTIONS
-
-//
-// function findWinner(player1Choice, player2Choice) {
-//   if(player1Choice === player2Choice) return 'Tie'
-//   if(winConditions[player1Choice] === player2Choice) return 'You win'
-//   return 'You lose'
-// }
-//
-// function play(e) {
-//
-//   const player1Choice = e.target.textContent
-//   const player2Choice = makeChoice()
-//
-//   player1.textContent = player1Choice
-//   player2.textContent = player2Choice
-//
-//   result.textContent = findWinner(player1Choice, player2Choice)
-// }
-//
-// function reset() {
-//   player1.textContent = ''
-//   player2.textContent = ''
-//   result.textContent = ''
-// }
-//
-// function init() {
-//   // DOM ELEMENTS
-//
-// }
 
 window.addEventListener('DOMContentLoaded', () => new Game())
