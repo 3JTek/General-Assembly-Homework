198642aa2cc91b56e7bb428de547c6f9bbebaa53
joshuaking06 <joshua.king0692@gmail.com>
2018-12-11 19:11:42 +0000

    finished bonus objectives

--- jjking1/whack-a-mole/app.js
+++ jjking1/whack-a-mole/app.js
@@ -1,16 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
+  // selecting elements
   const option = document.querySelectorAll('.moleholder')
-  const scoreDisplay = document.querySelector('#score')
+  const scoreDisplay = document.querySelector('.score')
   const countDisplay = document.querySelector('#countDisplay')
   const startBtn = document.querySelector('#start-button')
+  const startDisplay = document.querySelector('.start-game')
+  const resultDisplay = document.querySelector('#result-display')
 
 
-  let rand =0, userscore=0, count =59
 
+  let rand =0, userScore=0, count =59
+  // choosing a random mole
   function getRandom(){
     return Math.floor( Math.random() * option.length)
   }
-
+  // activating the mole
   function activeMole(){
     rand = getRandom()
     option[rand].classList.add('active')
@@ -18,27 +22,32 @@ document.addEventListener('DOMContentLoaded', () => {
       option[rand].classList.remove('active')
     },750)
   }
-
+  // counting down from 60
   function countdown(){
     countDisplay.textContent = count
     count--
   }
-
+  // increasing score if you hit mole, decreasing if you miss,choose clicks carefully. then updating text
   function userClicked(e){
     if(parseInt(e.target.id) === rand){
-      userscore++
+      userScore++
       e.target.removeEventListener('click', userClicked)
+    } else{
+      // choose your clicks carefully
+      userScore --
     }
-    scoreDisplay.innerText= userscore
+    scoreDisplay.innerText= userScore
   }
 
+  // reset the count and score for new game
   function resetScoreAndTimer(){
     count = 59
     countDisplay.textContent = 60
-    userscore = 0
+    userScore = 0
     scoreDisplay.textContent = 0
   }
 
+  // running the game
   function init(){
     resetScoreAndTimer()
     const timerId = setInterval(() => {
@@ -49,16 +58,19 @@ document.addEventListener('DOMContentLoaded', () => {
       countdown()
     }, 1000)
 
+    // ending the game
     setTimeout(() => {
       clearInterval(timerId)
-      startBtn.style.display='inline'
+      startDisplay.style.display='flex'
+      resultDisplay.innerText = `You scored ${userScore}. Play Again?`
     },60000)
 
   }
 
+  // beginning the game
   startBtn.addEventListener('click', () => {
     init()
-    startBtn.style.display='none'
+    startDisplay.style.display='none'
 
 
   })
