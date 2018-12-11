5d9c41cf3037783070b054845f01ca61b0a8b607
Dexter De Leon <dexdeleon@gmail.com>
2018-12-11 23:03:54 +0000

    Added some extra styling. Refactored JavaScript. Changed mole image.

--- DexDeLeon/whack-a-mole/js/app.js
+++ DexDeLeon/whack-a-mole/js/app.js
@@ -1,67 +1,81 @@
+let grid,
+  timer,
+  scoreDisplay,
+  startScreen,
+  startBtn,
+  mole,
+  score = 0,
+  finalScore
+
+function clickMole() {
+  score += 1
+  scoreDisplay.innerText = score
+  mole.classList.add('rubberBand')
+  // prevents mashing mole and getting more than one point by removing event listener after point
+  mole.removeEventListener('click', clickMole)
+}
+
+function hideButton() {
+  score = 0
+  scoreDisplay.innerText = score
+  startScreen.style.display = 'none'
+}
+
+function reset(countdown, timerId){
+  clearInterval(countdown)
+  clearInterval(timerId)
+  finalScoreFunc()
+  startBtn.innerText = 'Play again'
+  startScreen.style.display = 'flex'
+
+}
+// puts mole class onto random div
+function displayMole(randomNum){
+  grid[randomNum].classList.toggle('mole')
+  mole = document.querySelector('.mole')
+  mole.addEventListener('click', clickMole)
+}
+
+function hideMole(randomNum){
+  mole.classList.remove('rubberBand')
+  mole.removeEventListener('click', clickMole)
+  grid[randomNum].classList.toggle('mole')
+}
+
+// generates mole to randomly appear and disappear around the grid
+function startGame(){
+  hideButton()
+  const timerId = setInterval(() => {
+    const randomNum = Math.floor(Math.random() * 9)
+    displayMole(randomNum)
+    setTimeout(() => hideMole(randomNum), 750)
+  }, 1000)
+  // countdown
+  let timeRemaining = 60
+  const countdown = setInterval(() => {
+    timer.innerText = timeRemaining
+    timeRemaining--
+    if (timeRemaining === 0) {
+      // stops timer and moles appearing at the same moment
+      setTimeout(() => reset(countdown, timerId), 1000)
+    }
+  }, 1000)
+}
+
+function finalScoreFunc() {
+  finalScore.innerText = `You scored ${score}!`
+  finalScore.classList.add('finalScore')
+  startScreen.append(finalScore)
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const grid = document.querySelectorAll('div.cell')
-  const timer = document.querySelector('#time')
-  const scoreDisplay = document.querySelector('#score')
-  const startScreen = document.querySelector('div.start')
-  const startBtn = document.querySelector('#startBtn')
-  let mole
-
-  let score = 0
-  const clickMole = function() {
-    score += 1
-    scoreDisplay.innerText = score
-    // prevents mashing mole and getting more than one point by removing event listener after point
-    mole.removeEventListener('click', clickMole)
-  }
-
-  const hideButton = function() {
-    score = 0
-    scoreDisplay.innerText = score
-    startScreen.style.display = 'none'
-  }
-
-  // generates mole to randomly appear and disappear around the grid
-  function startGame(){
-    hideButton()
-    const timerId = setInterval(() => {
-      const randomNum = Math.floor(Math.random() * 9)
-      // puts mole class onto random div
-      grid[randomNum].classList.toggle('mole')
-      mole = document.querySelector('.mole')
-      mole.addEventListener('click', clickMole)
-      setTimeout(() => {
-        mole.removeEventListener('click', clickMole)
-        grid[randomNum].classList.toggle('mole')
-      }, 800)
-    }, 1000)
-
-
-    // countdown
-    let timeRemaining = 10
-    const countdown = setInterval(() => {
-      timer.innerText = timeRemaining
-      timeRemaining--
-      if (timeRemaining < 0) {
-        // stops timer and moles appearing at the same moment
-        clearInterval(countdown)
-        clearInterval(timerId)
-        finalScoreFunc()
-        startBtn.innerText = 'Play again'
-        startScreen.style.display = 'flex'
-
-      }
-    }, 1000)
-  }
-
-const finalScore = document.createElement('div')
-
-  const finalScoreFunc = function() {
-    finalScore.innerText = `You scored ${score}!`
-    finalScore.classList.add('finalScore')
-    startScreen.append(finalScore)
-  }
+  grid = document.querySelectorAll('div.cell')
+  timer = document.querySelector('#time')
+  scoreDisplay = document.querySelector('#score')
+  startScreen = document.querySelector('div.start')
+  startBtn = document.querySelector('#startBtn')
+  finalScore = document.createElement('div')
   startBtn.addEventListener('click', startGame)
 
 })
