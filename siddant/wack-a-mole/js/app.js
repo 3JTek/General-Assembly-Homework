e4b78c76db1a2e155b45c3515c9b2fd1951054cb
Siddant <siddantgurung@hotmail.co.uk>
2018-12-11 15:46:02 +0000

    josh and begona did countdown

--- siddant/wack-a-mole/js/app.js
+++ siddant/wack-a-mole/js/app.js
@@ -1,11 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
   const option = document.querySelectorAll('.moleholder')
+  const scoredisplay = document.querySelector('#score')
+  const countDisplay = document.querySelector('#countDisplay')
+
+  let rand =0, userscore=0, count =59
 
   function getRandom(){
     return Math.floor( Math.random() * option.length)
   }
-  getRandom()
-  option[getRandom()].classList.add('active')
+
+  function activeMole(){
+    rand = getRandom()
+    option[rand].classList.add('active')
+    setTimeout(() => {
+      option[rand].classList.remove('active')
+    },850)
+  }
   //console.log(option[getRandom()])
 
+
+  function countdown(){
+    countDisplay.textContent = count
+    count--
+  }
+
+
+  const timerId = setInterval(() => {
+    activeMole()
+    countdown()
+  }, 1000)
+
+
+  function userClicked(e){
+    if(parseInt(e.target.id) === rand){
+      console.log('well done')
+      userscore++
+    }
+    scoredisplay.innerText= `You Scored: ${userscore}`
+    console.log(`score ${userscore}`)
+  }
+
+  option.forEach(option => {
+    option.addEventListener('click', userClicked)
+  }
+  )
+
+
+  setTimeout(() => {
+    clearInterval(timerId)
+  },60000)
 })
