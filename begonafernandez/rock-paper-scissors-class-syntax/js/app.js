d6fa81b174b41a8c42919940c8d18e66211c04b6
Begona Fernandez <boadicea1979@yahoo.es>
2018-12-11 07:48:12 +0000

    refactored

--- begonafernandez/rock-paper-scissors-class-syntax/js/app.js
+++ begonafernandez/rock-paper-scissors-class-syntax/js/app.js
@@ -1,56 +1,55 @@
-// VARIABLES
-let buttons
-let player1
-let player2
-let result
-let resetBtn
+class RockPaperScissors {
+  constructor() {
+    this.buttons = document.querySelectorAll('button.choice')
+    this.player1 = document.querySelector('.player1')
+    this.player2 = document.querySelector('.player2')
+    this.result = document.querySelector('.result')
+    this.resetBtn = document.querySelector('button.reset')
+    this.winConditions = {
+      rock: 'scissors',
+      paper: 'rock',
+      scissors: 'paper'
+    }
+    this.choices = Object.keys(this.winConditions)
 
-const winConditions = {
-  rock: 'scissors',
-  paper: 'rock',
-  scissors: 'paper'
-}
-const choices = Object.keys(winConditions)
+    this.reset = this.reset.bind(this)
+    this.play = this.play.bind(this)
+    this.init()
+  }
 
-// FUNCTIONS
-function makeChoice() {
-  return choices[Math.floor(Math.random() * choices.length)]
-}
+  makeChoice() {
+    return this.choices[Math.floor(Math.random() * this.choices.length)]
+  }
 
-function findWinner(player1Choice, player2Choice) {
-  if(player1Choice === player2Choice) return 'Tie'
-  if(winConditions[player1Choice] === player2Choice) return 'You win'
-  return 'You lose'
-}
+  findWinner(player1Choice, player2Choice) {
+    if(player1Choice === player2Choice) return 'Tie'
+    if(this.winConditions[player1Choice] === player2Choice) return 'You win'
+    return 'You lose'
+  }
 
-function play(e) {
+  play(e) {
+    const player1Choice = e.target.textContent
+    const player2Choice = this.makeChoice()
 
-  const player1Choice = e.target.textContent
-  const player2Choice = makeChoice()
+    this.player1.textContent = player1Choice
+    this.player2.textContent = player2Choice
 
-  player1.textContent = player1Choice
-  player2.textContent = player2Choice
+    this.result.textContent = this.findWinner(player1Choice, player2Choice)
+  }
 
-  result.textContent = findWinner(player1Choice, player2Choice)
-}
+  reset() {
+    this.player1.textContent = ''
+    this.player2.textContent = ''
+    this.result.textContent = ''
+  }
 
-function reset() {
-  player1.textContent = ''
-  player2.textContent = ''
-  result.textContent = ''
-}
-
-function init() {
-  // DOM ELEMENTS
-  buttons = document.querySelectorAll('button.choice')
-  player1 = document.querySelector('.player1')
-  player2 = document.querySelector('.player2')
-  result = document.querySelector('.result')
-  resetBtn = document.querySelector('button.reset')
-
-  // EVENT LISTENERS
-  buttons.forEach(button => button.addEventListener('click', play))
-  resetBtn.addEventListener('click', reset)
+  init() {
+    this.buttons.forEach(button => button.addEventListener('click', this.play))
+    this.resetBtn.addEventListener('click', this.reset)
+  }
 }
 
-window.addEventListener('DOMContentLoaded', init)
+
+document.addEventListener('DOMContentLoaded', () => {
+  new RockPaperScissors()
+})
