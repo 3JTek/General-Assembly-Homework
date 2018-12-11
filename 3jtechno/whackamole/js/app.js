af9164a3e6af447cd353d81bee146936e862d5e3
3jtechno <3j.techno@gmail.com>
2018-12-11 22:31:50 +0000

    Creation of buttons with javascript

--- 3jtechno/whackamole/js/app.js
+++ 3jtechno/whackamole/js/app.js
@@ -5,87 +5,77 @@ document.addEventListener('DOMContentLoaded', () => {
   const startBtn = document.querySelector('.start-button')
 
   const gameDiv = document.querySelector('.game-page')
-  const gameHardDiv = document.querySelector('.hard-game')
+  const buttonsContainer = document.querySelector('.container')
   const scoreDisplay = document.querySelector('.score')
   const timerDisplay = document.querySelector('.timer')
-  const buttons = document.querySelectorAll('.game-buttons')
 
-  const gameOverDiv = document.querySelector('.game-over')
+  const gameOverDiv = document.querySelector('.game-over-page')
   const playAgainBtn = document.querySelector('.play-again-button')
 
   const imageUrl = 'url("https://i.pinimg.com/736x/de/5f/7b/de5f7b027fd2e73634898b40b6b8cc13--flower-beard-time-magazine.jpg")'
-  let score, timeRemaining, imagePosition, speed
+  let score, timeRemaining, imagePosition, speed, gridSize, timerId
+  const difficultyLevels = {
+    speed: [2000, 1500, 800],
+    gridSize: [12, 12, 24]
+  }
 
   function whackAFill(e) {
     e.target.style.backgroundImage = ''
     e.target.setAttribute('disabled', 'true')
     scoreDisplay.textContent = `Your score is ${score ++}`
   }
-
-
   function countDown() {
     timerDisplay.textContent = `Time Left: ${timeRemaining}`
     timeRemaining--
     if (timeRemaining >= 0) {
       setTimeout(countDown, 1000)
     } else {
+      clearInterval(timerId)
       gameDiv.classList.toggle('hidden')
       gameOverDiv.classList.toggle('hidden')
-
     }
   }
-
-  function startGame() {
-    setInterval(() => {
-      if(imagePosition){
+  function startGame(buttons) {
+    timerId = setInterval(() => {
+      console.log(imagePosition);
+      if(imagePosition || imagePosition === 0){
         buttons[imagePosition].style.backgroundImage = ''
         buttons[imagePosition].setAttribute('disabled', 'true')
       }
-      imagePosition = Math.floor(Math.random() * 12)
-
+      imagePosition = Math.floor(Math.random() * gridSize)+1
       buttons[imagePosition].style.backgroundImage = imageUrl
       buttons[imagePosition].removeAttribute('disabled')
-
     }, speed)
   }
 
-
   function init() {
     score = 0
-    timeRemaining = 100
+    timeRemaining = 1500
     scoreDisplay.textContent = `Your score is ${score}`
-
-    switch (difficultySelect.value) {
-      case '1':
-      speed = 2000
-      break
-      case '2':
-      speed = 1000
-      break
-      case '3':
-      speed = 800
-      buttons.forEach(button => {
-        button.classList.toggle('game-buttons-24')
-      })
-      gameHardDiv.classList.toggle('hidden')
-      break
+    speed = difficultyLevels.speed[difficultySelect.value - 1]
+    gridSize = difficultyLevels.gridSize[difficultySelect.value - 1]
+    for(let i = 0; i < gridSize; i++){
+      const newButton = document.createElement('button')
+      newButton.className = `game-buttons-${gridSize}`
+      newButton.setAttribute('disabled', 'true')
+      buttonsContainer.appendChild(newButton)
     }
-
+    const buttons = document.querySelectorAll('button')
+    buttons.forEach(button => {
+      button.addEventListener('click', whackAFill)
+    })
     welcomeDiv.classList.toggle('hidden')
     gameDiv.classList.toggle('hidden')
 
     countDown(timeRemaining)
-    startGame()
+    startGame(buttons)
   }
 
   function restart() {
-    welcomeDiv.classList.toggle('hidden')
     gameOverDiv.classList.toggle('hidden')
+    welcomeDiv.classList.toggle('hidden')
   }
 
-  buttons.forEach(button => {
-    button.addEventListener('click', whackAFill)
-  })
 
   startBtn.addEventListener('click', init)
   playAgainBtn.addEventListener('click', restart)
