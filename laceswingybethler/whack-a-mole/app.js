dc58d404358b8a01e9e7bf0a2a1f31d6a6de055a
laceswingybethler <beth.swingler@gmail.com>
2018-12-11 19:18:40 +0000

    Logic in place, rudimentary styling done.

---
+++ laceswingybethler/whack-a-mole/app.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded',() => init())
+//
+
+function init () {
+
+  //Get array of the buttons
+  const moleArray = document.querySelectorAll('div.mole')
+  let randomMole
+  let score = 0
+  let timer = 15
+  let randomMoleTimer
+
+  const scoreBox = document.querySelector('#score')
+  const timerScreen = document.querySelector('#timer')
+
+
+  function loop(){
+    if(randomMole !== undefined)randomMole.classList.remove('show')
+
+
+    const randomNumber = Math.floor(Math.random()*9)
+    const randomTime = Math.floor(Math.random()*1000)+250
+    // console.log(randomNumber)
+    randomMole = moleArray[randomNumber]
+    // console.log(randomMole)
+
+    randomMole.classList.add('show')
+
+    randomMoleTimer =  setTimeout(loop,randomTime)
+  }
+  loop()
+
+  //when the user clicks a mole initiate some functions
+  moleArray.forEach(mole => {
+    mole.addEventListener('click', e => {
+      score +=1
+      scoreBox.innerHTML = score
+      e.currentTarget.classList.remove('show')
+    })
+  })
+
+  function decrementTimer() {
+    const timerId = setInterval(() => {
+      timer--
+      timerScreen.innerHTML = timer
+      if (timer ===0){
+        clearInterval(randomMoleTimer)
+        clearInterval(timerId)
+      }
+    },1000)
+
+  }
+  decrementTimer()
+
+
+
+}
