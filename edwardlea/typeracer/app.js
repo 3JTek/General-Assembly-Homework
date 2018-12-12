1d92474a3cdef65dac79cb7dc84b6133c2954080
edwardlea <leaedward87@gmail.com>
2018-12-12 17:18:43 +0000

    Added timer and win and lose classes

--- edwardlea/typeracer/app.js
+++ edwardlea/typeracer/app.js
@@ -1,45 +1,71 @@
 $(() => {
 
+
+
   const $sampleTextBox = $('.sample-text-box')
   const $inputTextBox = $('.input-text-box')
   const $wordCountBox = $('.word-count-box')
   const $resetButton = $('.reset-button')
   const $wpmBox = $('.wpm-box')
+  const $countDown = $('.count-down')
   let startTime
   let index
   let wordCount
   let currentSample
+  let timerRemaining
+  let timerId
+  init()
 
+  function checkForWin() {
+    if(currentSample.length === index){
+      //Game Won!!!
+      $inputTextBox.addClass('win')
+      clearInterval(timerId)
+    }
 
+  }
 
+  $inputTextBox.on('keydown', gameLogic)
 
-
-  $inputTextBox.on('keydown', e => {
+  function gameLogic(e) {
     if(startTime === undefined) startTime = new Date()
-    console.log(e.key)
     if(e.key === currentSample.charAt(index)) {
       index++
+      checkForWin()
       incWordCount(e)
-
     } else {
       e.preventDefault()
     }
-  })
+  }
+
+  function timer(){
+    timerId = setTimeout(timer, 1000)
+    if(timerRemaining === 0) {
+      clearInterval(timerId)
+      $inputTextBox.addClass('lose')
+    }
+    $countDown.text(timerRemaining)
+    timerRemaining--
+  }
+
 
   function init(){
     index = 0
     wordCount = 0
+    $wordCountBox.text('0')
+    $wpmBox.text('0')
+    timerRemaining = 10
     startTime = undefined
     currentSample = samples[Math.floor(Math.random()*samples.length)]
     $sampleTextBox.text(currentSample)
     $inputTextBox.val('')
-    console.log($inputTextBox)
-
+    $inputTextBox.removeClass('win')
+    $inputTextBox.removeClass('lose')
+    clearInterval(timerId)
+    timer()
   }
 
   $resetButton.on('click', init)
-
-
 
   function incWordCount(e){
     if(e.key === ' '){
@@ -47,23 +73,7 @@ $(() => {
       const now = new Date()
       const diff = now - startTime
       const diffMins = diff/(1000*60)
-      console.log('Diff '+diff/1000)
-      console.log('WPM '+ wordCount /diffMins)
       $wpmBox.text(Math.round(wordCount /diffMins))
     }
-    console.log(wordCount)
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
