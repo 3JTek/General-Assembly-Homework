982febcbb8ff0dec73aa0293e48fc91cd1b07ece
SirAbbott <tom@iynk.com>
2018-12-12 21:27:21 +0000

    fixed resest button so timer also resets

--- SirAbbott/typeracer/js/app.js
+++ SirAbbott/typeracer/js/app.js
@@ -3,43 +3,84 @@ $(() => {
   const $textExample = $('.text-example')
   const $textarea = $('textarea')
   const $wordCount = $('.word-count')
-  const sampleText = samples[Math.floor(Math.random() * samples.length)]
   const $wpmDisplay = $('.wpm-display')
+  const $resetBtn = $('.reset-button')
+  const $startBtn = $('.start-button')
 
-  let count = 0, i =0
+  let count = 0
+  let i = 0
   let timePassed = 0
-  let wpm =0
-  function calculateWpm(count){
-    wpm = Math.round((count / timePassed) * 60)
-    if (wpm < 1000)$wpmDisplay.text(wpm)
+  let wpm = 0
+  let sampleText
+  let startTime
+  let gameOn
+
+
+  function init() {
+    gameOn = true
+    playGame()
   }
 
-  function startTimer(){
-    const timerId = setInterval(() => {
-      timePassed++
+  function startTimer() {
+    timePassed = 0
+    startTime = setInterval(() => {
       console.log(timePassed)
-    },1000)
+      timePassed++
+    }, 1000)
 
+  }
+
+  function calculateWpm() {
+    wpm = Math.round((count / timePassed) * 60)
+    if (wpm < 1000) $wpmDisplay.text(wpm)
+  }
+
+  function generateText() {
+    sampleText = samples[Math.floor(Math.random() * samples.length)]
+    $textExample.text(sampleText)
   }
 
 
   function increaseCount() {
-    count ++
+    count++
     $wordCount.text(count)
   }
 
-  $textExample.text(sampleText)
+  function playGame() {
+    if (gameOn) {
+      generateText()
+      $textarea.on('keydown', type)
+    }
+  }
 
-
-
-  $textarea.on('keydown', (e) => {
+  function type(e) {
     if (i === 1) startTimer()
-    if(e.key === sampleText.charAt(i)){
+    if (e.key === sampleText.charAt(i)) {
       i++
-      if (e.key === ' '){
+      if (e.key === ' ') {
         increaseCount()
-        calculateWpm(count)
+        calculateWpm()
       }
     } else e.preventDefault()
-  })
+  }
+
+  function resetCounter() {
+    count = 0
+    timePassed = 0
+    i = 0
+    wpm = 0
+    $textarea.val('')
+    $wpmDisplay.text('')
+    $textarea.off('keydown', type)
+  }
+
+  function resetGame() {
+    clearInterval(startTime)
+    resetCounter()
+    gameOn = false
+  }
+
+  $startBtn.on('click', init)
+  $resetBtn.on('click', resetGame)
+
 })
