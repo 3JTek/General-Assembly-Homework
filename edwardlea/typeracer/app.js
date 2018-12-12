1d4cb42c09a891d0c329f8f041afdc53e7f214d1
edwardlea <leaedward87@gmail.com>
2018-12-12 22:07:55 +0000

    css style added to page

--- edwardlea/typeracer/app.js
+++ edwardlea/typeracer/app.js
@@ -1,79 +1,120 @@
-$(() => {
+$(()=>init())
+//declare Variables
+let $sampleTextBox
+let $inputTextBox
+let $wordCountBox
+let $playButton
+let $wpmBox
+let $countDown
+let startTime
+let index
+let wordCount
+let currentSample
+let timerRemaining
+let timerId
+const gameLength = 60
 
+function init(){
+  //Get DOM elements
+  $sampleTextBox = $('.sample-text-box')
+  $inputTextBox = $('.input-text-box')
+  $wordCountBox = $('.word-count-box')
+  $playButton = $('.play-button')
+  $wpmBox = $('.wpm-box')
+  $countDown = $('.count-down')
 
-
-  const $sampleTextBox = $('.sample-text-box')
-  const $inputTextBox = $('.input-text-box')
-  const $wordCountBox = $('.word-count-box')
-  const $resetButton = $('.reset-button')
-  const $wpmBox = $('.wpm-box')
-  const $countDown = $('.count-down')
-  let startTime
-  let index
-  let wordCount
-  let currentSample
-  let timerRemaining
-  let timerId
-  init()
-
-  function checkForWin() {
-    if(currentSample.length === index){
-      //Game Won!!!
-      $inputTextBox.addClass('win')
-      clearInterval(timerId)
-    }
-
-  }
-
+  //Add event listeners
+  $playButton.on('click', playGame)
   $inputTextBox.on('keydown', gameLogic)
 
-  function gameLogic(e) {
-    if(startTime === undefined) startTime = new Date()
-    if(e.key === currentSample.charAt(index)) {
-      index++
-      checkForWin()
-      incWordCount(e)
-    } else {
-      e.preventDefault()
-    }
-  }
+  //Reset Variables
+  resetVariables()
+}
 
-  function timer(){
-    timerId = setTimeout(timer, 1000)
-    if(timerRemaining === 0) {
-      clearInterval(timerId)
-      $inputTextBox.addClass('lose')
-    }
-    $countDown.text(timerRemaining)
-    timerRemaining--
-  }
+function resetVariables(){
+  //RESET
+  index = 0
+  wordCount = 0
+  $wordCountBox.text(wordCount)
+  $wpmBox.text('0')
+  timerRemaining = gameLength
+  $countDown.text(timerRemaining)
+}
 
-
-  function init(){
-    index = 0
-    wordCount = 0
-    $wordCountBox.text('0')
-    $wpmBox.text('0')
-    timerRemaining = 10
-    startTime = undefined
-    currentSample = samples[Math.floor(Math.random()*samples.length)]
-    $sampleTextBox.text(currentSample)
-    $inputTextBox.val('')
-    $inputTextBox.removeClass('win')
-    $inputTextBox.removeClass('lose')
+function checkForWin() {
+  if(currentSample.length === index){
+    //Game Won!!!
+    $inputTextBox.addClass('win')
+    $sampleTextBox.addClass('win')
+    //Win set text to PLay Again
+    $playButton.text('Play Again')
     clearInterval(timerId)
-    timer()
   }
+}
 
-  $resetButton.on('click', init)
+function wpmCheck(){
+  // find time at last word completion
+  const now = new Date()
+  // calculate and display wpm
+  const diff = (now - startTime)/(1000*60)
+  $wpmBox.text(Math.round(wordCount /diff))
+}
 
-  function incWordCount(e){
-    if(e.key === ' '){
-      $wordCountBox.text(++wordCount)
-      const now = new Date()
-      const diff = now - startTime
-      const diffMins = diff/(1000*60)
-      $wpmBox.text(Math.round(wordCount /diffMins))
-    }
+
+function incWordCount(e){
+  // at stroke of space key
+  if(e.key === ' '){
+    // word count incremented and updated on screen
+    $wordCountBox.text(++wordCount)
+    // called wpmCheck to calculate current wpm
+    wpmCheck()
   }
-})
+}
+
+function gameLogic(e) {
+  if(startTime === undefined){
+    startTime = new Date()
+    //During game set text to Reset
+    $playButton.text('Reset')
+  }
+  // if current pressed key is equal to the character index increase index
+  if(e.key === currentSample.charAt(index)) {
+    index++
+    checkForWin()
+    incWordCount(e)
+  } else {
+    e.preventDefault()
+  }
+}
+
+function timer(){
+  timerId = setTimeout(timer, 1000)
+  if(timerRemaining === 0) {
+    clearInterval(timerId)
+    $inputTextBox.addClass('lose')
+    $sampleTextBox.addClass('lose')
+
+    //Lose set text to P{lay again}
+    $playButton.text('Play Again')
+  }
+  $countDown.text(timerRemaining)
+  timerRemaining--
+}
+
+function playGame(){
+  resetVariables()
+
+  //reset game conditions
+  startTime = undefined
+  currentSample = samples[Math.floor(Math.random()*samples.length)]
+  $sampleTextBox.text(currentSample)
+  $inputTextBox.val('')
+  $inputTextBox.removeClass('win')
+  $inputTextBox.removeClass('lose')
+  $sampleTextBox.removeClass('win')
+  $sampleTextBox.removeClass('lose')
+  clearInterval(timerId)
+  $inputTextBox.focus()
+  // start game timer
+  timer()
+}
