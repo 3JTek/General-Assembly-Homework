778e10e7a04c23264a27e4f19751e9a93663ed0c
3jtechno <3j.techno@gmail.com>
2018-12-12 09:32:13 +0000

    Add Class to create the grid

--- 3jtechno/whackamole/js/app.js
+++ 3jtechno/whackamole/js/app.js
@@ -1,82 +1,104 @@
-document.addEventListener('DOMContentLoaded', () => {
+let score, timeRemaining, imagePosition, speed, gridSize, timerId
+let difficultySelect, welcomeDiv, gameOverDiv, buttons, gridObj
+const difficultyLevels = {
+  speed: [2000, 1500, 800],
+  gridSize: [12, 12, 24]
+}
+const imageUrl = 'url("https://i.pinimg.com/736x/de/5f/7b/de5f7b027fd2e73634898b40b6b8cc13--flower-beard-time-magazine.jpg")'
 
-  const welcomeDiv = document.querySelector('.welcome-page')
-  const difficultySelect = document.querySelector('select')
-  const startBtn = document.querySelector('.start-button')
+class WhackFill{
+  constructor(){
+    this.gameDiv = document.querySelector('.game-page')
+    this.buttonsContainer = document.querySelector('.container')
+    this.scoreDisplay = document.querySelector('.score')
+    this.timerDisplay = document.querySelector('.timer')
 
-  const gameDiv = document.querySelector('.game-page')
-  const buttonsContainer = document.querySelector('.container')
-  const scoreDisplay = document.querySelector('.score')
-  const timerDisplay = document.querySelector('.timer')
+    this.countDown = this.countDown.bind(this)
+    this.targetReach = this.targetReach.bind(this)
 
-  const gameOverDiv = document.querySelector('.game-over-page')
-  const playAgainBtn = document.querySelector('.play-again-button')
-
-  const imageUrl = 'url("https://i.pinimg.com/736x/de/5f/7b/de5f7b027fd2e73634898b40b6b8cc13--flower-beard-time-magazine.jpg")'
-  let score, timeRemaining, imagePosition, speed, gridSize, timerId
-  const difficultyLevels = {
-    speed: [2000, 1500, 800],
-    gridSize: [12, 12, 24]
+    this.init()
   }
 
-  function whackAFill(e) {
-    e.target.style.backgroundImage = ''
-    e.target.setAttribute('disabled', 'true')
-    scoreDisplay.textContent = `Your score is ${score ++}`
-  }
-  function countDown() {
-    timerDisplay.textContent = `Time Left: ${timeRemaining}`
+  countDown() {
+    this.timerDisplay.textContent = `Time Left: ${timeRemaining}`
     timeRemaining--
     if (timeRemaining >= 0) {
-      setTimeout(countDown, 1000)
+      setTimeout(this.countDown, 1000)
     } else {
       clearInterval(timerId)
-      gameDiv.classList.toggle('hidden')
+      this.gameDiv.classList.toggle('hidden')
       gameOverDiv.classList.toggle('hidden')
     }
   }
-  function startGame(buttons) {
+  targetReach(e) {
+    buttons[imagePosition].classList.remove('showImage')
+    this.scoreDisplay.textContent = `Your score is ${score ++}`
+  }
+  startGame(buttons) {
     timerId = setInterval(() => {
-      console.log(imagePosition);
       if(imagePosition || imagePosition === 0){
-        buttons[imagePosition].style.backgroundImage = ''
-        buttons[imagePosition].setAttribute('disabled', 'true')
+        //Hide image
+        buttons[imagePosition].classList.remove('showImage')
       }
-      imagePosition = Math.floor(Math.random() * gridSize)+1
-      buttons[imagePosition].style.backgroundImage = imageUrl
-      buttons[imagePosition].removeAttribute('disabled')
+      //Select and show new image
+      imagePosition = Math.floor(Math.random() * gridSize)
+      buttons[imagePosition].classList.add('showImage')
     }, speed)
   }
-
-  function init() {
-    score = 0
-    timeRemaining = 1500
-    scoreDisplay.textContent = `Your score is ${score}`
-    speed = difficultyLevels.speed[difficultySelect.value - 1]
-    gridSize = difficultyLevels.gridSize[difficultySelect.value - 1]
+  createGrid(gridSize){
+    //Create a 4x3 or 6x4 depending of the difficulty
     for(let i = 0; i < gridSize; i++){
       const newButton = document.createElement('button')
       newButton.className = `game-buttons-${gridSize}`
-      newButton.setAttribute('disabled', 'true')
-      buttonsContainer.appendChild(newButton)
+      this.buttonsContainer.appendChild(newButton)
     }
-    const buttons = document.querySelectorAll('button')
+    buttons = document.querySelectorAll('.container button')
     buttons.forEach(button => {
-      button.addEventListener('click', whackAFill)
+      button.addEventListener('click', this.targetReach)
     })
-    welcomeDiv.classList.toggle('hidden')
-    gameDiv.classList.toggle('hidden')
-
-    countDown(timeRemaining)
-    startGame(buttons)
   }
+  init() {
+    //Configuring the setting of the game
+    score = 0
+    timeRemaining = 100
+    this.scoreDisplay.textContent = `Your score is ${score}`
+    speed = difficultyLevels.speed[difficultySelect.value - 1]
+    gridSize = difficultyLevels.gridSize[difficultySelect.value - 1]
+
+    this.createGrid(gridSize)
+
+    //Hide home page and show grid page
+    welcomeDiv.classList.toggle('hidden')
+    this.gameDiv.classList.toggle('hidden')
+
+    this.countDown(timeRemaining)
+    this.startGame(buttons)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  welcomeDiv = document.querySelector('.welcome-page')
+  difficultySelect = document.querySelector('select')
+  const startBtn = document.querySelector('.start-button')
+
+  gameOverDiv = document.querySelector('.game-over-page')
+  const playAgainBtn = document.querySelector('.play-again-button')
 
   function restart() {
+    //Delete the grid and display the home page and
     gameOverDiv.classList.toggle('hidden')
     welcomeDiv.classList.toggle('hidden')
+    buttons.forEach(button =>{
+      gridObj.buttonsContainer.removeChild(button)
+    })
   }
 
+  function newObject(){
+    gridObj = new WhackFill()
+  }
 
-  startBtn.addEventListener('click', init)
+  startBtn.addEventListener('click', newObject)
   playAgainBtn.addEventListener('click', restart)
+
 })
