3d131dee9a5d6c7ab704b6ab60690b7192ad3955
3jtechno <3j.techno@gmail.com>
2018-12-12 21:47:07 +0000

    Add restart pop-up window

--- 3jtechno/type-racer/app.js
+++ 3jtechno/type-racer/app.js
@@ -1,15 +1,10 @@
 $(() => {
-  const textToType = samples[Math.floor(Math.random() * samples.length)]
   const $textarea1 = $('textarea:first-of-type')
   const $textarea2 = $('textarea:last-of-type')
   const $wordCountDisplay = $('#wc')
   const $wordPerMinute = $('#wpm')
-  let startTime, currentTime, characterNb, wordNb, elapsedTime
-
-  //Initialise counters and text area
-  $wordCountDisplay.text('0')
-  $wordPerMinute.text('0')
-  $textarea1.text(textToType)
+  let textToType, startTime, currentTime, characterNb, wordNb, elapsedTime, wordPerMin, restart
+  const promptText = ' word per minuts, congratulations. Wanna try to beat this ? (y)'
 
   function updateCounters(){
     //Display nb of words completed so far
@@ -19,7 +14,8 @@ $(() => {
     currentTime = new Date().getTime()
     //Nb of words since the first letter has being typed
     elapsedTime = (currentTime - startTime)/1000/60
-    $wordPerMinute.text(Math.floor(wordNb / elapsedTime))
+    wordPerMin = Math.floor(wordNb / elapsedTime)
+    $wordPerMinute.text(wordPerMin)
   }
 
   function checkLetterTyped(e){
@@ -39,8 +35,27 @@ $(() => {
       //Prevent the textArea to display the letter typed
       e.preventDefault()
     }
+    //If it is the last character, prompt the user to play again
+    if(characterNb === $textarea1.val().length - 1){
+      //Hack, delay the prompt to allow the last character to be displayed
+      setTimeout(() => {
+        restart = prompt(wordPerMin + promptText) === 'y'? true:false
+        if(restart){
+          initialiseGame()
+        }
+      },500)
+    }
+  }
+  function initialiseGame(){
+    //Initialise counters and text area
+    $wordCountDisplay.text('0')
+    $wordPerMinute.text('0')
+    textToType = samples[Math.floor(Math.random() * samples.length)]
+    $textarea1.text(textToType)
+    $textarea2.val('')
   }
 
   $textarea2.on('keydown', checkLetterTyped)
 
+  initialiseGame()
 })
