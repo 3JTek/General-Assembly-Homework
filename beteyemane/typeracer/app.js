559493cc2d81a7ee6d9273cf928227feea66c22e
byemane <beteyemane@hotmail.co.uk>
2018-12-12 17:16:40 +0000

    Logic is done

---
+++ beteyemane/typeracer/app.js
@@ -0,0 +1,24 @@
+/* global randomText */
+
+$(() => {
+  const $textbox = $('.textbox')
+  const $type = $('.type')
+  let i = 0
+  // console.log($randomText)
+  // textbox LINK to sample texts array to show
+  function getRandom() {
+    return randomText[Math.floor(Math.random() * randomText.length)]
+  }
+  const textToType = getRandom()
+
+  $textbox.text(textToType)
+  // $randomText = $textbox
+  // format type box to only allow correct keys down
+  console.log(textToType)
+  $type.on('keydown', (e) => {
+    if (e.key === textToType.charAt(i)) i++
+    else e.preventDefault()
+  })
+
+
+})
