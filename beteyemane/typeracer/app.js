6fbc378d84c7ee93ad5eb95a83d20fefe7476787
byemane <beteyemane@hotmail.co.uk>
2018-12-12 20:32:34 +0000

    Word count added

--- beteyemane/typeracer/app.js
+++ beteyemane/typeracer/app.js
@@ -3,22 +3,39 @@
 $(() => {
   const $textbox = $('.textbox')
   const $type = $('.type')
+  const $wordCount = $('.word-count')
+  let $wpm = $('.wordsPerMinute')
   let i = 0
-  // console.log($randomText)
-  // textbox LINK to sample texts array to show
+  let counts = 0
+
   function getRandom() {
     return randomText[Math.floor(Math.random() * randomText.length)]
   }
   const textToType = getRandom()
 
   $textbox.text(textToType)
-  // $randomText = $textbox
-  // format type box to only allow correct keys down
-  console.log(textToType)
-  $type.on('keydown', (e) => {
-    if (e.key === textToType.charAt(i)) i++
-    else e.preventDefault()
-  })
 
+  function counter() {
+    $type.on('keydown', (e) => {
+      if (e.key === textToType.charAt(i)) {
+        i++
+        if (e.key === ' ') {
+          counts++
+          $wordCount.text(counts)
+        } else e.preventDefault()
+      }
+    })
+  }
+  counter()
+
+  // function calculateWpm() {
+  //   $wpm = Math.round(($wordCount/counts) * 60)
+  //   if($wordCount > 1){
+  //     return $wpm.text($wpm)
+  //   }else {
+  //     return ('')
+  //   }
+  // }
+  // console.log(calculateWpm())
 
 })
