84d8b8c9274135f5b7949f5e11421d78c152bf1a
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2018-12-12 18:04:50 +0000

    added reset function

--- typeracer/app.js
+++ typeracer/app.js
@@ -1,66 +1,65 @@
 /* global samples */
-$(() => {
+$(() =>  {
 
-  console.log('js loaded')
-
-  //find the first text textarea
-  //find the text area for user
-  //find the word Count
-  // find the WPM
   const $text = $('.text')
   const $input = $('.input')
   const $wordcount = $('.word-count')
   const $wpm = $('.wpm')
-  let wordCount = 0
-  let i = 0
+  const $resetBtn =$('.resetBtn')
 
-  //fill the text area with paragraphs from data.js - random
-  const textSamples = samples[Math.floor(Math.random() * samples.length)]
-  $text.text(textSamples)
-  //add event listener for key up, only type if input matches text
-  $input.on('keydown', (e) => {
-    if(e.key === textSamples.charAt(i)) {
-      i++
-      wordCounter(e)
-      $wordcount.text(`Your word count is ${wordCount}`)
-      calculateWpm()
-    } else {
-      e.preventDefault()
+  playGame()
+  function playGame(){
+    let wordCount = 0
+    let i = 0
+    const textSamples = samples[Math.floor(Math.random() * samples.length)]
+    $text.text(textSamples)
+
+    //add event listener for key up, only type if input matches text
+    $input.on('keydown', (e) => {
+      if(i === 1) startTimer()
+      if(e.key === textSamples.charAt(i)) {
+        i++
+        wordCounter(e)
+        $wordcount.text(`Your word count is ${wordCount}`)
+        calculateWpm()
+      } else {
+        e.preventDefault()
+      }
+    })
+
+    function wordCounter(e) {
+      if(e.key === ' ') wordCount++
+      return wordCount
     }
-  })
 
-  function wordCounter(e) {
-    if(e.key === ' ') wordCount++
-    return wordCount
+    //calculate wpm
+    // start timer on key down
+    //wpm = number of words counted at given time divided by current time * (60 * 1000)
+    let timePassed = 0
+    let wordsPerMinute = 0
+    function calculateWpm() {
+      wordsPerMinute = Math.round((wordCount/timePassed) * 60)
+      if(wordCount > 1){
+        return $wpm.text(`Your typing speed is ${wordsPerMinute} WPM`)
+      }else {
+        return ('')
+      }
+    }
+
+    function startTimer(){
+      const timerId = setInterval(() => {
+        timePassed++
+        console.log(timePassed)
+      }, 1000)
+    }
   }
-
-  //calculate wpm
-  // start timer on key down
-  //wpm = number of words counted at given time divided by current time * (60 * 1000)
-  let timePassed = 0
-  let wpm = 0
-  function calculateWpm() {
-    console.log(wordCount, timePassed)
-    wpm = Math.round((wordCount/timePassed) * 60)
-    console.log(wpm)
-    return wpm
+  function reset(e) {
+    $input.val('')
+    $wordcount.text('Your word count is')
+    $wpm.text('Your typing speed is WPM')
+    playGame()
   }
-
-
-  function startTimer(){
-    const timerId = setInterval(() => {
-      timePassed++
-      console.log(timePassed)
-    }, 1000)
-  }
-  startTimer()
-
-
-
-
-
-
-
+  $resetBtn.on('click', reset)
 
 
 })
