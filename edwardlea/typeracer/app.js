a688e2d594bb52ab01fd7e4fc0f5fecf62393f9c
edwardlea <leaedward87@gmail.com>
2018-12-12 15:54:04 +0000

    Completed game logic, (WPM and Word Count)

--- edwardlea/typeracer/app.js
+++ edwardlea/typeracer/app.js
@@ -2,21 +2,41 @@ $(() => {
 
   const $sampleTextBox = $('.sample-text-box')
   const $inputTextBox = $('.input-text-box')
+  const $wordCountBox = $('.word-count-box')
+  const $wpmBox = $('.wpm-box')
+  let startTime
   let index = 0
+  let wordCount = 0
   const currentSample = samples[Math.floor(Math.random()*samples.length)]
 
 
 
   $sampleTextBox.text(currentSample)
 
-  $inputTextBox.on('keypress', e => {
+  $inputTextBox.on('keydown', e => {
+    if(startTime === undefined) startTime = new Date()
     console.log(e.key)
     if(e.key === currentSample.charAt(index)) {
       index++
+      incWordCount(e)
+
     } else {
       e.preventDefault()
     }
   })
+
+  function incWordCount(e){
+    if(e.key === ' '){
+      $wordCountBox.text(++wordCount)
+      const now = new Date()
+      const diff = now - startTime
+      const diffMins = diff/(1000*60)
+      console.log('Diff '+diff/1000)
+      console.log('WPM '+ wordCount /diffMins)
+      $wpmBox.text(Math.round(wordCount /diffMins))
+    }
+    console.log(wordCount)
+  }
 
 
 
