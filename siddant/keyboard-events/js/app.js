bbe4edc7e2fcb5d811ff83ca04775a6b471edbf6
Siddant <siddantgurung@hotmail.co.uk>
2018-12-13 06:57:45 +0000

    code refactored, timerRemaning extra validation added

--- siddant/keyboard-events/js/app.js
+++ siddant/keyboard-events/js/app.js
@@ -10,14 +10,12 @@ $(() => {
   const $timeRemaningHolder = $('.timeRemaning')
   const $timeRemaning = $timeRemaningHolder.find('span')
 
-  let timerId
-  let textToType, text = ''
-  let i = 0, wordcount = 0, time = 0, wpm = 0, timeRemaining = 60, gameStart = false
+  let timerId, textToType, text = '', i = 0, wordcount = 0, time = 0, wpm = 0 
+  let timeRemaining = 60, gameStart = false
 
   function generateText() {
     textToType =  samples[Math.floor(Math.random() * samples.length)]
   }
-
 
   function calcwpm() {
     wpm = Math.round((wordcount/time) * 60)
@@ -26,22 +24,23 @@ $(() => {
     }
   }
 
-
   function endgame() {
     if(text === textToType || timeRemaining === 0) {
       clearInterval(timerId)
       $reset.show()
       gameStart = false
-      $example.css('border-color','red')
-
+      $example.css('border-color',' #719ECE')
     }
   }
 
   function startTime(){
     timerId = setInterval(() => {
       time++
-      timeRemaining--
+      if(timeRemaining> 0){
+        timeRemaining--
+      }
       endgame()
+      calcwpm()
       $timeRemaning.text(timeRemaining)
     }, 1000)
   }
@@ -72,27 +71,28 @@ $(() => {
     gameStart = true
   }
 
-
-  $textarea.on('keydown', (e) => {
-    if(e.key === textToType.charAt(i) && gameStart === true){
-      // if the character is a space, [the wordcount increases by one
-      if(textToType.charAt(i) === ' '){
-        wordcount++
-        $wordCountDisp.text(wordcount)
-        calcwpm()
+  function init(){
+    $textarea.on('keydown', (e) => {
+      if(e.key === textToType.charAt(i) && gameStart === true){
+        // if the character is a space, [the wordcount increases by one
+        if(textToType.charAt(i) === ' '){
+          wordcount++
+          $wordCountDisp.text(wordcount)
+        }
+        i++
+        text += e.key
+        $(e.target).css('border-color','green')
+      }else{
+        e.preventDefault()
+        $(e.target).css('border-color','red')
       }
-      i++
-      text += e.key
-      $(e.target).css('border-color','green')
-    }else{
-      e.preventDefault()
-      $(e.target).css('border-color','red')
-    }
-    if (i === 1 && gameStart === true) {
-      startTime()
-    }
-  })
+      if (i === 1 && gameStart === true) {
+        startTime()
+      }
+    })
+    $reset.on('click', reset)
+    gameSetup()
+  }
 
-  $reset.on('click', reset)
-  gameSetup()
+  init()
 })
