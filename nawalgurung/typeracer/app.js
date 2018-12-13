a828520ba8a4130995edf3547477209b2cebaa2a
Nawal00 <nawalgurung80@gmail.com>
2018-12-13 14:00:27 +0000

    typerace homework submission

--- nawalgurung/typeracer/app.js
+++ nawalgurung/typeracer/app.js
@@ -1,39 +1,54 @@
 /* global samples */
-
 $(() => {
-  const textToType = samples[Math.floor(Math.random() * samples.length)]
+  let textToType
   const $textarea1 = $('textarea:first-of-type')
   const $textarea2 = $('textarea:last-of-type')
   const $wordCountDisplay = $('#wc')
   const $wordPerMinute = $('#wpm')
+  const $resetBtn = $('.resetBtn')
   let startTime
   let currentTime
+  let i=0
 
   //Initialise counters and text area
   $wordCountDisplay.text('0')
   $wordPerMinute.text('0')
-  $textarea1.text(textToType)
 
-  let i=0
+  function random(){
+    textToType = samples[Math.floor(Math.random() * samples.length)]
+    $textarea1.text(textToType)
+  }
 
+  random()
+
+  function reset(){
+    // create new text on textarea1
+    random()
+    $textarea2.val('')
+    $wordCountDisplay.text('0')
+    $wordPerMinute.text('0')
+    i=0
+  }
+
+  $resetBtn.on('click', reset)
   $textarea2.on('keydown', (e) => {
-    //Good Letter
+  //Correct Letter
     if (e.key === textToType.charAt(i)){
-      //If first letter, grab timestamp
+    //If first letter is typed correctly, grab timestamp
       if(i === 0){
         startTime = new Date().getTime()
       }
       if(e.key === ' '){
-        //Display no of words completed so far
+      //Display no of words completed so far
         $wordCountDisplay.text($textarea2.val().split(' ').length)
         //Grab a timestamp
         currentTime = new Date().getTime()
-        //Nb of words since the first letter has being typed
+        //No of words since the first letter has being typed
         $wordPerMinute.text(Math.floor($textarea2.val().split(' ').length / ((currentTime - startTime)/1000/60)))
       }
       i++
-    }else{
-      //Wrong letter
+    } else{
+    //Wrong letter
       e.preventDefault()
     }
   })
