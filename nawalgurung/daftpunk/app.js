058950840fb22b3e123456794e67013d73b51bff
Nawal00 <nawalgurung80@gmail.com>
2018-12-18 11:39:58 +0000

    layout with flex

--- nawalgurung/daftpunk/app.js
+++ nawalgurung/daftpunk/app.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+  // --------- BUTTON SELECTORS ----------
   const musicButton = document.querySelectorAll('.btn')
   const sound = document.querySelector('#sound')
 
   //loop through each button
   musicButton.forEach(musicButton => {
-  //add addEventListener to and pass in the event arguement
+  //add addEventListener to and pass in the event argue
     musicButton.addEventListener('click', (e) => {
-      //when the button is click send the e.target.value and concat to sounds/....wav
+      //when the button is clicked send the e.target.value and concat to sounds/....wav
       //set src attr to sounds/'+e.target.value+'
       sound.setAttribute('src','sounds/'+e.target.value+'.wav')
       sound.play()
     })
-
   })
 
 })
