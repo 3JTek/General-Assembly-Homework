b0543f7cd72ca06a180518e3082e7e055bab8dd6
Dexter De Leon <dexdeleon@gmail.com>
2019-01-14 19:28:26 +0000

    Select on change returns flags from that region. Each flag div links to its country's wikipedia entry.

--- DexDeLeon/rest-countries-jquery/app.js
+++ DexDeLeon/rest-countries-jquery/app.js
@@ -1,22 +1,35 @@
 $(() => {
   const $countries = $('div.countries')
+  const $select = $('select.regions')
 
-  $.ajax({
-    method: 'GET',
-    url: 'https://restcountries.eu/rest/v2/all'
-  })
-    .then(countries => {
-      countries.forEach(country => {
-        $countries.append(`
-          <div>
-            <h3>${country.name}</h3>
-            <h4>${country.nativeName}</h4>
-            <div class="flagDiv">
-              <img src="${country.flag}" alt="Flag of ${country.name}">
-            </div>
-          </div>
-        `)
-      })
+
+  function getFlags(region){
+    $countries.empty()
+    $.ajax({
+      method: 'GET',
+      url: `https://restcountries.eu/rest/v2/${region}`
     })
+      .then(countries => {
+        countries.forEach(country => {
+          $countries.append(`
+            <a href="https://en.wikipedia.org/wiki/${country.name}" target="_blank">
+              <div>
+                <h3>${country.name}</h3>
+                <h4>${country.nativeName}</h4>
+                <div class="flagDiv">
+                  <img src="${country.flag}" alt="Flag of ${country.name}">
+                </div>
+              </div>
+            </a>
+          `)
+        })
+      })
+  }
+
+  getFlags('all')
+
+  $select.on('change', (e) => {
+    getFlags(e.target.value)
+  })
 
 })
