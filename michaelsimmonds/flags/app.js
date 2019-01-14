a61f8a2e7035110e37fb162dd210ccd5f1638828
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-14 17:57:35 +0000

    Can now select desired dropdown region

--- michaelsimmonds/flags/app.js
+++ michaelsimmonds/flags/app.js
@@ -1,27 +1,36 @@
 $(() => {
 
   const $flags = $('.flags')
+  const $regionForm = $('.region')
 
-  $.ajax({
-    method: 'GET',
-    url: 'https://restcountries.eu/rest/v2/all'
-  })
-    .then(flags => {
-      flags.forEach(flag => {
-        $flags.append(`
-          <div>
-            <h3>${flag.name}</h3>
-            <h5>${flag.nativeName}</h5>
-            <img src='${flag.flag}' alt='${flag.name}'>
-          </div>
-        `)
-      })
+  const suffix = 'all'
+
+  function getRegion(e) {
+    console.dir(e)
+    getFlags(`region/${e.target.value}`)
+  }
+
+  function getFlags(suffix) {
+    $flags.empty()
+    $.ajax({
+      method: 'GET',
+      url: `https://restcountries.eu/rest/v2/${suffix}`
     })
+      .then(flags => {
+        flags.forEach(flag => {
+          $flags.append(`
+            <div>
+              <h3>${flag.name}</h3>
+              <h5>${flag.nativeName}</h5>
+              <img src='${flag.flag}' alt='${flag.name}'>
+            </div>
+          `)
+        })
+      })
+  }
 
+  getFlags(suffix)
 
-
-
-
-
+  $regionForm.on('change', getRegion)
 
 })
