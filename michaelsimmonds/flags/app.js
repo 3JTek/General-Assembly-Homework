a68a492d1dbd9edcf4485812bc5cce077196c1ad
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-14 16:50:40 +0000

    Set the display with the flags

---
+++ michaelsimmonds/flags/app.js
@@ -0,0 +1,27 @@
+$(() => {
+
+  const $flags = $('.flags')
+
+  $.ajax({
+    method: 'GET',
+    url: 'https://restcountries.eu/rest/v2/all'
+  })
+    .then(flags => {
+      flags.forEach(flag => {
+        $flags.append(`
+          <div>
+            <h4>${flag.name}</h4>
+            <h4>${flag.nativeName}</h4>
+            <img src='${flag.flag}' alt='${flag.name}'>
+          </div>
+        `)
+      })
+    })
+
+
+
+
+
+
+
+})
