41b3365406ba96556c395c5310c2e5d61248064d
SirAbbott <tom@iynk.com>
2019-01-14 17:25:17 +0000

    flags and names added

--- SirAbbott/rest-countries/app.js
+++ SirAbbott/rest-countries/app.js
@@ -1,3 +1,26 @@
 $(() => {
+  const $flagImages = $('.flags')
+  const $dropDown = $('.regions')
+
+  $.ajax({
+    method: 'GET',
+    url: 'https://restcountries.eu/rest/v2/all'
+  }).then(flags => {
+    flags.forEach(flag => {
+      $flagImages.append(`
+      <div>
+      <h2>${flag.name}</h4>
+      <h4>${flag.nativeName}></h4>
+      <img src="${flag.flag}" alt="${flag.name}" />
+      </div>`)
+    })
+  })
+
+  $.ajax({
+    method: 'GET',
+    url: 'https://restcountries.eu/rest/v2/all'
+  })
+
+
 
 })
