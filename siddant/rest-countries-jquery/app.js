6e73ac137bf38d9ba46b7615cc15630361694736
Siddant <siddantgurung@hotmail.co.uk>
2019-01-14 17:01:32 +0000

    starting on bonus

--- siddant/rest-countries-jquery/app.js
+++ siddant/rest-countries-jquery/app.js
@@ -1,3 +1,35 @@
 $(() => {
-  console.log('here')
+  const $countries = $('.countries')
+  const $filterCountries = $('form select')
+
+
+  function getCountries(variable){
+    $countries.empty()
+    $.ajax({
+      method: 'GET',
+      url: `https://restcountries.eu/rest/v2/${variable}`
+    })
+      .then(countries =>{
+        countries.forEach(country =>{
+          $countries.append(`
+          <div>
+          <h2>
+          ${country.name}
+          </h2>
+          <small>${country.nativeName}</small>
+          <img src="${country.flag}" atl="${country.name}" />
+          </div>
+
+          `)
+        })
+      })
+  }
+
+
+  getCountries('all')
+
+  $filterCountries.change(e => getCountries(e.target.value))
+
+
+  //https://restcountries.eu/rest/v2/all
 })
