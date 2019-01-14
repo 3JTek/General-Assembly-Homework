4185f58cf2a88c3ac76d38f235bd0a2a3dea36c5
edwardlea <leaedward87@gmail.com>
2019-01-14 17:59:51 +0000

    search functionality added

--- edwardlea/ajax-homework/app.js
+++ edwardlea/ajax-homework/app.js
@@ -1,28 +1,47 @@
 $(() => {
   const $flags = $('.flags')
+  const $dropdown = $('#drop-down')
+  const $textArea = $('#text-search')
 
-  $.ajax({
-    method: 'GET',
-    url: 'https://restcountries.eu/rest/v2/all'
+  function getCountries(type){
+    $flags.empty()
+    $.ajax({
+      method: 'GET',
+      url: `https://restcountries.eu/rest/v2/${type}`
+    }).then(
+      countries => {
+        countries.forEach(country =>{
+          $flags.append(`
+            <div>
+            <h4> ${country.name}</h4>
+            <h6> ${country.nativeName} </h6>
+            <img src="${country.flag}" alt="${country.name}"/>
+            </div>
+            `)
+        })
+      }
+    )
+  }
 
-  }).then(
-    countries => {
-      console.log(countries)
-      countries.forEach(country =>{
-        $flags.append(`
-          <div>
-          <h4> ${country.name}</h4>
-          <h6> ${country.nativeName} </h6>
-          <img src="${country.flag}" alt="${country.name}"/>
-          </div>
-          `)
-      })
-
-
+  function getContinentCountries(e){
+    if(e.target.value === 'all'){
+      getCountries('all')
     }
-  )
+    getCountries(`region/${e.target.value}`)
+  }
 
+  function getSearchCountries(e){
+    if(e.target.value === ''){
+      getCountries('all')
+    }
+    e.target.value.toLowerCase()
+    getCountries(`name/${e.target.value.toLowerCase()}`)
+  }
 
+  getCountries('all')
+
+  $dropdown.on('change', getContinentCountries)
+  $textArea.on('input', getSearchCountries)
 
 
 })
