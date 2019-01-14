17582ce31231c0693004591db3c2e56ed74c6335
Gessy90 <gessicasantoro90@hotmail.it>
2019-01-14 18:24:48 +0000

    final final

---
+++ Gessy90/rest-countries-jquery/app.js
@@ -0,0 +1,24 @@
+$(() => {
+  const $container =$('.container')
+  const $form =$('#dropdown')
+
+  function getCountries(variable){
+    console.log(variable)
+    $.ajax({
+      method: 'GET',
+      url: `https://restcountries.eu/rest/v2/${variable}`
+    })
+      .then(container =>{
+        container.forEach(container => {
+          $container.append(`
+        <div>
+          <h4>${container.name}</h4>
+          <h5>${container.nativeName}</h5>
+          <img src="${container.flag}" alt="${container.name}"/>
+        </div>
+        `)
+        })
+      })
+  }
+  $form.change(e => getCountries(e.target.value))
+})
