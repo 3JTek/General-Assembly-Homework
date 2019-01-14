ad07d62208df7f4902aefa916c8cd1c1a21b1d7d
SirAbbott <tom@iynk.com>
2019-01-14 18:06:27 +0000

    filter by dropdown done

--- SirAbbott/rest-countries/app.js
+++ SirAbbott/rest-countries/app.js
@@ -1,26 +1,48 @@
 $(() => {
-  const $flagImages = $('.flags')
+  const $flags = $('.flags')
   const $dropDown = $('.regions')
+  const $search = $('input')
 
-  $.ajax({
-    method: 'GET',
-    url: 'https://restcountries.eu/rest/v2/all'
-  }).then(flags => {
+  function getFlags() {
+    $flags.empty()
+    $.ajax({
+      method: 'GET',
+      url: 'https://restcountries.eu/rest/v2/all'
+    }).then(flags => appendTodivs(flags))
+  }
+
+  function appendTodivs(flags) {
     flags.forEach(flag => {
-      $flagImages.append(`
+      $flags.append(`
       <div>
       <h2>${flag.name}</h4>
       <h4>${flag.nativeName}></h4>
-      <img src="${flag.flag}" alt="${flag.name}" />
+      <img src="${flag.flag}" alt="${flag.name}"
       </div>`)
     })
-  })
+  }
 
-  $.ajax({
-    method: 'GET',
-    url: 'https://restcountries.eu/rest/v2/all'
-  })
+  function filterByRegion(e) {
+    let regionalURL = `https://restcountries.eu/rest/v2/region/${e.target.value}`
+    if (e.target.value === 'All') {
+      regionalURL = 'https://restcountries.eu/rest/v2/all'
+    }
+    $flags.empty()
+    $.ajax({
+      method: 'GET',
+      url: regionalURL
+    }).then(flags => appendTodivs(flags))
+  }
 
+  function searchFilter(e) {
+    $.ajax({
+      method: 'GET',
+      url: `https://restcountries.eu/rest/v2/name/${e.target.value}`
+    }).then(flags => appendTodivs(flags))
+  }
 
+  getFlags()
+  $dropDown.on('change', e => filterByRegion(e))
+  $search.on('keydown', e => searchFilter(e))
 
 })
