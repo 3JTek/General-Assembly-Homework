a12687184c9ac2637efe08bf7c10be4b779d1c2d
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-01-14 17:47:08 +0000

    drop down is working and filtering by country

--- charlottemorgan/rest-countries-homework/rest-countries-jquery/app.js
+++ charlottemorgan/rest-countries-homework/rest-countries-jquery/app.js
@@ -22,4 +22,28 @@ $(() => {
     })
 
 
+  function flagByRegion(e) {
+    $flags.empty()
+    $.ajax({
+      method: 'GET',
+      url: `https://restcountries.eu/rest/v2/region/${e.target.value}`
+    })
+      .then(flags => {
+        console.log(flags)
+        flags.forEach(flag => {
+          $flags.append(`
+            <div>
+            <h2>${flag.name}</h2>
+            <h4>${flag.nativeName}</h4>
+            <div class="imageDiv">
+            <img src="${flag.flag}" alt="${flag.name}" />
+            </div>
+            </div>
+            `)
+        })
+      })
+  }
+
+  $dropDown.on('change', flagByRegion)
+
 })
