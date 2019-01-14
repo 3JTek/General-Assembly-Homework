c78bae3bf3a8f738f681a39cabb49839cea6c0b3
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-01-14 18:31:55 +0000

    searh function is now working

--- charlottemorgan/rest-countries-homework/rest-countries-jquery/app.js
+++ charlottemorgan/rest-countries-homework/rest-countries-jquery/app.js
@@ -2,6 +2,7 @@ $(() => {
 
   const $flags = $('.flags')
   const $dropDown = $('.regions')
+  const $search = $('.input')
 
   $.ajax({
     method: 'GET',
@@ -48,6 +49,33 @@ $(() => {
       })
   }
 
+  function searchBar(e){
+    $flags.empty()
+    let searchURL = `https://restcountries.eu/rest/v2/name/${e.target.value}`
+    if(e.target.value === '') {
+      searchURL = 'https://restcountries.eu/rest/v2/all'
+    }
+    $.ajax({
+      method: 'GET',
+      url: searchURL
+    })
+      .then(flags => {
+        flags.forEach(flag => {
+          $flags.append(`
+          <div>
+          <h2>${flag.name}</h2>
+          <h4>${flag.nativeName}</h4>
+          <div class="imageDiv">
+          <img src="${flag.flag}" alt="${flag.name}" />
+          </div>
+          </div>
+          `)
+        })
+      })
+
+  }
+
   $dropDown.on('change', flagByRegion)
+  $search.on('keyup', searchBar)
 
 })
