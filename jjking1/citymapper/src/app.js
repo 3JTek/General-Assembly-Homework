bc7725ee5be40cc1ecce83932935ae341c4b1837
joshuaking06 <joshua.king0692@gmail.com>
2019-01-15 17:35:51 +0000

    finished

--- jjking1/citymapper/src/app.js
+++ jjking1/citymapper/src/app.js
@@ -1,4 +1,27 @@
 import $ from 'jquery'
 import './style.scss'
 
-alert('hello')
+const $lineDisplay = $('.line-display')
+
+function getLines(){
+  $lineDisplay.empty()
+  console.log('refreshing')
+  $.ajax({
+        method: 'GET',
+        url: 'https://api.tfl.gov.uk/line/mode/tube/status',
+      })
+        .then(data => {
+          data.forEach(line => {
+            $lineDisplay.append(`
+                <div class="${line.id}"><h2> ${line.name}</h2>
+                <p class="status">${line.lineStatuses[0].statusSeverityDescription}</p>
+                 </div>
+              `)
+          })
+        })
+    setTimeout(getLines, 300000)
+  }
+
+
+
+getLines()
