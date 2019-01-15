a03b0426e7efb768ddb68fec4944ce32e0b5fbb2
edwardlea <leaedward87@gmail.com>
2019-01-15 20:43:06 +0000

    css styling added to page

--- edwardlea/citymapper-homework/src/app.js
+++ edwardlea/citymapper-homework/src/app.js
@@ -6,28 +6,25 @@ let delayedLines = []
 const $lineStatus = $('.line-status')
 const $lastUpdate = $('#last-update')
 const $delayedLines = $('.delayed-lines')
+const $delayedLinesTitle = $('.delayed-lines-title')
+const $allLinesTitle = $('.all-lines-title')
 
 function getLineStatus(){
   $.get('https://api.tfl.gov.uk/line/mode/tube/status')
     .then(data => {
       tubeData = data
-      console.log(tubeData)
       filterDelayed()
       displayLines()
-      console.dir(new Date().toLocaleDateString(), new Date().toLocaleTimeString())
       $lastUpdate.text(`${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}`)
     })
-  // setTimeout(getLineStatus, 300000)
+  setTimeout(getLineStatus, 300000)
 }
 
 function filterDelayed(){
   delayedLines = tubeData.filter(line => {
     return line.lineStatuses[0].statusSeverityDescription === 'Minor Delays' || line.lineStatuses[0].statusSeverityDescription === 'Major Delays'
   })
-  console.log(delayedLines)
 }
-
-
 
 function displayLines() {
   $lineStatus.empty()
@@ -40,15 +37,22 @@ function displayLines() {
       </div>
       `)
   })
-  delayedLines.forEach(line =>{
-    $delayedLines.append(`
-      <div class=${line.id}>
-      <h4>${line.name}</h4>
-      <h6> ${line.lineStatuses[0].statusSeverityDescription}</h6>
-      </div>
+  if(delayedLines.length === 0){
+    $delayedLinesTitle.addClass('no-delays')
+    $allLinesTitle.text('A good service is on all lines')
+  } else {
+    $delayedLinesTitle.removeClass('no-delays')
+    $allLinesTitle.text('All lines')
+    delayedLines.forEach(line =>{
+      $delayedLines.append(`
+        <div class=${line.id}>
+        <h4>${line.name}</h4>
+        <h6> ${line.lineStatuses[0].statusSeverityDescription}</h6>
+        </div>
         `
-    )
-  })
+      )
+    })
+  }
 }
 
 getLineStatus()
