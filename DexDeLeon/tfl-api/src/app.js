849efa547f7076b211c80f7a00eeebeef923691c
Dexter De Leon <dexdeleon@gmail.com>
2019-01-15 21:36:25 +0000

    Refactored code into two smaller functions. The main getData function calls itself again after 5 minutes.

--- DexDeLeon/tfl-api/src/app.js
+++ DexDeLeon/tfl-api/src/app.js
@@ -2,16 +2,32 @@ import $ from 'jquery'
 import './style.scss'
 const $container = $('.container')
 
-
-$.get('https://api.tfl.gov.uk/line/mode/tube/status')
-  .then(lines => {
-    lines.forEach(line => {
-      const $line = $(`
-          <div id="${line.id}">
-            <h2>${line.name}</h2>
-            <p>${line.lineStatuses[0].statusSeverityDescription}</p>
-          </div>
-        `)
-      $container.append($line)
+//Gets new data from the TFL API
+function getData(){
+  $.get('https://api.tfl.gov.uk/line/mode/tube/status')
+    .then(lines => {
+      updateDisplay(lines)
     })
+  console.log('Updated.')
+  // Function calls itself again after 5 minutes
+  setTimeout(getData, 300000)
+}
+
+//Updates the DOM with new data from the TFL API
+function updateDisplay(lines){
+  //Empty existing divs
+  $container.empty()
+  //For each tube line...
+  lines.forEach(line => {
+    // ...create a DOM element with the appropriate data
+    const $line = $(`
+      <div id="${line.id}">
+      <h2>${line.name}</h2>
+      <p>${line.lineStatuses[0].statusSeverityDescription}</p>
+      </div>
+      `)
+    $container.append($line)
   })
+}
+
+getData()
