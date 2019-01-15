8afcac7ecb7f18ae16458d859e8178db917b6cbe
Dexter De Leon <dexdeleon@gmail.com>
2019-01-15 21:15:49 +0000

    Lines and service status successfully displayed.

--- DexDeLeon/tfl-api/src/app.js
+++ DexDeLeon/tfl-api/src/app.js
@@ -1,2 +1,17 @@
 import $ from 'jquery'
 import './style.scss'
+const $container = $('.container')
+
+
+$.get('https://api.tfl.gov.uk/line/mode/tube/status')
+  .then(lines => {
+    lines.forEach(line => {
+      const $line = $(`
+          <div id="${line.id}">
+            <h2>${line.name}</h2>
+            <p>${line.lineStatuses[0].statusSeverityDescription}</p>
+          </div>
+        `)
+      $container.append($line)
+    })
+  })
