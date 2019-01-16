e900ba101a31856294ad338cb9e5c31a78661739
seb yeates <yeatessa@gmail.com>
2019-01-16 16:31:31 +0000

    coffee homework

---
+++ syeates/react-coffee-assembly/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <p>&copy; General Assembly 2017</p>
+      </footer>
+    )
+  }
+}
+
+export default Footer
