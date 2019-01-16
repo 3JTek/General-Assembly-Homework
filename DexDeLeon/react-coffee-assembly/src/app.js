08808238fa4d08bdb905a036526200eee1fd944b
Dexter De Leon <dexdeleon@gmail.com>
2019-01-16 20:42:50 +0000

    Footer displayed.

--- DexDeLeon/react-coffee-assembly/src/app.js
+++ DexDeLeon/react-coffee-assembly/src/app.js
@@ -10,6 +10,7 @@ import Hero from './components/Hero'
 import Gallery from './components/Gallery'
 import Menu from './components/Menu'
 import About from './components/About'
+import Footer from './components/Footer'
 
 class App extends React.Component {
 
@@ -61,8 +62,9 @@ class App extends React.Component {
           <Hero />
           <Gallery gallery={this.state.gallery} />
           <Menu menus={this.state.menus} />
-          <About about={this.state.about}/>
+          <About about={this.state.about} />
         </main>
+        <Footer footer={this.state.footer} />
       </div>
     )
   }
