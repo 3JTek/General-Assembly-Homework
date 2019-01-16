16898f4ce9e74f8dcf87f5f5c861392dd7436e9d
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-01-16 17:43:39 +0000

    all working

--- charlottemorgan/react-coffee-assembly/src/app.js
+++ charlottemorgan/react-coffee-assembly/src/app.js
@@ -10,6 +10,7 @@ import Hero from './components/Hero'
 import Gallery from './components/Gallery'
 import Menu from './components/Menu'
 import About from './components/About'
+import Footer from './components/Footer'
 
 class App extends React.Component {
 
@@ -49,6 +50,7 @@ class App extends React.Component {
         <Gallery />
         <Menu menus={this.state.menus} />
         <About about={this.state.about}/>
+        <Footer footer={this.state.footer}/>
       </div>
     )
   }
