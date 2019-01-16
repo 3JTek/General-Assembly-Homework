3619a42b53de8141e27e445dc9c18669e81203dd
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-01-16 21:32:23 +0000

    formatting

--- charlottemorgan/react-coffee-assembly/src/components/About.js
+++ charlottemorgan/react-coffee-assembly/src/components/About.js
@@ -13,7 +13,7 @@ const About = (props) => {
         </div>
       </div>
     </section>
-
   )
 }
+
 export default About
--- charlottemorgan/react-coffee-assembly/src/components/Nav.js
+++ charlottemorgan/react-coffee-assembly/src/components/Nav.js
@@ -22,8 +22,4 @@ class Nav extends React.Component {
   }
 }
 
-
-
-
-
 export default Nav
