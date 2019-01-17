43ba6eedbea08ee76c3a73ae6999e74ea0ccc17e
byemane <beteyemane@hotmail.co.uk>
2019-01-17 18:48:14 +0000

    players choice and comp choice done

--- beteyemane/react-rock-paper-scissors/src/app.js
+++ beteyemane/react-rock-paper-scissors/src/app.js
@@ -10,7 +10,10 @@ class App extends React.Component {
         rock: 'scissors',
         paper: 'rock',
         scissors: 'paper'
-      }
+      },
+
+      computerChoice: '',
+      playersChoice: ''
     }
 
     this.handleSubmit = this.handleSubmit.bind(this)
@@ -20,7 +23,9 @@ class App extends React.Component {
   }
 
   handleSubmit(e) {
-    const random = [Math.floor(Math.random() * 3)]
+    const random = this.choices[Math.floor(Math.random() * 3)]
+    this.setState({computerChoice: random})
+    this.setState({playersChoice: e.target.value})
     e.preventDefault()
     console.log(e.target.value)
     console.log(random)
@@ -28,7 +33,11 @@ class App extends React.Component {
 
   render() {
     return (
-      <Buttons handleSubmit={this.handleSubmit}/>
+      <div>
+        <Buttons handleSubmit={this.handleSubmit}/>
+        <h1>Players Choice: {this.state.playersChoice}</h1>
+        <h1>Computers Choice: {this.state.computerChoice}</h1>
+      </div>
     )
   }
 }
