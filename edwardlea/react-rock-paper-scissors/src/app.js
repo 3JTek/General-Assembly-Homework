896b6942aff5f1c04c087c484100c922d6add02e
edwardlea <leaedward87@gmail.com>
2019-01-17 17:56:58 +0000

    click button logic added

--- edwardlea/react-rock-paper-scissors/src/app.js
+++ edwardlea/react-rock-paper-scissors/src/app.js
@@ -7,41 +7,49 @@ class App extends React.Component {
     super()
 
     this.state = {
-      computerChoice: '',
-      playerChoice: '',
+      computerChoice: null,
+      playerChoice: null,
+      winner: null,
       options: [
         'rock',
         'paper',
         'scissors'
-      ]
-
+      ],
+      winConditions: {
+        rock: 'scissors',
+        paper: 'rock',
+        scissors: 'paper'
+      }
     }
-
 
     this.computerChoice = this.computerChoice.bind(this)
     this.handleChoice = this.handleChoice.bind(this)
-
-
-
-
+    this.winnerLogic = this.winnerLogic.bind(this)
   }
 
   handleChoice(e) {
-    console.log(e.target.value)
-    const computerChoice = this.computerChoice()
-    
+    this.setState({playerChoice: e.target.value, computerChoice: this.computerChoice()}, () => {
+      console.log(this.state.playerChoice)
+      console.log(this.state.computerChoice)
+      console.log(this.winnerLogic())
 
+    })
   }
 
   computerChoice(){
-    console.log(this.state.options[Math.floor(Math.random() * (this.state.options.length))])
     return this.state.options[Math.floor(Math.random() * (this.state.options.length))]
 
   }
 
-  winnerLogic(){
 
+  winnerLogic() {
+    if(this.state.computerChoice){
+      if(this.state.playerChoice === this.state.computerChoice) return 'Tie'
+      if(this.state.winConditions[this.state.playerChoice] === this.state.computerChoice) return 'You win'
+      return 'You lose'
+    }
   }
+
 
 
 
@@ -49,12 +57,13 @@ class App extends React.Component {
     return(
       <div>
         <div>
-          <h1> Rock, Paper, Scicsors</h1>
+          <h1> Rock, Paper, Scissors</h1>
+          <h2></h2>
         </div>
         <div>
-          <button value="Rock" onClick={this.handleChoice}>Rock</button>
-          <button value="Paper" onClick={this.handleChoice}>Paper</button>
-          <button value="Sciscors" onClick={this.handleChoice}>Sciscors</button>
+          <button value="rock" onClick={this.handleChoice}>Rock</button>
+          <button value="paper" onClick={this.handleChoice}>Paper</button>
+          <button value="scissors" onClick={this.handleChoice}>Scissors</button>
 
         </div>
       </div>
