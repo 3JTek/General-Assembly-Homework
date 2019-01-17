8b549120ffb7c451be980cdcac74820771000a57
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-01-17 17:11:29 +0000

    buttons are clicking and computer is making a random choice

--- charlottemorgan/react-rock-paper-scissors/src/app.js
+++ charlottemorgan/react-rock-paper-scissors/src/app.js
@@ -9,27 +9,48 @@ class App extends React.Component {
     super()
 
     this.state = {
-
+      computerChoice: '',
+      playerChoice: '',
+      winner: '',
+      options: [
+        'rock',
+        'paper',
+        'scissors'
+      ]
     }
+
+    this.handleChoice = this.handleChoice.bind(this)
+
+
   }
 
-render() {
-  return(
-    <div>
-    <button>Rock</button>
-    <button>Paper</button>
-    <button>Scissors</button>
+  handleChoice(e){
+    console.log(e.target.value)
+    this.computerChoice()
+  }
+
+  computerChoice(){
+    console.log(this.state.options[Math.floor(Math.random() * (this.state.options.length))])
+  }
+
+
+  render() {
+    return(
+      <div>
+        <div>
+          <h1>Rock, Paper, Scissors</h1>
+        </div>
+        <div>
+          <button value="rock" onClick={this.handleChoice}>Rock</button>
+          <button value="promaper" onClick={this.handleChoice}>Paper</button>
+          <button value="scissors" onClick={this.handleChoice}>Scissors</button>
+        </div>
+      </div>
 
 
 
-
-
-    </div>
-
-
-
-  )
-}
+    )
+  }
 
 }
 
