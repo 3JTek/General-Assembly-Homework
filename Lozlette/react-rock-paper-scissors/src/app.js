f4f11ae6f76fc25020c12dba965a7627896ceab0
Lauren Bell <laurengilbert0@gmail.com>
2019-01-17 23:29:44 +0000

    a few changes made but game logic not working

--- Lozlette/react-rock-paper-scissors/src/app.js
+++ Lozlette/react-rock-paper-scissors/src/app.js
@@ -42,6 +42,7 @@ class App extends React.Component {
     } else {
       this.winner = 'You lose'
     }
+    console.log('function working')
   }
 
   makeChoice() {
@@ -51,7 +52,7 @@ class App extends React.Component {
   clickButton(){
     this.player1Choice = 'Rock'
     console.log('rock')
-    console.log(this.findWinner())
+    this.findWinner()
   }
 
   clickButton2(){
