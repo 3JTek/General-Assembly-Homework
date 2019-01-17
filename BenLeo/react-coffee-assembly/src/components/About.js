08cbe58a2806d11433ff1aa0fe2d940263992d23
Ben Lander <ben.l.lander@googlemail.com>
2019-01-17 15:53:30 +0000

    Admin commit of older projects

--- BenLeo/react-coffee-assembly/src/components/About.js
+++ BenLeo/react-coffee-assembly/src/components/About.js
@@ -7,7 +7,12 @@ const About = (props) => {
         <div className="row">
           <div className="col-sm-12">
             <h2>About</h2>
-            {props.text.map((content, index) => <p key={index}>{content}</p>)}
+            {props.text.map((line, index) =>
+              <div key={index}>
+                <h2>${line.name}</h2>
+                <h4>${line.lineStatuses[0].statusSeverityDescription}</h4>
+              </div>
+            )}
           </div>
         </div>
       </div>
