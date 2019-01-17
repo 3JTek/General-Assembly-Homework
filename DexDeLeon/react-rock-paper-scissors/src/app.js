5430d650319988b114af9fcd9c725aa7f4a1cdec
Dexter De Leon <dexdeleon@gmail.com>
2019-01-17 23:14:06 +0000

    Customised messages displayed depending on winner or tied choices.

--- DexDeLeon/react-rock-paper-scissors/src/app.js
+++ DexDeLeon/react-rock-paper-scissors/src/app.js
@@ -10,28 +10,53 @@ import Choice from './components/Choice'
 class App extends React.Component{
   constructor(){
     super()
-
+    const sprEf = '\nIt\'s super effective!'
     this.state = {
       options: [
         {
           name: 'rock',
-          beats: ['scissors', 'lizard']
+          beats: ['scissors', 'lizard'],
+          action: [
+            `The rock used body slam on the scissors.${sprEf}`,
+            `The rock used crush on the lizard.${sprEf}`,
+            'The rocks smash against each other, but nothing happens...'
+          ]
         },
         {
           name: 'paper',
-          beats: ['rock', 'spock']
+          beats: ['rock', 'spock'],
+          action: [
+            `The paper used cover on the rock.${sprEf}`,
+            `The paper used disprove on Spock.${sprEf}`,
+            'The two sheets of paper lie awkwardly on top of each other, but nothing happens...'
+          ]
         },
         {
           name: 'scissors',
-          beats: ['paper', 'lizard']
+          beats: ['paper', 'lizard'],
+          action: [
+            `The scissors used cut on the paper.${sprEf}`,
+            `The scissors used decapitate on the lizard.${sprEf}`,
+            'The scissors snip angrily at each other, but nothing happens...'
+          ]
         },
         {
           name: 'lizard',
-          beats: ['paper', 'spock']
+          beats: ['paper', 'spock'],
+          action: [
+            `The lizard used devour on the paper.${sprEf}`,
+            `The lizard used poison on Spock.${sprEf}`,
+            'The lizards seem cool with each other; I think they might be friends now...'
+          ]
         },
         {
           name: 'spock',
-          beats: ['rock', 'scissors']
+          beats: ['rock', 'scissors'],
+          action: [
+            `Spock used disintegrate on the rock.${sprEf}`,
+            `Spock used smash on the scissors.${sprEf}`,
+            'The two Spocks exchange their ideas and logical observations...'
+          ]
         }
       ],
       player: {
@@ -46,10 +71,13 @@ class App extends React.Component{
         message: '',
         image: ''
       },
-      text: 'Click a button to start!'
+      text: 'Click a button to start!',
+      action: ''
     }
+
     this.handleClick = this.handleClick.bind(this)
     this.reset = this.reset.bind(this)
+    this.getWinner = this.getWinner.bind(this)
   }
 
   // METHODS *******************************************************************
@@ -74,10 +102,29 @@ class App extends React.Component{
     const player = this.state.player
 
     if (cpu.choice === player.choice){
+      this.setState({
+        action: this.state.options[player.index].action[2]
+      })
       return 'It\'s a tie!'
     } else if(this.state.options[player.index].beats.includes(cpu.choice)){
+      const winner = this.state.options[player.index]
+      this.setState({
+        action: winner.action[
+          winner.beats.findIndex(option =>
+            option === cpu.choice
+          )
+        ]
+      })
       return 'You Win!'
     } else if(this.state.options[cpu.index].beats.includes(player.choice)){
+      const winner = this.state.options[cpu.index]
+      this.setState({
+        action: winner.action[
+          winner.beats.findIndex(option =>
+            option === player.choice
+          )
+        ]
+      })
       return 'You Lose!'
     }
   }
@@ -100,7 +147,8 @@ class App extends React.Component{
     this.setState({
       player: playerInit,
       cpu: cpuInit,
-      text: 'Click a button to start!!'
+      text: 'Click a button to start!!',
+      action: ''
     })
   }
 
@@ -134,6 +182,7 @@ class App extends React.Component{
               message={this.state.player.message}
             />
             <div className="center">
+              <h3 id="actionMessage">{this.state.action}</h3>
               <h2 id="message">{this.state.text}</h2>
               <GameBoard
                 options={this.state.options}
