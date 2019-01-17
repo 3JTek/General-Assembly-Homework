4be654f02ca5e0fb7a5e95eccf0b882f469514ee
edwardlea <leaedward87@gmail.com>
2019-01-17 17:09:24 +0000

    computer randomly generating selection

--- edwardlea/react-rock-paper-scissors/src/app.js
+++ edwardlea/react-rock-paper-scissors/src/app.js
@@ -9,23 +9,53 @@ class App extends React.Component {
     this.state = {
       computerChoice: '',
       playerChoice: '',
-      
-
-
-
+      options: [
+        'rock',
+        'paper',
+        'scissors'
+      ]
 
     }
+
+
+    this.computerChoice = this.computerChoice.bind(this)
+    this.handleChoice = this.handleChoice.bind(this)
+
+
+
+
   }
+
+  handleChoice(e) {
+    console.log(e.target.value)
+    const computerChoice = this.computerChoice()
+    
+
+  }
+
+  computerChoice(){
+    console.log(this.state.options[Math.floor(Math.random() * (this.state.options.length))])
+    return this.state.options[Math.floor(Math.random() * (this.state.options.length))]
+
+  }
+
+  winnerLogic(){
+
+  }
+
+
 
   render(){
     return(
       <div>
         <div>
+          <h1> Rock, Paper, Scicsors</h1>
         </div>
         <div>
-          <button>Rock</button>
-          <button>Paper</button>
-          <button>Scisors</button>
+          <button value="Rock" onClick={this.handleChoice}>Rock</button>
+          <button value="Paper" onClick={this.handleChoice}>Paper</button>
+          <button value="Sciscors" onClick={this.handleChoice}>Sciscors</button>
+
         </div>
       </div>
     )
