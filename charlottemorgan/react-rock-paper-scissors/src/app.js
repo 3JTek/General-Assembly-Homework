c2025e9543005128da323d8c8e3bc67161b73b7a
Charlotte Morgan <charlotteellenmorgan@yahoo.co.uk>
2019-01-17 18:07:57 +0000

    all logic working and displaying on page, all info just in app.js need to make components

--- charlottemorgan/react-rock-paper-scissors/src/app.js
+++ charlottemorgan/react-rock-paper-scissors/src/app.js
@@ -2,15 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 
-
-
 class App extends React.Component {
   constructor(){
     super()
 
     this.state = {
-      computerChoice: '',
-      playerChoice: '',
+      computerChoice: null,
+      playerChoice: null,
       options: [
         'rock',
         'paper',
@@ -20,52 +18,72 @@ class App extends React.Component {
         rock: 'scissors',
         paper: 'rock',
         scissors: 'paper'
-      }
+      },
+      winner: null
     }
 
-    this.handleChoice = this.handleChoice.bind(this)
+
     this.computerChoice = this.computerChoice.bind(this)
-    this.winLogic = this.winLogic.bind(this)
+    this.handleChoice = this.handleChoice.bind(this)
+    this.winnerLogic = this.winnerLogic.bind(this)
+
+
+
 
   }
 
-  handleChoice(e){
-    console.log(e.target.value)
-    console.log(this.computerChoice())
-    this.setState({playerChoice: e.target.value})
-    this.setState({computerChoice: this.computerChoice()})
-    console.log(this.winLogic())
+  handleChoice(e) {
+    this.setState({playerChoice: e.target.value,computerChoice: this.computerChoice()}, () => {
+      this.setState({winner: this.winnerLogic()})
+    })
+
   }
 
   computerChoice(){
     return this.state.options[Math.floor(Math.random() * (this.state.options.length))]
-  }
 
-  winLogic() {
-    if(this.state.playerChoice === this.state.computerChoice) return 'Tie'
-    if(this.state.winConditions[this.state.playerChoice] === this.state.computerChoice) return 'You win'
-    return 'You lose'
   }
 
 
+  winnerLogic() {
+    if(this.state.computerChoice){
+      if(this.state.playerChoice === this.state.computerChoice) return 'Tie'
+      if(this.state.winConditions[this.state.playerChoice] === this.state.computerChoice) return 'You win'
+      return 'You lose'
+    }
+  }
 
-  render() {
+
+
+
+  render(){
     return(
       <div>
         <div>
-          <h1>Rock, Paper, Scissors</h1>
+          <h1> Rock, Paper, Scissors</h1>
+        </div>
+        <div className="player">
+          <h2>Player Choice: {this.state.playerChoice}</h2>
+        </div>
+        <div className="computer">
+          <h2>Computer Choice: {this.state.computerChoice}</h2>
         </div>
         <div>
           <button value="rock" onClick={this.handleChoice}>Rock</button>
           <button value="paper" onClick={this.handleChoice}>Paper</button>
           <button value="scissors" onClick={this.handleChoice}>Scissors</button>
         </div>
+        <div className="winner">
+          <h2>Winner: {this.state.winner}</h2>
+        </div>
       </div>
-
     )
+
   }
 
 }
+
+
 
 ReactDOM.render(
   <App />,
