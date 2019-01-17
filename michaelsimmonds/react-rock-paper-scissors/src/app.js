cf9e6dd0fa0c9084be7c4e60974bb5ab2c77f985
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-17 19:46:31 +0000

    Added win logic

--- michaelsimmonds/react-rock-paper-scissors/src/app.js
+++ michaelsimmonds/react-rock-paper-scissors/src/app.js
@@ -14,38 +14,43 @@ class App extends React.Component {
         scissors: 'paper'
       },
       player: '',
-      comp: ''
+      comp: '',
+      outcome: ''
     }
     this.choices = Object.keys(this.state.win)
-    console.log(this.choices)
 
     this.handleClick = this.handleClick.bind(this)
+    this.winnerLogic = this.winnerLogic.bind(this)
   }
 
 
   handleClick(e) {
     e.preventDefault()
-    console.log(e.target.value)
-    this.setState({ player: e.target.value})
-    const rand = this.choices[Math.floor(Math.random() * this.choices.length)]
-    this.setState({ comp: rand })
+    const player = e.target.value
+    console.log(player)
+    this.setState({ player: player})
+    const comp = this.choices[Math.floor(Math.random() * this.choices.length)]
+    console.log(comp)
+    this.setState({ comp: comp})
+    const winner = this.winnerLogic(player, comp)
+    console.log(winner)
   }
 
+  winnerLogic(player, comp) {
+    if(player === comp) return 'tie'
+    if(this.state.win[player] === comp) return 'win'
+    else return 'lose'
+  }
 
-
-  // handleSubmit(e) {
-  //   e.preventDefault()
-  //   const task = { task: this.state.newTask}
-  //   const newTodos = [...this.state.todos, task]  // spread previous todos before one just made so as not to change original
-  //   this.setState({todos: newTodos, newTask: ''}) // updates the todos, newTask:'' clears the box after submission
-  // }
 
   render() {
     return (
       <div>
         <ButtonOne handleClick={this.handleClick}/>
-        <h1> {this.state.player} </h1>
-        <h1> {this.state.comp} </h1>
+        <h1>Player Chooses:</h1>
+        <h2>{this.state.player}</h2>
+        <h1>Computer Chooses:</h1>
+        <h2>{this.state.comp}</h2>
       </div>
     )
   }
