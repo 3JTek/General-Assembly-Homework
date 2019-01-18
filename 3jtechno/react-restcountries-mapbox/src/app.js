68702686fce615bbb97c108371dfc9e6961b7c7f
3jtechno <3j.techno@gmail.com>
2019-01-18 17:30:26 +0000

    Map displayed

--- 3jtechno/react-restcountries-mapbox/src/app.js
+++ 3jtechno/react-restcountries-mapbox/src/app.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import mapboxgl from 'mapbox-gl'
+
+import './scss/style.scss'
 
 class App extends React.Component {
 
+  componentDidMount(){
+    console.log(this.mapDOMElement)
+    mapboxgl.accessToken = process.env.MAPBOX_TOKEN
+    console.log(process.env);
+    const map = new mapboxgl.Map({
+      container: this.mapDOMElement,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      zoom: 1
+    })
+  }
+
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div id='map' ref={element => this.mapDOMElement = element}/>
     )
   }
 }
