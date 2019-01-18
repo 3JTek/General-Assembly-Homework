80d9ea5bd9559eb0d2cb1a2b19095aa16a6c4e49
3jtechno <3j.techno@gmail.com>
2019-01-18 19:51:19 +0000

    Map displays all flags

--- 3jtechno/react-restcountries-mapbox/src/app.js
+++ 3jtechno/react-restcountries-mapbox/src/app.js
@@ -4,24 +4,38 @@ import ReactDOM from 'react-dom'
 import './scss/style.scss'
 
 import Map from './components/Map'
+import axios from 'axios'
 
 class App extends React.Component {
 
   constructor(props){
     super(props)
 
+    this.countriesWithCoord,
     this.state = {
-      style: 'mapbox://styles/mapbox/streets-v9',
-      zoom: 1
+      mapDisplay: {
+        style: 'mapbox://styles/mapbox/streets-v9',
+        zoom: 1,
+        countries: []
+      }
     }
+  }
+  componentDidMount(){
+    //Get the data from restcountries api and store them in App state
+    axios.get('https://restcountries.eu/rest/v2/all').then(data => {
+      this.setState({countries: data.data})
+    })
   }
 
 
-
   render() {
+    if(!this.state.countries) return false
     return (
       <main>
-        <Map {...this.state}/>
+        <Map
+          mapDisplay={this.state.mapDisplay}
+          countries={this.state.countries}
+        />
       </main>
     )
   }
--- 3jtechno/react-restcountries-mapbox/src/components/Map.js
+++ 3jtechno/react-restcountries-mapbox/src/components/Map.js
@@ -3,17 +3,40 @@ import mapboxgl from 'mapbox-gl'
 
 class Map extends React.Component{
 
+  constructor(){
+    super()
+    this.countriesWithCoord
+  }
+
   componentDidMount(){
-    const {style, zoom} = this.props
+    const {mapDisplay, countries} = this.props
     mapboxgl.accessToken = process.env.MAPBOX_TOKEN
     const map = new mapboxgl.Map({
       container: this.mapDOMElement,
-      style: style,
-      zoom: zoom
+      style: mapDisplay.style,
+      zoom: mapDisplay.zoom
+    })
+    console.log('before funciton');
+
+    // Remove countries that do not have coordinates
+    this.countriesWithCoord = countries.filter(country => {
+      return country.latlng.length === 2
+    })
+    //Create Markup on the Map
+    console.log(this.countriesWithCoord)
+    console.log('after funciton');
+
+    this.countriesWithCoord.forEach(country => {
+      const markerDOM = document.createElement('div')
+      markerDOM.style.backgroundImage = `url(${country.flag})`
+      const marker = new mapboxgl.Marker(markerDOM)
+        .setLngLat(country.latlng.reverse())
+        .addTo(map)
     })
   }
 
   render(){
+
     return(
       <div id='map' ref={element => this.mapDOMElement = element}/>
     )
