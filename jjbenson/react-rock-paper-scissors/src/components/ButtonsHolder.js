30906f84a51fa08b7771814bd47934268745cd33
James Benson <you@example.com>
2019-01-18 09:19:51 +0000

    Added reset button

--- jjbenson/react-rock-paper-scissors/src/components/ButtonsHolder.js
+++ jjbenson/react-rock-paper-scissors/src/components/ButtonsHolder.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
-import Button from './Button.js'
+// import Button from './Button.js'
 
-const Buttons = ({choices, handleClick}) => {
+const Buttons = ({choices, handleClick, handleReset }) => {
   return(
     <div className="buttonHolder">
-      {choices.map(val => <Button key={val} handleClick={handleClick} value={val}/>)}
+      {/*choices.map(val => <Button key={val} handleClick={handleClick} value={val}/>)*/}
+      {choices.map(val => <button key={val} onClick={handleClick} value={val}>{val}</button>)}
+      <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
