530d31f373e9ca6394a4e42814d97c24250df32f
Dexter De Leon <dexdeleon@gmail.com>
2019-01-19 21:46:25 +0000

    REST countries data obtained through axios and currently logging to console.

--- DexDeLeon/react-restcountries-mapbox/src/app.js
+++ DexDeLeon/react-restcountries-mapbox/src/app.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import axios from 'axios'
+
 import './scss/style.scss'
 
 import Map from './components/Map'
 
 class App extends React.Component {
+
+  componentDidMount(){
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(countries => console.log(countries))
+  }
 
   render() {
     return (
