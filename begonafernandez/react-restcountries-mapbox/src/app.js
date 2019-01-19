11920290bd30036e30a005ead834e1c94079dc3f
Begona Fernandez <boadicea1979@yahoo.es>
2019-01-19 21:28:00 +0000

    extracted Mao to it's own componenet

--- begonafernandez/react-restcountries-mapbox/src/app.js
+++ begonafernandez/react-restcountries-mapbox/src/app.js
@@ -1,24 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import mapboxgl from 'mapbox-gl'
-mapboxgl.accessToken = process.env.MAPBOX_TOKEN
-import 'mapbox-gl/dist/mapbox-gl.css'
-
 import './scss/style.scss'
 
+import Map from './components/Map'
 class App extends React.Component {
-
-  componentDidMount() {
-    this.map = new mapboxgl.Map({
-      container: this.mapDiv,
-      style: 'mapbox://styles/mapbox/streets-v9'
-    })
-  }
 
   render() {
     return (
-      <div className="map" ref={mapDiv => this.mapDiv = mapDiv} />
+      <Map />
     )
   }
 }
---
+++ begonafernandez/react-restcountries-mapbox/src/components/Map.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import mapboxgl from 'mapbox-gl'
+mapboxgl.accessToken = process.env.MAPBOX_TOKEN
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+class Map extends React.Component {
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      zoom: 1
+    })
+  }
+
+  render() {
+    return(
+      <div className="map" ref={mapDiv => this.mapDiv = mapDiv} />
+    )
+  }
+}
+export default Map
