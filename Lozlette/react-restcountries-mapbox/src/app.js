2bb652f27aefd895f4799e9164ff3128e66f7212
Lauren Bell <laurengilbert0@gmail.com>
2019-01-19 17:15:08 +0000

    trying to loop through data array but getting error message cannot read property 'length' of undefined

--- Lozlette/react-restcountries-mapbox/src/app.js
+++ Lozlette/react-restcountries-mapbox/src/app.js
@@ -9,17 +9,23 @@ import Map from './components/Map'
 
 class App extends React.Component {
 
+
   componentDidMount() {
     axios.get('https://restcountries.eu/rest/v2/all')
-      .then(response => this.setState({ points: response.data }))
-    console.log(this.state)
+      .then(response => this.setState( { countries: response.data }))
+      .catch( error => console.log(error))
   }
 
-
-
-
   render() {
+    if (this.state){
+      for (let i = 0; i < this.state.data.length; i++) {
+        console.log(this.state.data[i].latlng)
+      }
+    }
+
     console.log(this.state)
+
+
     return (
       <main>
         <Map />
