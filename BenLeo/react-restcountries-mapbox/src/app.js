4469adf38a6362467be80ba82bfb56b42a149b68
Ben Lander <ben.l.lander@googlemail.com>
2019-01-19 13:57:22 +0000

    Imported API Data

--- BenLeo/react-restcountries-mapbox/src/app.js
+++ BenLeo/react-restcountries-mapbox/src/app.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import axios from 'axios'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+import './scss/style.scss'
+
 class App extends React.Component {
+
+  componentDidMount() {
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(res => this.setState(res.data))
+  }
 
   render() {
     return (
