7c052057c087c3fbc57d032237493cb2808d25f7
Siddant <siddantgurung@hotmail.co.uk>
2019-01-19 19:38:29 +0000

    done

--- siddant/react-restcountries-mapbox/src/components/Map.js
+++ siddant/react-restcountries-mapbox/src/components/Map.js
@@ -8,9 +8,10 @@ class Map extends React.Component{
     this.map = new mapboxgl.Map({
       container: this.mapDiv,
       style: 'mapbox://styles/mapbox/streets-v9',
-      center: {lng: -0.118092, lat: 51.509865},
-      zoom: 5
+      center: {lng: -0, lat: 0},
+      zoom: 1
     })
+
     this.props.country.filter(country => country.location.length !== 0)
       .map(country => {
         const markerElement = document.createElement('DIV')
@@ -18,8 +19,22 @@ class Map extends React.Component{
         markerElement.style.backgroundImage = `url(${country.flag})`
         markerElement.style.backgroundSize = 'contain'
         markerElement.style.backgroundRepeat = 'no-repeat'
+        markerElement.addEventListener('click',()=>{
+          this.map.flyTo({
+            center: {lat: country.location[0], lng: country.location[1]},
+            zoom: 5
+          })
+
+        })
         return new mapboxgl.Marker(markerElement)
           .setLngLat({lat: country.location[0], lng: country.location[1]})
+          .setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML(`
+                      <img src="${country.flag}" alt="${country.name}"  height="50" width="70">
+                      <h3>${country.name}</h3>
+                      <p>${country.nativeName}</p>
+                      <p>population: ${country.population}</p>
+                    `))
           .addTo(this.map)
       })
 
