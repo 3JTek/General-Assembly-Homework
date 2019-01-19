484b89c2ca1d3eab9b2f4762ff423b940940214a
byemane <beteyemane@hotmail.co.uk>
2019-01-19 17:22:28 +0000

    got data from rest-flag

--- beteyemane/react-restcountries-mapbox/src/app.js
+++ beteyemane/react-restcountries-mapbox/src/app.js
@@ -4,8 +4,14 @@ import Map from './components/Map'
 
 import './scss/style.scss'
 import 'mapbox-gl/dist/mapbox-gl.css'
+import axios from 'axios'
+
 
 class App extends React.Component {
+  componentDidMount() {
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(data => console.log(data))
+  }
 
   render() {
     return (
