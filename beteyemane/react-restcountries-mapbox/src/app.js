51dbd259bffe2ed72f436a9f861656ba8837b312
byemane <beteyemane@hotmail.co.uk>
2019-01-19 16:41:48 +0000

    mapbox up and running

--- beteyemane/react-restcountries-mapbox/src/app.js
+++ beteyemane/react-restcountries-mapbox/src/app.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-class App extends React.Component {
+import mapboxgl from 'mapbox-gl'
+mapboxgl.accessToken = process.env.mapboxToken 
+import 'mapbox-gl/dist/mapbox-gl.css'
 
+import './scss/style.scss'
+
+
+class App extends React.Component {
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv,
+      style: 'mapbox://styles/mapbox/streets-v9'
+    })
+
+  }
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div className="map" ref={el => this.mapDiv = el}/>
     )
   }
 }
