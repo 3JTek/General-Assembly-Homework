f1f2916ac71cb95b93a910d302547ef33a474b54
joshuaking06 <joshua.king0692@gmail.com>
2019-01-20 16:13:20 +0000

    got mapbox

--- jjking1/react-restcountries-mapbox/src/app.js
+++ jjking1/react-restcountries-mapbox/src/app.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import axios from 'axios'
+
 class App extends React.Component {
 
+  componentDidMount(){
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(res => this.setState({
+        countries: res.data
+      }))
+
+  }
+
   render() {
+    if(!this.state) return null
+    console.log(this.state.countries)
     return (
       <h1>Hello World!</h1>
     )
---
+++ jjking1/react-restcountries-mapbox/src/components/Map.js
@@ -0,0 +1,5 @@
+import react from 'react'
+
+class Map extends React.Component{
+  
+}
