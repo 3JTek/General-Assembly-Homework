deb82dcf077e4934eaeb3dafa748b46109238239
Dexter De Leon <dexdeleon@gmail.com>
2019-01-20 17:46:13 +0000

    Flag markers now displaying country information on click.

--- DexDeLeon/react-restcountries-mapbox/src/components/Map.js
+++ DexDeLeon/react-restcountries-mapbox/src/components/Map.js
@@ -22,14 +22,26 @@ class Map extends React.Component {
 
     const filtered = this.props.countries.filter(country => country.latlng.length === 2)
     return filtered.map(country => {
+      // Set a className for countries by replacing spaces in names with dashes and converting to lowercase
+      const countryClass = `${country.name.toLowerCase().split(' ').join('-')}`
       const marker = document.createElement('div')
-      marker.classList.add('marker', `${country.name.toLowerCase().split(' ').join('-')}`)
+      marker.classList.add('marker', `${countryClass}`)
       marker.innerHTML = `<img src='${country.flag}' alt='Flag of ${country.name}'>`
+
+      const popUp = new mapboxgl.Popup({offset: [0, -50], anchor: 'bottom'})
+        .setHTML(`
+            <h3>Country: ${country.name}</h3>
+            <h3>Native Name: ${country.nativeName}</h3>
+            <h4>Capital: ${country.capital}</h4>
+            <h4>Population: ${country.population}</h4>
+
+          `)
 
       const [ lat, lng ]  = country.latlng
       // Offset property moves the marker 50px up
       return new mapboxgl.Marker(marker, {offset: [0, -50]})
         .setLngLat([lng, lat])
+        .setPopup(popUp)
         .addTo(this.map)
     })
   }
