70e7db68e5d1cdbbc1702a551d939a5e87bb62d1
Dexter De Leon <dexdeleon@gmail.com>
2019-01-20 21:30:31 +0000

    Basic header added.

--- DexDeLeon/react-restcountries-mapbox/src/app.js
+++ DexDeLeon/react-restcountries-mapbox/src/app.js
@@ -5,6 +5,7 @@ import axios from 'axios'
 
 import './scss/style.scss'
 
+import Header from './components/Header'
 import Map from './components/Map'
 
 class App extends React.Component {
@@ -18,22 +19,31 @@ class App extends React.Component {
     axios.get('https://restcountries.eu/rest/v2/all')
       .then(countries => this.setState({ countries: countries.data }))
   }
-
-  // selectHandler(e){
   //
+  // selectHandler(e){
+  //   //Create a new array of countries that match the selected region
   //   const countries = this.state.countries.filter(country =>
   //     country.region.toLowerCase() === e.target.value
   //   )
   //   console.log(countries, this.state.countries)
+  //   //Set state with the new array of countries
   //   this.setState({ countries })
   // }
 
   render() {
     if (!this.state) return null
     return (
+      <div>
+        <Header />
+        <Map
+          zoom={1.25}
+          // Map center set to Philippines
+          center={{lat: 12.8797, lng: 121.7740}}
+          countries={this.state.countries}
+        />
+      </div>
       // Attempted to add a select to filter through flag markers.
       // Managed to filter this.state.countries by region, but couldn't get the Map component to redraw the markers.
-      // <div>
       //   <select onChange={this.selectHandler} >
       //     <option value="all">All</option>
       //     <option value="africa">Africa</option>
@@ -42,12 +52,6 @@ class App extends React.Component {
       //     <option value="europe">Europe</option>
       //     <option value="oceania">Oceania</option>
       //   </select>
-      <Map
-        zoom={1.25}
-        center={{lat: 12.8797, lng: 121.7740}}
-        countries={this.state.countries}
-      />
-      // </div>
     )
   }
 }
