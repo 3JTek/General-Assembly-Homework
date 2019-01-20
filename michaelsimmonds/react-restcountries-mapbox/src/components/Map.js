30abe406904cfb8eb7c588f712e4341be8c00bdc
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-20 15:10:58 +0000

    Have map set up

---
+++ michaelsimmonds/react-restcountries-mapbox/src/components/Map.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import mapboxgl from 'mapbox-gl'
+mapboxgl.accessToken = process.env.MAPBOX_TOKEN
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+class Map extends React.Component {
+
+  componentDidMount() {
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      center: this.props.center,
+      zoom: this.props.zoom
+    })
+
+    // this.props.markers.map(marker => {
+    //   const nbBikes = marker.additionalProperties.find(property => property.key === 'NbBikes').value
+    //   const markerElement = document.createElement('DIV')
+    //   markerElement.className = 'custom-marker'
+    //   markerElement.textContent = nbBikes
+    //
+    //   return new mapboxgl.Marker(markerElement)
+    //     .setLngLat({ lat: marker.lat, lng: marker.lon})
+    //     .addTo(this.map)
+    // })
+  }
+
+
+
+  render() {
+    return (
+      <div className="map" ref={el => this.mapDiv = el}/>  //this is shorthad for <div><div/>. this is javascript, not html
+    )
+  }
+}
+
+export default Map
