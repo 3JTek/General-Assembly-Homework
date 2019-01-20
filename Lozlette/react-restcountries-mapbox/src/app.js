e78f7b832212832090f50ba1ab14fc1e00063021
Lauren Bell <laurengilbert0@gmail.com>
2019-01-20 22:28:47 +0000

    filtering empty arrays from latlng array

--- Lozlette/react-restcountries-mapbox/src/app.js
+++ Lozlette/react-restcountries-mapbox/src/app.js
@@ -22,28 +22,48 @@ class App extends React.Component {
   render() {
     if(!this.state) return null
 
-    console.log(this.state)
+    // console.log(this.state)
     let latLng
     let lat
     let lng
+    const lnglatMapbox = []
+    let filteredLngLat = []
     const data = Object.values(this.state)
-    console.log(data, typeof data)
+    // console.log(data, typeof data)
 
 
 
     for (let i = 0; i < data[0].length; i++) {
       latLng = Object.values(data[0][i])
-      console.log('working', data[0][i].latlng)
+      // console.log('working', data[0][i].latlng)
       lat = data[0][i].latlng[0]
-      console.log('lat', data[0][i].latlng[0])
+      // console.log('lat', data[0][i].latlng[0])
       lng = data[0][i].latlng[1]
-      console.log('lng', data[0][i].latlng[1])
+      // console.log('lng', data[0][i].latlng[1])
+      lnglatMapbox.push([lng,lat])
 
     }
     console.log('latlng', latLng)
+    console.log(lnglatMapbox)
+
+
+    // const filteredLngLat = lnglatMapbox.forEach((array) => {
+    //   array.filter(function(innerArrays){
+    //     return innerArrays !== undefined
+    //   })
+    // }
+    // )
+
+    for (let i = 0; i < lnglatMapbox.length; i++){
+      filteredLngLat.push(lnglatMapbox[i].filter( Boolean ))
+    }
+
+    console.log(filteredLngLat)
 
 
 
+
+    // console.log(filteredLngLat)
     // if (Object.value(countries)){
     //   for (let i = 0; i < Object.value(this.state).length; i++) {
     //     console.log(Object.value(this.state)[i].latlng)
