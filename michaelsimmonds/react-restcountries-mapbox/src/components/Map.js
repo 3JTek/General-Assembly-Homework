319d45bc9ac3645dac1dcea858d8feb99db75185
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-20 21:08:25 +0000

    Added flags to map, with pop-up info

--- michaelsimmonds/react-restcountries-mapbox/src/components/Map.js
+++ michaelsimmonds/react-restcountries-mapbox/src/components/Map.js
@@ -18,17 +18,20 @@ class Map extends React.Component {
       if (marker.latlng.length === 2) {
         const markerElement = document.createElement('DIV')
         markerElement.className = 'custom-marker'
+        markerElement.style.backgroundImage = `url(${marker.flag})`
+        markerElement.style.backgroundSize = 'contain'
+        markerElement.style.backgroundRepeat = 'no-repeat'
+        markerElement.src = marker.flag
 
 
         return new mapboxgl.Marker(markerElement)
           .setLngLat({ lat: marker.latlng[0], lng: marker.latlng[1]})
+          .setPopup(new mapboxgl.Popup({ offset: 25 })
+            .setHTML(`<h3>${marker.name}</h3><p>${marker.nativeName}</p>`))
           .addTo(this.map)
       }
     })
-
   }
-
-
 
   render() {
     console.log(this.props.markers[0].latlng)
