8f26720132aeae9ce1963fc04006ddee33a562dd
Gessy90 <gessicasantoro90@hotmail.it>
2019-01-21 17:48:06 +0000

    final

---
+++ Gessy90/winebored/src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return(
+    <section className="section">
+      <div className="container">
+        <h1 className="h1"> Homepage </h1>
+      </div>
+    </section>
+  )
+}
+
+export default Home
