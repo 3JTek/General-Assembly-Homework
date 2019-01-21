891e99657fb43591f68f7460e500ef20a1f53ffe
Alessandro Lepri <alex.lepri84@gmail.com>
2019-01-21 18:44:22 +0000

    Added some style

--- alexlepri/winebored/src/components/Home.js
+++ alexlepri/winebored/src/components/Home.js
@@ -5,7 +5,9 @@ const Home = () => {
   return (
     <section className="section">
       <div className="container">
-        <h1 className="title is-1"> Home </h1>
+        <h1 className="title is-1"> Welcome to our wines selection </h1>
+        <img src= "https://www.roughguides.com/wp-content/uploads/2013/06/05.chiantiItaly-847508211-1680x1050.jpg"  />
+
       </div>
     </section>
   )
