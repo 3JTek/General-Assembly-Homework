e9d832814f69e1255ba64c484d443ba1d9091e97
seb yeates <yeatessa@gmail.com>
2019-01-21 18:24:12 +0000

    imports and nav added

--- syeates/winebored/src/app.js
+++ syeates/winebored/src/app.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import 'bulma'
+import './style.scss'
+
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+
+
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <main>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/wines">wines Index</Link>
+          </nav>
+        </main>
+      </BrowserRouter>
     )
   }
 }
 
 ReactDOM.render(
   <App />,
+
+
   document.getElementById('root')
 )
