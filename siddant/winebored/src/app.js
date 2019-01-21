1db74bbe447e3a68ed9419a5bfbab58a975783e2
Siddant <siddantgurung@hotmail.co.uk>
2019-01-21 16:14:24 +0000

    apages added

--- siddant/winebored/src/app.js
+++ siddant/winebored/src/app.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import {BrowserRouter, Route, Link, Switch} from 'react-router-dom'
+
+import Home from './components/Home'
+import ShowWine from './components/ShowWine'
+
+import './style.scss'
+
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <main>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/wines">Wines</Link>
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/wines" component={ShowWine} />
+          </Switch>
+        </main>
+      </BrowserRouter>
+
     )
   }
 }
---
+++ siddant/winebored/src/components/ShowWine.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowWine = () =>{
+  return(
+    <h1>Wine</h1>
+  )
+}
+
+export default ShowWine
