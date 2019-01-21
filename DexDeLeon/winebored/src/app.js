782e2f7907c3b62a6cc0881c9e03e4f3b2812bbb
Dexter De Leon <dexdeleon@gmail.com>
2019-01-21 16:54:57 +0000

    Axios getting wines and saving to state.

--- DexDeLeon/winebored/src/app.js
+++ DexDeLeon/winebored/src/app.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import axios from 'axios'
+
+import 'bulma'
+import './style.scss'
+
 class App extends React.Component {
+  constructor(){
+    super()
+
+    this.state = {}
+  }
+
+  componentDidMount(){
+    axios.get('https://winebored.herokuapp.com/wines')
+      .then(res => this.setState({ wines: res.data }))
+  }
+
   render() {
     return (
-      <h1>Hello World</h1>
+      <header>
+        <h1 className="title is-1">WineBored</h1>
+      </header>
     )
   }
 }
