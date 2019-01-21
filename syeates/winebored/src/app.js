3aa3f8869fb8346bdb37e5915b36b311334c7e41
seb yeates <yeatessa@gmail.com>
2019-01-21 19:36:32 +0000

    notes added for me to understand whats going on

--- syeates/winebored/src/app.js
+++ syeates/winebored/src/app.js
@@ -1,16 +1,21 @@
+// Import the Language
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// Import the styling libraries
 import 'bulma'
 import './style.scss'
 
+// Import the navigation router witch contains switch
 import {BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 
+// Import the components
 import Home from './components/Home'
 import WineIndex from './components/WineIndex'
 import WineShow from './components/WineShow'
 
-
+// Structure of the page with the interchangeable components (switch) depends on the router-dom
 class App extends React.Component {
   render() {
     return (
--- syeates/winebored/src/components/Home.js
+++ syeates/winebored/src/components/Home.js
@@ -1,7 +1,8 @@
 import React from 'react'
-
+// Standard Homepage just contains h1 home 
 const Home = () => {
   return (
+
     <section className="section">
       <div className="container">
         <h1 className="title is-1">Home</h1>
