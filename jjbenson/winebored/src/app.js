6a169db28d6e204566c7a5e0a097d05534c25582
James Benson <you@example.com>
2019-01-21 23:34:18 +0000

    Fixed linter issue

--- jjbenson/winebored/src/app.js
+++ jjbenson/winebored/src/app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 // import 'bulma'
 import './style.scss'
