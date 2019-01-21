1d0f276403fa79b90d8195063c8188bc1b0aa3b8
joshuaking06 <joshua.king0692@gmail.com>
2019-01-21 15:54:23 +0000

    set up webpack

--- jjking1/winebored/src/app.js
+++ jjking1/winebored/src/app.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Link, BrowserRouter, Route, Switch } from 'react-router-dom'
+import 'bulma'
+import './style.scss'
 
 class App extends React.Component {
   render() {
