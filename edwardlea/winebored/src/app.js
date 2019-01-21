6520a183902c0f1c45360865d3fedf0c0308223e
edwardlea <leaedward87@gmail.com>
2019-01-21 15:58:50 +0000

    Home pages section added to application

--- edwardlea/winebored/src/app.js
+++ edwardlea/winebored/src/app.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import {BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+
+
+import './style.scss'
+
+
+import 'bulma'
+
+import Home from './components/Home'
+
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <main>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/.cheeses">All Cheeses</Link>
+
+          </nav>
+          <Switch>
+            <Route exact path="/" component={Home}></Route>
+          </Switch>
+        </main>
+      </BrowserRouter>
     )
   }
 }
