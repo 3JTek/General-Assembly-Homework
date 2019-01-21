57b2f8fbfb98c4d750adb6325b6338558b9d20d0
Dexter De Leon <dexdeleon@gmail.com>
2019-01-21 17:06:30 +0000

    Wine names generated as h2 elements

--- DexDeLeon/winebored/src/app.js
+++ DexDeLeon/winebored/src/app.js
@@ -10,19 +10,29 @@ class App extends React.Component {
   constructor(){
     super()
 
-    this.state = {}
+    this.state = {
+      wines: []
+    }
   }
 
   componentDidMount(){
     axios.get('https://winebored.herokuapp.com/wines')
       .then(res => this.setState({ wines: res.data }))
+      .catch(error => console.error('ERROR', error))
   }
 
   render() {
     return (
-      <header>
-        <h1 className="title is-1">WineBored</h1>
-      </header>
+      <div>
+        <header>
+          <h1 className="title is-1">WineBored</h1>
+        </header>
+        <main>
+          {this.state.wines.map(wine =>
+            <h2 className="subtitle" key={wine._id}>{wine.name}</h2>
+          )}
+        </main>
+      </div>
     )
   }
 }
