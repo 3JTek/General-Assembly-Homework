cd044a98c1040cf7ec7b70e4e5730bae7ce30524
Alessandro Lepri <alex.lepri84@gmail.com>
2019-01-21 17:44:08 +0000

    Fixed Index.js issues

--- alexlepri/winebored/src/app.js
+++ alexlepri/winebored/src/app.js
@@ -1,10 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
+
+import 'bulma'
+import './style.scss'
+
+
+
+
+import WinesIndex from './components/WinesIndex'
+import Home from './components/Home'
+import WinesShow from './components/WinesShow'
+
+
 class App extends React.Component {
   render() {
     return (
-      <h1>Hello World</h1>
+      <BrowserRouter>
+        <main>
+
+          <nav>
+            <Link to="/"> Home </Link>
+            <Link to="/wines"> Wines Index </Link>
+          </nav>
+
+          <Switch>
+            <Route path="/wines/:id" component={WinesShow} />
+            <Route path="/wines" component={WinesIndex}/>
+            <Route path="/" component={Home} />
+          </Switch>
+        </main>
+      </BrowserRouter>
     )
   }
 }
---
+++ alexlepri/winebored/src/components/WinesShow.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import axios from 'axios'
+
+class WinesShow extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {}
+  }
+
+  componentDidMount() {
+    axios.get(`https://winebored.herokuapp.com/wines/${this.props.match.params.id}`)
+      .then(res => this.setState({wine: res.data}))
+  }
+
+  render () {
+    if(!this.state.wine) return null
+    const { name, origin, image, tastingNotes } = this.state.wine
+    return (
+      <section className="section">
+        <div className="container">
+          <h1 className="title is-1">{name}</h1>
+          <hr />
+
+          <div className="columns">
+            <div className="column">
+              <figure className="image">
+                <img src={image} alt={name} />
+              </figure>
+            </div>
+
+            <div className="column">
+              <h4 className="title is-4">{origin}</h4>
+              <hr />
+
+              <h4 className="title is-4">Tasting Notes</h4>
+              <p>{tastingNotes}</p>
+              <hr />
+            </div>
+          </div>
+        </div>
+      </section>
+    )
+
+  }
+
+}
+
+export default WinesShow
