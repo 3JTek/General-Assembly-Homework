3b8d81d8afce70f0aee8c275c93de553f08ef7ca
seb yeates <yeatessa@gmail.com>
2019-01-22 09:28:42 +0000

    added some styling

--- syeates/winebored/src/components/Home.js
+++ syeates/winebored/src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
 
     <section className="section">
       <div className="container">
-        <h1 className="title is-1 has-text-centered">Home</h1>
+        <h1 className="title is-1 has-text-centered">Wine Wine Wine delilah</h1>
         <img src="https://images.pexels.com/photos/1097425/pexels-photo-1097425.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" alt="My Image" height="887" width="897"/>
       </div>
     </section>
