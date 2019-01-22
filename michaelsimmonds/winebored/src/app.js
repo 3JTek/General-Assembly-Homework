cba1462a0186c496f040b3353d33fd25f29301df
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-01-22 16:49:31 +0000

    Added a Login Component

--- michaelsimmonds/winebored/src/app.js
+++ michaelsimmonds/winebored/src/app.js
@@ -10,6 +10,7 @@ import Home from './components/Home'
 import WinesIndex from './components/wines/WinesIndex'
 import WinesShow from './components/wines/WinesShow'
 import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 
 class App extends React.Component {
@@ -22,12 +23,14 @@ class App extends React.Component {
             <Link to="/">Home</Link>
             <Link to="/wines">Wines</Link>
             <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
           </nav>
 
           <Switch>
             <Route path="/wines/:id" component={WinesShow} />
             <Route path="/wines" component={WinesIndex} />
-            <Route path='/register' component={Register} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
             <Route path="/" component={Home}/>
           </Switch>
 
