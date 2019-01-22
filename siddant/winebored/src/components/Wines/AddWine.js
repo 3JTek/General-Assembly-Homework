6e844f3a1cdd4531783cc0b465ca209a625499f9
Siddant <siddantgurung@hotmail.co.uk>
2019-01-22 18:44:34 +0000

    Done

--- siddant/winebored/src/components/Wines/AddWine.js
+++ siddant/winebored/src/components/Wines/AddWine.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import axios from 'axios'
+import WinesForm from './WinesForm'
 
 import Auth from '../../lib/Auth'
 
@@ -38,75 +39,17 @@ class AddWine extends React.Component {
           headers: { Authorization: `Bearer ${Auth.getToken()}`}
         }
       )
-      .then((res) => {
-        alert(res.message)
-        console.log('added')
-      })
+      .then(() => this.props.history.push('/wines'))
       .catch(err => console.log(err))
   }
 
   render(){
-    const{name, origin,image,tastingNotes,grape,abv,price} = this.state.data
     return(
-      <section className="section">
-        <div className="container">
-          <h1 className="title is-1">Add New Wines</h1>
-          <form onSubmit={this.handleSubmit}>
-
-            <div className="field">
-              <label className="label">Name</label>
-              <div className="control">
-                <input className="input" type="text" onChange={this.handleChange} placeholder="Wine Name" name="name" value={name}/>
-              </div>
-            </div>
-
-            <div className="field">
-              <label className="label">Origin</label>
-              <div className="control">
-                <input className="input" type="text"onChange={this.handleChange} placeholder="Origin" name="origin" value={origin}/>
-              </div>
-            </div>
-
-
-            <div className="field">
-              <label className="label">Image</label>
-              <div className="control">
-                <input className="input" type="text" onChange={this.handleChange} placeholder="Image" name="image" value={image}/>
-              </div>
-            </div>
-
-            <div className="field">
-              <label className="label">Grapes</label>
-              <div className="control">
-                <input className="input" type="text" onChange={this.handleChange} placeholder="Grapes" name="grape" value={grape}/>
-              </div>
-            </div>
-
-            <div className="field">
-              <label className="label">Alcohol by Volume</label>
-              <div className="control">
-                <input className="input" type="number"onChange={this.handleChange} placeholder="0" name="abv" value={abv} min="0" />
-              </div>
-            </div>
-
-            <div className="field">
-              <label className="label">Wine Price</label>
-              <div className="control">
-                <input className="input" type="number" onChange={this.handleChange} placeholder="0" name="price" value={price} min="0" />
-              </div>
-            </div>
-
-            <div className="field">
-              <label className="label">Tasting Notes</label>
-              <div className="control">
-                <textarea className="textarea" type="text" onChange={this.handleChange} placeholder="Tasting Notes" name="tastingNotes" value={tastingNotes}/>
-              </div>
-            </div>
-
-            <button className="button is-primary">Submit</button>
-          </form>
-        </div>
-      </section>
+      <WinesForm
+        data={this.state.data}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
     )
   }
 }
