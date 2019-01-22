b26184c0bc22e1bdde4e17f52dc90f234a1815aa
Dexter De Leon <dexdeleon@gmail.com>
2019-01-22 07:24:49 +0000

    Some basic responsive styles added.

--- DexDeLeon/winebored/src/components/Header.js
+++ DexDeLeon/winebored/src/components/Header.js
@@ -3,6 +3,21 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 class Header extends React.Component {
+
+  clickHandler(e){
+    // Get the target from the "data-target" attribute
+    const target = e.target.dataset.target
+    const $target = document.getElementById(target)
+
+    console.log(e.target)
+    console.log()
+
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    e.target.classList.toggle('is-active')
+    $target.classList.toggle('is-active')
+
+  }
+
   render(){
     return(
       <header>
@@ -12,7 +27,7 @@ class Header extends React.Component {
               <h1 className="title is-1">WineBored</h1>
             </a>
 
-            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" onClick={this.clickHandler}>
+            <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navMenu" onClick={this.clickHandler}>
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
               <span aria-hidden="true"></span>
--- DexDeLeon/winebored/src/components/WineCard.js
+++ DexDeLeon/winebored/src/components/WineCard.js
@@ -4,7 +4,7 @@ const WineCard = ({ name, image, origin, tastingNotes }) => {
   return(
     <div className="card">
       <div className="card-header">
-        <h3 className="card-header-title title is-4">{name}</h3>
+        <h3 className="card-header-title title is-4 is-centered">{name}</h3>
       </div>
       <div className="card-image">
         <figure className="image">
