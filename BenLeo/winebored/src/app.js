a354335c9f8daaee3e60fd3f9a22a10e109e0ca0
Ben Lander <ben.l.lander@googlemail.com>
2019-01-22 15:51:44 +0000

    Added Auth class

--- BenLeo/winebored/src/app.js
+++ BenLeo/winebored/src/app.js
@@ -2,8 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import Home from './components/Home'
+import Login from './components/Auth/Login'
 import WineIndex from './components/WineIndex'
 import WineShow from './components/WineShow'
+
 
 import 'bulma'
 import './style.scss'
@@ -18,11 +20,13 @@ class App extends React.Component {
           <nav>
             <Link to="/">Home</Link>
             <Link to="/wines">Wine Index</Link>
+            <Link to="/login">Login</Link>
           </nav>
 
           <Switch>
             <Route path="/wines/:id" component={WineShow} />
             <Route path="/wines" component={WineIndex} />
+            <Route path="/login" />
             <Route path="/" component={Home} />
           </Switch>
         </main>
