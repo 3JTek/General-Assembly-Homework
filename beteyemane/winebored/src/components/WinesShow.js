28f7071f352382fef93eeafcf768451955dfd10b
byemane <beteyemane@hotmail.co.uk>
2019-01-22 08:14:12 +0000

    completed

--- beteyemane/winebored/src/components/WinesShow.js
+++ beteyemane/winebored/src/components/WinesShow.js
@@ -16,18 +16,26 @@ class WineboardsShow extends React.Component {
 
   render () {
     if (!this.state.wine) return null
-    const { name, image } = this.state.wine
+    const { name, image, tastingNotes, price } = this.state.wine
     return(
       <section className="section">
         <div className='container'>
           <h1 className="title is-size-2">{name}</h1>
           < hr/>
           <div className="columns">
-            <div className="column">
-              <figure className="image">
-                <img src={image} alt={image}/>
-              </figure>
+            <div className="column is-centered">
+              <img src={image} alt={image}/>
             </div>
+
+            <div className="column">
+              <h4 className="title is-4">Tasting Notes</h4>
+              <p>{tastingNotes}</p>
+              <hr />
+              <h4 className="title is-4">Price</h4>
+              <p>{price}</p>
+              <hr />
+            </div>
+
           </div>
         </div>
       </section>
