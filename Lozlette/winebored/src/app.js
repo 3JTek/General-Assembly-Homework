aa0891096c8872abb13faac36b1073521d3bb82e
Lauren Bell <laurengilbert0@gmail.com>
2019-01-22 19:50:26 +0000

    Register and Login forms rendered and some comments added

--- Lozlette/winebored/src/app.js
+++ Lozlette/winebored/src/app.js
@@ -7,8 +7,10 @@ import './style.scss'
 import { BrowserRouter, Link, Switch, Route } from 'react-router-dom'
 
 import Home from './components/Home'
-import WinesIndex from './components/WinesIndex'
-import WinesShow from './components/WinesShow'
+import WinesIndex from './components/Wines/WinesIndex'
+import WinesShow from './components/Wines/WinesShow'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 
 
 class App extends React.Component {
@@ -19,10 +21,14 @@ class App extends React.Component {
           <nav className="links">
             <Link to='/' className="link">Home</Link>
             <Link to='/wines' className="link">Wines Index</Link>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
           </nav>
           <Switch>
             <Route path="/wines/:id" component={WinesShow} />
             <Route path="/wines" component={WinesIndex} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
             <Route exact path="/" component={Home} />
           </Switch>
         </main>
