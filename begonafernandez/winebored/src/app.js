f03a55cd608a555483a4b051a932c1696aa54e17
Begona Fernandez <boadicea1979@yahoo.es>
2019-01-22 16:45:38 +0000

    atyled and added extra links on the navbar

--- begonafernandez/winebored/src/app.js
+++ begonafernandez/winebored/src/app.js
@@ -6,8 +6,8 @@ import 'bulma'
 import './style.scss'
 
 import Home from './components/Home'
-import WinesIndex from './components/WinesIndex'
-import WinesShow from './components/WinesShow'
+import WinesIndex from './components/wines/WinesIndex'
+import WinesShow from './components/wines/WinesShow'
 import NavBar from './components/NavBar'
 
 class App extends React.Component {
