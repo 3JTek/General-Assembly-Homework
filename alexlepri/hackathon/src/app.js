f59ac80074c555c6b0026520bb8f6aa2e0dc109d
Alessandro Lepri <alex.lepri84@gmail.com>
2019-01-23 18:51:26 +0000

    Added mapbox and working

---
+++ alexlepri/hackathon/src/app.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+import Map from './components/Map'
+
+
+import axios from 'axios'
+
+import './scss/style.scss'
+
+
+class App extends React.Component {
+
+  componentDidMount () {
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(res => this.setState({ countries: res.data }))
+
+  }
+
+
+  render() {
+    if (!this.state) return null
+
+    return (
+
+      <main>
+        <Map countries={this.state.countries} zoom={1.5} />
+      </main>
+
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ alexlepri/hackathon/src/components/Map.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+mapboxgl.accessToken = process.env.MAPBOX_TOKEN
+
+
+class Map extends React.Component {
+
+  componentDidMount() {
+
+    this.map = new mapboxgl.Map({
+      container: this.mapDiv,
+      style: 'mapbox://styles/mapbox/streets-v9',
+      zoom: this.props.zoom
+
+    })
+  }
+
+  render () {
+    return (
+      <div className="map" ref={mapDiv => this.mapDiv = mapDiv} />
+    )
+  }
+}
+
+export default Map
