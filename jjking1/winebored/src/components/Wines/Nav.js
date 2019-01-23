f852e51b2106a6128eb301c48df80c83117bd358
joshuaking06 <joshua.king0692@gmail.com>
2019-01-23 16:48:19 +0000

    got files

--- jjking1/winebored/src/components/Wines/Nav.js
+++ jjking1/winebored/src/components/Wines/Nav.js
@@ -3,17 +3,17 @@ import { Link } from 'react-router-dom'
 
 const Nav = () => {
   return(
-        <nav className="navbar has-background-dark" role="navigation" aria-label="main navigation">
+        <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
             <div className="container is-fluid">
               <div id="navbarBasicExample" className="navbar-menu">
                 <div className="navbar-start">
-                    <Link className="navbar-item has-text-white" to="/"> Home </Link>
-                    <Link className="navbar-item has-text-white" to="/wines"> All Wines </Link>
-                    <Link className="navbar-item has-text-white" to="/wines/new">New Wine </Link>
+                    <Link className="navbar-item" to="/"> Home </Link>
+                    <Link className="navbar-item" to="/wines"> All Wines </Link>
+                    <Link className="navbar-item" to="/wines/new">New Wine </Link>
                 </div>
                 <div className="navbar-end">
-                  <Link className="navbar-item has-text-white" to="/register">Register</Link>
-                  <Link className="navbar-item has-text-white" to="/login">Login</Link>
+                  <Link className="navbar-item" to="/register">Register</Link>
+                  <Link className="navbar-item" to="/login">Login</Link>
                 </div>
               </div>
             </div>
