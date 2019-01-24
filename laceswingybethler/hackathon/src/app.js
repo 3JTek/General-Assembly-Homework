498ac5d7ede3061132dedb2a543eeaacb9125096
laceswingybethler <beth.swingler@gmail.com>
2019-01-24 15:14:49 +0000

    User location rendering map AND events. About to test for presence of geolocation in browser.

--- laceswingybethler/hackathon/src/app.js
+++ laceswingybethler/hackathon/src/app.js
@@ -12,25 +12,19 @@ class App extends React.Component {
 
   constructor() {
     super()
-
+    // this.state = {
+    //   userLat: '',
+    //   userLng: ''
+    // }
 
   }
-
-  //get user's location
-  // getUserLocation() {
-  //   navigator.geolocation.getCurrentPosition((pos) => {
-  //     this.userLat = pos.coords.latitude
-  //     this.userLng = pos.coords.longitude
-  //     console.log(this.userLat)
-  //     console.log(this.userLng)
-  //   })
-  // }
 
 
   componentDidMount() {
     navigator.geolocation.getCurrentPosition((pos) => {
       const {latitude, longitude}  = pos.coords
       this.getEvents(latitude, longitude)
+      //this.setState({ userLat: latitude, userLng: longitude })
       console.log(pos.coords)
     })
     //this.getEvents = this.getEvents.bind(this)
@@ -41,10 +35,9 @@ class App extends React.Component {
     const today = new Date(),
       date = today.getFullYear() + '-' + ((today.getMonth()+1) < 9 ? '0'+(today.getMonth()+1) : today.getMonth()) + '-' + (today.getDate() < 9 ? '0' + (today.getDate()) : today.getDate())
 
-    axios.get(`https://cors-anywhere.herokuapp.com/https://www.skiddle.com/api/v1/events/search/?api_key=4bf27d4db7486be5b02c0d7dd0fa06af&latitude=${latitude}&longitude=${longitude}&radius=10&order=distance&description=1&minDate=${date}&maxDate=${date}`)
+    axios.get(`https://cors-anywhere.herokuapp.com/https://www.skiddle.com/api/v1/events/search/?api_key=4bf27d4db7486be5b02c0d7dd0fa06af&latitude=${latitude}&longitude=${longitude}&radius=5&order=distance&description=1&minDate=${date}&maxDate=${date}&limit=100`)
 
-      .then(res => this.setState({ events: res.data }))
-    console.log(latitude, longitude)
+      .then(res => this.setState({ events: res.data, userLat: latitude, userLng: longitude}))
   }
 
   //{{ baseURL  }}&latitude=51.5153&longitude=0.0723&radius=10&order=distance&description=1&minDate=2019-01-23&maxDate=2019-01-25
@@ -52,14 +45,14 @@ class App extends React.Component {
 
   render() {
     if (!this.state) return null
-    //console.log(this.state.events)
+    console.log(this.state.events)
     //console.log(this.state.events.results.venue.name)
     return (
       <main>
         <Map
           events={this.state.events.results}
-          center={{ lat: 51.5, lng: -0.07}}
-          zoom={11}
+          center={{ lat: this.state.userLat, lng: this.state.userLng}}
+          zoom={13}
         />
       </main>
     )
