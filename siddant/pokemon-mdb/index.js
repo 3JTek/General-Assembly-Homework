e5ac0f5acc290b21792ba02387618e7ba221ab5d
Siddant <siddantgurung@hotmail.co.uk>
2019-01-28 18:50:37 +0000

    delete and update added

--- siddant/pokemon-mdb/index.js
+++ siddant/pokemon-mdb/index.js
@@ -23,6 +23,7 @@ app.get('/pokemons', (req, res) =>{
   Pokemon
     .find()
     .then(pokemon => res.status(200).json(pokemon))
+    .catch(err => res.status(404).json(err))
 })
 
 app.post('/pokemons', (req, res) =>{
@@ -37,6 +38,32 @@ app.get('/pokemons/:id', (req, res) =>{
     .findById(req.params.id)
     .then(pokemon => res.status(200).json(pokemon))
     .catch(err => res.status(404).json(err))
+})
+
+app.delete('/pokemons/:id', (req, res) =>{
+  Pokemon
+    .remove({_id: req.params.id})
+    .then(res.status(200).json({message: 'pokemon has been deleted'}))
+    .catch(err => res.status(404).json(err))
+})
+
+app.put('/pokemons/:id', (req, res) =>{
+  Pokemon
+    .update(req.body)
+    .then(pokemon => res.status(200).json(pokemon))
+    .catch(err => res.status(404).json(err))
+})
+
+
+//error handling
+
+app.all('/*', (req, res)=>{
+  res.status(404).json({message: 'Cannot find your request'})
+})
+
+app.use((err, req, res, next)=>{
+  res.status(500).json({ message: 'Server is currently down!' })
+  next(err)
 })
 
 app.listen(4000, ()=> console.log('server has been started at port 4000'))
