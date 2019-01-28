a616d1d61c914b21f613b407d77eec22321eded5
Dexter De Leon <dexdeleon@gmail.com>
2019-01-28 17:35:53 +0000

    Added some console logs and now understanding the Show route a little more.

--- DexDeLeon/node-server/index.js
+++ DexDeLeon/node-server/index.js
@@ -20,18 +20,22 @@ const Guitar = mongoose.model('Guitar', guitarSchema)
 
 app.use(bodyParser.json())
 
+// INDEX ***********************************************************************
 app.get('/guitars', (req, res) => {
   Guitar.find()
     .then(guitars => res.status(200).json(guitars))
 })
 
+// CREATE **********************************************************************
 app.post('/guitars', (req, res) => {
   Guitar.create(req.body)
     .then(guitar => res.status(201).json(guitar))
     .catch(err => res.status(422).json(err.errors))
 })
 
+// SHOW ************************************************************************
 app.get('/guitars/:id', (req, res) => {
+  console.log('req', req.params)
   Guitar.find( { _id: req.params.id } )
     .then(guitar => res.status(200).json(guitar))
     .catch(err => res.status(404).json(err.errors))
