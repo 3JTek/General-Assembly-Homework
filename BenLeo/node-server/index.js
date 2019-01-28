a9aca08ad5aaf13d24b3096b2bd7adf920d7999e
Ben Lander <ben.l.lander@googlemail.com>
2019-01-28 19:36:51 +0000

    POST request working

--- BenLeo/node-server/index.js
+++ BenLeo/node-server/index.js
@@ -4,13 +4,13 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-mongoose.connect('http://localhost/musicapi')
+mongoose.connect('mongodb://localhost/musicapi')
 
 const albumSchema = new mongoose.Schema({
   name: { type: String, required: true},
   artist: { type: String, required: true},
-  year: Number,
-  genre: String,
+  year: { type: Number },
+  genre: { type: String },
   coverArt: { type: String, required: true},
   trackCount: { type: Number, required: true}
 })
@@ -19,8 +19,17 @@ const Album = mongoose.model('Album', albumSchema)
 
 app.use(bodyParser.json())
 
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
+  Album
+    .find()
+    .then(albums => res.status(200).json(albums))
+})
 
+app.post('/albums', (req, res) => {
+  Album
+    .create(req.body)
+    .then(album => res.status(201).json(album))
+    .catch(err => res.status(422).json(err.errors))
 })
 
 app.listen(4000, () => console.log('App is listening on Port 4000'))
