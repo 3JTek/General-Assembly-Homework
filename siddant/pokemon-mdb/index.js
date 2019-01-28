c5bccae3aa3d6cf15f6d386af7637d236d6309ba
Siddant <siddantgurung@hotmail.co.uk>
2019-01-28 15:51:19 +0000

    post request respond

--- siddant/pokemon-mdb/index.js
+++ siddant/pokemon-mdb/index.js
@@ -9,4 +9,8 @@ app.get('/pokemons', (req, res) =>{
   console.log('list of pokemon')
 })
 
+app.post('/pokemons', (req, res) =>{
+  console.log('pokemon has been added')
+})
+
 app.listen(4000, ()=> console.log('server has been started at port 4000'))
