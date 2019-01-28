242a559263c34d8b90b2f1575e5e4144628e9adb
Dexter De Leon <dexdeleon@gmail.com>
2019-01-28 16:18:54 +0000

    Index and Create routes set up.

--- DexDeLeon/node-server/index.js
+++ DexDeLeon/node-server/index.js
@@ -1,5 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://localhost/guitardb')
+
+app.get('/guitars', (req, res) => {
+  res.status(200).json({ route: 'index' })
+})
+
+app.post('/guitars', (req, res) => {
+  res.status(201).json({ route: 'create' })
+})
 
 app.listen(4000, () => console.log('Running on 4000'))
