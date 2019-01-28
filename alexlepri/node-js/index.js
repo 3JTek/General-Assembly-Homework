431897ab12b9892abaeb499306fc12e03c1a3da0
Alessandro Lepri <alex.lepri84@gmail.com>
2019-01-28 15:48:26 +0000

    Added my Schema

--- alexlepri/node-js/index.js
+++ alexlepri/node-js/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const mongoose = require('mongoose')
-// const bodyParser = require('body-parser')
+const bodyParser = require('body-parser')
 
 
 const app = express()
 
 mongoose.connect('mongodb://localhost/player')
+
+const playerSchema = mongoose.Schema({
+  fullName: { type: String, required: true },
+  age: { type: Number},
+  country: { type: String, required: true},
+  DoB: { type: Date, required: true},
+  role: { type: String, required: true},
+  image: { type: String, required: true},
+  salary: { type: Number},
+  currentTeam: { type: String, required: true}
+})
+
+const Player = mongoose.model('Player', playerSchema)
+
+app.use(bodyParser.json())
 
 
 
