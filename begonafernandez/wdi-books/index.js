3054077c32401b1ac1deac22bb255d51b37d0a35
Begona Fernandez <boadicea1979@yahoo.es>
2019-01-28 21:46:49 +0000

    added show route

--- begonafernandez/wdi-books/index.js
+++ begonafernandez/wdi-books/index.js
@@ -21,6 +21,15 @@ app.get('/books', (req, res) => {
     .then(books => res.status(200).json(books))
 })
 
+app.get('/books/:id', (req, res) => {
+  console.log(req.body)
+  Book
+    .find({_id: req.params.id})
+    .then(book => res.status(200).json(book))
+    .catch(err => res.status(404).json(err.message))
+})
+
+
 app.post('/books', (req, res) => {
   Book
     .create(req.body)
