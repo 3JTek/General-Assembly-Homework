95e77a6587e9abd1f776ed017f879987cfa2dcbf
Alessandro Lepri <alex.lepri84@gmail.com>
2019-01-28 16:20:10 +0000

    Posted and working on status 201

--- alexlepri/node-js/index.js
+++ alexlepri/node-js/index.js
@@ -5,16 +5,16 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
-mongoose.connect('mongodb://localhost/player')
+mongoose.connect('mongodb://localhost/Top-Team')
 
-const playerSchema = mongoose.Schema({
+const playerSchema = new mongoose.Schema({
   fullName: { type: String, required: true },
   age: { type: Number},
-  country: { type: String, required: true},
+  country: { type: String},
   DoB: { type: Date, required: true},
   role: { type: String, required: true},
   image: { type: String, required: true},
-  salary: { type: Number},
+  salary: { type: String },
   currentTeam: { type: String, required: true}
 })
 
@@ -22,7 +22,18 @@ const Player = mongoose.model('Player', playerSchema)
 
 app.use(bodyParser.json())
 
+app.get('/players', (req, res) => {
+  Player
+    .find()
+    .then(players => res.status(200).json(players))
+})
 
+app.post('/players', (req, res) => {
+  Player
+    .create(req.body)
+    .then(player => res.status(201).json(player))
+    .catch(err => res.status(422).json(err.errors))
+})
 
 
 app.listen(4000, () => console.log('Express is running on port 4000'))
