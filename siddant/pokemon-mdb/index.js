856afd03b781b674e52efab52a1560256f33ca48
Siddant <siddantgurung@hotmail.co.uk>
2019-01-28 16:24:42 +0000

    get and post request works

--- siddant/pokemon-mdb/index.js
+++ siddant/pokemon-mdb/index.js
@@ -4,13 +4,34 @@ const bodyParser = require('body-parser')
 
 const app =  express()
 
+mongoose.connect('mongodb://localhost/pokemons-db')
+
+const pokemonSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  type: {type: String, required: true},
+  height: {type: Number},
+  weight: {type: Number},
+  gender: {type: String},
+  weakness: {type: String, required: true},
+  owner: {type: String, required: true}
+})
+const Pokemon = mongoose.model('Pokemon',pokemonSchema)
+
+
+app.use(bodyParser.json())
+
+
 
 app.get('/pokemons', (req, res) =>{
-  console.log('list of pokemon')
+  Pokemon
+    .find()
+    .then(pokemon => res.status(200).json(pokemon))
 })
 
 app.post('/pokemons', (req, res) =>{
-  console.log('pokemon has been added')
+  Pokemon
+    .create(req.body)
+    .then(pokemon => res.status(201).json(pokemon))
 })
 
 app.listen(4000, ()=> console.log('server has been started at port 4000'))
