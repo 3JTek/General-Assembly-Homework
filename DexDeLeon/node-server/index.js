67a5e18bf685ca3d0249471fb6d20fc148f5d19f
Dexter De Leon <dexdeleon@gmail.com>
2019-01-28 16:41:53 +0000

    Index route returning an empty array in Insomnia.

--- DexDeLeon/node-server/index.js
+++ DexDeLeon/node-server/index.js
@@ -5,8 +5,21 @@ const app = express()
 
 mongoose.connect('mongodb://localhost/guitardb')
 
+const guitarSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  origin: { type: String, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  body: { type: String },
+  neck: { type: String }
+})
+
+const Guitar = mongoose.model('Guitar', guitarSchema)
+
 app.get('/guitars', (req, res) => {
-  res.status(200).json({ route: 'index' })
+  Guitar.find()
+    .then(guitars => res.status(200).json(guitars))
 })
 
 app.post('/guitars', (req, res) => {
