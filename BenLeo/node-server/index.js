ce44cd9ab1e8a351fcd1917b023329e201cababe
Ben Lander <ben.l.lander@googlemail.com>
2019-01-28 17:49:02 +0000

    Added Mongoose Schema & Model

--- BenLeo/node-server/index.js
+++ BenLeo/node-server/index.js
@@ -4,4 +4,23 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+mongoose.connect('http://localhost/musicapi')
+
+const albumSchema = new mongoose.Schema({
+  name: { type: String, required: true},
+  artist: { type: String, required: true},
+  year: Number,
+  genre: String,
+  coverArt: { type: String, required: true},
+  trackCount: { type: Number, required: true}
+})
+
+const Album = mongoose.model('Album', albumSchema)
+
+app.use(bodyParser.json())
+
+app.get('/', (res, req) => {
+
+})
+
 app.listen(4000, () => console.log('App is listening on Port 4000'))
