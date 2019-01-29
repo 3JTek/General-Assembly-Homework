0b0adf759521a3336c00cd652fa9c33f5f6e36cc
edwardlea <leaedward87@gmail.com>
2019-01-29 07:25:10 +0000

    Final commit

--- edwardlea/bourbon-api/index.js
+++ edwardlea/bourbon-api/index.js
@@ -2,10 +2,13 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+// Creating the Express server
 const app = express()
 
+// Creating the new database/model via Mongoose
 mongoose.connect('mongodb://localhost/bourbon-fun')
 
+// Creating a model schema with Mongoose to provide a blueprint for the data required.
 const bourbonSchema = new mongoose.Schema({
   name: {type: String, required: true},
   manufacturer: {type: String, required: true},
@@ -15,8 +18,10 @@ const bourbonSchema = new mongoose.Schema({
   image: {type: String}
 })
 
+// Creating the collection named Bourbon using the Mongoose schema created above
 const Bourbon = mongoose.model('Bourbon', bourbonSchema)
 
+// Body-Parser added to the serve to be used with middleware
 app.use(bodyParser.json())
 
 // Index request
@@ -43,13 +48,13 @@ app.get('/bourbons/:id',(req, res) => {
 
 // handling errors by the requestor
 app.all('/*', (req, res) => {
-  res.status(404).json({ message: 'Not found' })
+  res.status(404).json({ message: 'Page not found' })
 })
 
 // handling errors with server / code
 app.use((err, req, res, next) => {
 
-  res.status(500).json({ message: 'Oops, this is embarassing!' })
+  res.status(500).json({ message: 'Something has broken' })
   next(err)
 })
 
