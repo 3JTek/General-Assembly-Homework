f1a9aebd476e885aa1b43aab889566247fa392c8
laceswingybethler <beth.swingler@gmail.com>
2019-01-30 17:05:36 +0000

    User JS file complete - pre validation and pre save - about to start auth JS rotes

--- laceswingybethler/starwars-api-lab/models/user.js
+++ laceswingybethler/starwars-api-lab/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -6,7 +7,36 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true }
 })
 
-
+//password confirmation
 userSchema.virtual('passwordConfirmation')
+  .set(function setPasswordConfirmation(passwordConfirmation) {
+    this._passwordConfirmation = passwordConfirmation
+  })
 
+//check passwords match in pre-validation
+userSchema.pre('validate', function checkPasswordsMatch(next) {
+  if (
+    this.isModified('password') &&
+    this._passwordConfirmation !== this.password
+  ) {
+    this.invalidate('passwordConfirmation', 'Passwords do not match')
+  }
+  next()
+})
+
+//pre-save - hash the password
+userSchema.pre('save', function hashPassword(next) {
+  if(this.isModified('password')) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8))
+  }
+  next()
+})
+
+//compare hashed passwords
+userSchema.methods.validatePassword = function(password) {
+  return bcrypt.compareSync(password, this.password)
+}
+
+
+//export
 module.exports = mongoose.model('User', userSchema)
