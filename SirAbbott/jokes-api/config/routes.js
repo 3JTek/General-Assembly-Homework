ada6e424c69ecdc7718d3154c2c5693875e5906e
SirAbbott <tom@iynk.com>
2019-01-30 17:02:40 +0000

    login and authorization done

--- SirAbbott/jokes-api/config/routes.js
+++ SirAbbott/jokes-api/config/routes.js
@@ -1,13 +1,17 @@
 const router = require('express').Router()
 const jokesController = require('../controllers/jokes')
 const authController = require('../controllers/auth')
+const secureRoute = require('../lib/secureRoute')
 
 router.route('/register')
   .post(authController.register)
 
+router.route('/login')
+  .post(authController.login)
+
 router.route('/jokes')
   .get(jokesController.index)
-  .post(jokesController.create)
+  .post(secureRoute, jokesController.create)
 
 router.route('/jokes/:id')
   .get(jokesController.show)
--- SirAbbott/jokes-api/controllers/auth.js
+++ SirAbbott/jokes-api/controllers/auth.js
@@ -1,5 +1,5 @@
 const User = require('../models/user')
-
+const jwt = require('jsonwebtoken')
 
 function registerRoute(req, res) {
   User
@@ -8,8 +8,24 @@ function registerRoute(req, res) {
     .catch(err => res.status(422).json(err.errors))
 }
 
+function loginRoute(req, res) {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if(!user || !user.validatePassword(req.body.password)) {
+        return res.status(401).json({ message: 'Unauthorized' })
+      }
 
+      const payload = { sub: user._id }
+      const token = jwt.sign(payload, process.env.SECRET , { expiresIn: '6h' })
+
+      res.json({
+        token,
+        message: `Welcome back ${user.username}!`
+      })
+    })
+}
 
 module.exports = {
-  register: registerRoute
+  register: registerRoute,
+  login: loginRoute
 }
