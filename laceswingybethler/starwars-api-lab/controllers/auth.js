d9a0b464ffa6547525e761842b5836b015d3424f
laceswingybethler <beth.swingler@gmail.com>
2019-01-30 18:20:33 +0000

    Added secureRoute and fixed problem with missing dot env in Index file

--- laceswingybethler/starwars-api-lab/controllers/auth.js
+++ laceswingybethler/starwars-api-lab/controllers/auth.js
@@ -1,2 +1,33 @@
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+
+//Register
+function registerRoute(req, res) {
+  User
+    .create(req.body)
+    .then(() => res.status(201).json({ message: 'Registered' }))
+    .catch(err => res.status(422).json(err.errors))
+}
+
+//Login
+function loginRoute(req, res) {
+  User.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user || !user.validatePassword(req.body.password)) {
+        return res.status(401).json({ message: 'Not allowed!'})
+      }
+      const payload = { sub: user._id }
+      const token = jwt.sign(payload, process.env.SECRET, { expiresIn: '6h' })
+
+      res.json({
+        token,
+        message: `Welcome back ${user.username}!`
+      })
+    })
+}
+
+
+module.exports = {
+  register: registerRoute,
+  login: loginRoute
+}
