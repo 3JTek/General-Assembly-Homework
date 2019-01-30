3c6d9381c30c4775e974784dffc9c3f8c588238c
Ben Lander <ben.l.lander@googlemail.com>
2019-01-30 17:30:22 +0000

    Initial commit

---
+++ BenLeo/serverside-auth/config/routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router()
+const mugsController = require('../controllers/mugs')
+const authController = require('../controllers/auth')
+
+router.route('/register')
+  .post(authController.register)
+
+router.route('/mugs')
+  .get(mugsController.index)
+  .post(mugsController.create)
+
+router.route('/mugs/:id')
+  .get(mugsController.show )
+  .put(mugsController.update)
+  .delete(mugsController.delete)
+
+module.exports = router
---
+++ BenLeo/serverside-auth/models/user.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+// const bcrypt = require('bcrypt')
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+})
+
+module.exports = mongoose.model('User', userSchema)
