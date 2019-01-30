b0ea3d82d748d9c6c6b1cc7bfc66b28b878c7459
Alessandro Lepri <alex.lepri84@gmail.com>
2019-01-30 19:30:11 +0000

    Added update function on pastas.js and install dotenv

--- alexlepri/pasta-API/controller/pastas.js
+++ alexlepri/pasta-API/controller/pastas.js
@@ -1,10 +1,14 @@
 const Pasta = require('../models/pasta')
 
-function indexRoute(req, res){
+function indexRoute(req, res) {
+
+  const { fields, ...query} = req.query
+  const select = fields ? fields.split(',') : []
+
   Pasta
-    .find()
-    .then( pastas => res.status(200).json(pastas))
-}
+    .find(query)
+    .select(select)
+    .then(pastas => res.status(200).json(pastas))
 
 function createRoute(req, res){
   Pasta
@@ -13,12 +17,20 @@ function createRoute(req, res){
     .catch( err => res.status(422).json(err.errors) )
 }
 
-function showRoute(req, res){
+function showRoute(req, res) {
   Pasta
     .findById(req.params.id)
     .then(pasta => res.status(200).json(pasta))
-    .catch( err => res.status(422).json(err.errors) )
+    .catch( err => res.status(422).json(err.errors))
+}
 
+function updateRoute (req, res) {
+  Pasta
+    .findById(req.params.id)
+    .then(pasta => pasta.set(req.body))
+    .then(pasta => pasta.save())
+    .then(pasta => res.status(200).json(pasta))
+    .catch(err => res.status(422).json(err.errors))
 }
 
 function deleteRoute(req, res) {
@@ -29,11 +41,10 @@ function deleteRoute(req, res) {
     .catch(err => res.status(422).json(err.errors))
 }
 
-
-
 module.exports = {
   index: indexRoute,
   create: createRoute,
   show: showRoute,
-  delete: deleteRoute
+  delete: deleteRoute,
+  update: updateRoute
 }
