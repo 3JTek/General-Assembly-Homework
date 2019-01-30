c7e0feeee5f7d64a2402ccbe75fa7b6863938ca6
laceswingybethler <beth.swingler@gmail.com>
2019-01-30 16:44:23 +0000

    Initial commit

---
+++ laceswingybethler/starwars-api-lab/index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const express = require('express')
+const bodyParser = require('body-parser')
+const router = require('./config/router')
+
+const app = express()
+
+mongoose.connect('mongodb://localhost/starwars-api-lab')
+
+app.use(bodyParser.json())
+
+app.use(router)
+
+app.listen(4000, () => console.log('Express is running on port 4000'))
