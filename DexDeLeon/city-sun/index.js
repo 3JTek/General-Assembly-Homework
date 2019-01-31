b8e248cd96728d29ae696721ebdc224bc7e2f510
Dexter De Leon <dexdeleon@gmail.com>
2019-01-31 21:36:23 +0000

    Request in express logging long and lat of London to console.

--- DexDeLeon/city-sun/index.js
+++ DexDeLeon/city-sun/index.js
@@ -1,6 +1,20 @@
 require('dotenv').config()
 
 const express = require('express')
+const rp = require('request-promise')
+
 const app = express()
+
+const options = {
+  url: 'https://api.opencagedata.com/geocode/v1/json',
+  qs: {
+    key: process.env.OPENCAGE_KEY,
+    q: 'London'
+  }
+}
+
+rp(options)
+  .then(res => console.log(JSON.parse(res).results[0].geometry))
+
 
 app.listen(4000, () => console.log('Express running on 4000'))
