100850430b5e48f5a05ced9bd8df4a1a8854c2a4
laceswingybethler <beth.swingler@gmail.com>
2019-01-31 23:44:06 +0000

    Get request is returning weather data. Currently just a summary. Need to create a function to extract the forecast in proper format, and the lng and lat from the given city.

--- laceswingybethler/city-sun/controllers/forecast.js
+++ laceswingybethler/city-sun/controllers/forecast.js
@@ -1,4 +1,5 @@
 const key = process.env.DARK_SKY_TOKEN
+//const key = '6b5fbb37728b23fd9090339b5a975f19'
 const rp = require('request-promise')
 
 
@@ -7,8 +8,16 @@ function forecast(req, res) {
   console.log(city)
   const lat = 42.3601
   const lng = -71.0589
+  console.log(`https://api.darksky.net/forecast/${key}/${lat},${lng}`)
   return rp(`https://api.darksky.net/forecast/${key}/${lat},${lng}`)
+
+    .then((textObj) => {
+      const data = JSON.parse(textObj)
+      console.log(data.daily.summary)
+    })
+
     .then(() => res.status(200).json(`Your forecast in ${city} has been requested`))
+
     .catch(err => res.status(422).json(err.message))
 }
 
