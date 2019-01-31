d19a61a691d9be775b8a76ec1972696072601948
James Benson <you@example.com>
2019-01-31 17:44:09 +0000

    Made API requests to DarkSky and OpenCage

--- jjbenson/city-sun/index.js
+++ jjbenson/city-sun/index.js
@@ -11,15 +11,31 @@ const darkSkyKey = process.env.DARKSKY_KEY
 const openCageKey = process.env.OPENCAGE_KEY
 
 
-rp('https://api.opencagedata.com/geocode/v1/json',{
-  qs: {
-    key: openCageKey,
-    pretty: 1,
-    no_annotations: 1,
-    q: 'Croydon'
-  },
-  json: true
-}).then( (data) => console.log(data))
+app.get('/forecast', (req, res) => {
+  rp('https://api.opencagedata.com/geocode/v1/json',{
+    qs: {
+      key: openCageKey,
+      pretty: 1,
+      no_annotations: 1,
+      q: 'Croydon, UK'
+    },
+    json: true
+  })
+    .then( (data) => {
+      const {lng, lat} = data.results[0].geometry
+      console.log(lng, lat)
+      rp(`https://api.darksky.net/forecast/${darkSkyKey}/${lng},${lat}`)
+        .then(weatherData => {
+          weatherData = JSON.parse(weatherData)
+          const dailyData = weatherData.daily.data.map( (day)=>{
+            const {time, summary, icon, temperatureHigh, temperatureLow} = day
+            return { time, summary, icon, temperatureHigh, temperatureLow }
+          } )
+          console.log(dailyData)
+          res.json(dailyData)
+        })
+    })
+})
 
-
+app.listen(4000, () => console.log('Express is running on port 4000'))
 //https://api.opencagedata.com/geocode/v1/json?key=05d1ee367fb14936a8372232782c01f1&pretty=1&no_annotations=1&q=Croydon
