e85959b1ed765354bc2a488c574d8b8029ef9110
edwardlea <leaedward87@gmail.com>
2019-01-31 18:38:41 +0000

    weather data nested in first request

--- edwardlea/city-sun/index.js
+++ edwardlea/city-sun/index.js
@@ -8,48 +8,7 @@ const app = express()
 
 app.use(bodyParser.json())
 
-// function forecastRoute(req, res){
-//   const options = {
-//     uri: 'https://api.opencagedata.com/geocode/v1/json?',
-//     qs: {
-//       key: process.env.GEO_TOKEN,
-//       q: req.query.city
-//
-//     },
-//     json: true
-//   }
-
-  // rp(options)
-  //   .then(data => {
-  //     console.log(data.results[0].geometry)
-  //     getForecast(data.results[0].geometry)
-    // })
-    // .then(res.status(200).json({message: 'Message received'}) )
-
-
-//     .catch(err => console.log(err))
-// }
-
-// function getForecast(location){
-//   const options = {
-//     uri: `https://api.darksky.net/forecast/${process.env.DARKSKY_TOKEN}/${location.lat}/${location.lng}`,
-//     json: true
-//   }
-//
-//   rp(options)
-//     .then(data => {
-//       console.log(data)
-//       res.status(200).json({data})
-//     })
-//
-//
-//
-//     .catch(err => console.log(err))
-// }
-
-
-
-app.get('/forecast', (req, res, next) =>{
+app.get('/forecast', (req, res ) =>{
   rp({
     uri: 'https://api.opencagedata.com/geocode/v1/json?',
     qs: {
@@ -59,21 +18,23 @@ app.get('/forecast', (req, res, next) =>{
     json: true
   })
     .then(data => {
-      console.log(data.results[0].geometry)
-      const lat = data.results[0].geometry.lat
-      const lng = data.results[0].geometry.lng
+      const {lat, lng} = data.results[0].geometry
       const token = process.env.DARKSKY_TOKEN
-      console.log(token)
-      rp(`https://api.darksky.net/forecast/${token}/${lat},${lng}`
-
-        // `https://api.darksky.net/forecast/${token}/${data.results[0].geometry.lat},${data.results[0].geometry.lng}`
-      )
+      rp(`https://api.darksky.net/forecast/${token}/${lat},${lng}`)
         .then((data) => {
-          console.log(JSON.parse(data).hourly)
-          res.status(200).json( JSON.parse(data) )
+          const weatherData = JSON.parse(data).daily
+
+          const {summary, icon } = weatherData
+
+          const dataNew = weatherData.data.map(date => {
+            const {time, summary, icon, temperatureHigh, temperatureLow} = date
+            return {time, summary, icon, temperatureHigh, temperatureLow}
+          })
+          const displayData = { summary, icon, dataNew }
+
+          res.status(200).json( displayData )
         })
         .catch(err => console.log(err))
-        // getForecast(data.results[0].geometry)
     })
 
 })
