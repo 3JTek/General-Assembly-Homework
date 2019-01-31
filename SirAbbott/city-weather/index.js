36d60cbbd3405fe51c0b5e5b4181474c4b55e408
SirAbbott <tom@iynk.com>
2019-01-31 18:05:34 +0000

    both api working and returning

--- SirAbbott/city-weather/index.js
+++ SirAbbott/city-weather/index.js
@@ -1,1 +1,30 @@
 require('dotenv').config()
+
+const express = require('express')
+const rp = require('request-promise')
+
+const app = express()
+
+const geoApi = process.env.OPEN_CAGE_API
+const weatherApi = process.env.DARK_SKY_API
+
+app.get('/forecast', (req, res) => {
+  rp('https://api.opencagedata.com/geocode/v1/json?',{
+    qs: {
+      key: geoApi,
+      q: req.query.city
+    },
+    json: true
+  })
+    .then( data => {
+      const { lat, lng } = data.results[0].geometry
+      console.log(lat, lng)
+      rp(`https://api.darksky.net/forecast/${weatherApi}/${lat},${lng}`)
+        .then(data => res.status(200).json(JSON.parse(data).daily.data))
+    })
+})
+
+
+
+
+app.listen(4000, () => console.log('Express is running on port 4000'))
