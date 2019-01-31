0e8627d5a18160d6d7e6f6632182f55b3b598d35
Dexter De Leon <dexdeleon@gmail.com>
2019-01-31 22:02:58 +0000

    Request to Dark Sky logging the daily forecast for the week with hardcoded London coordinates.

--- DexDeLeon/city-sun/index.js
+++ DexDeLeon/city-sun/index.js
@@ -5,16 +5,29 @@ const rp = require('request-promise')
 
 const app = express()
 
-const options = {
+const openCageOptions = {
   url: 'https://api.opencagedata.com/geocode/v1/json',
   qs: {
     key: process.env.OPENCAGE_KEY,
     q: 'London'
+  },
+  json: true
+}
+
+rp(openCageOptions)
+  .then(res => console.log(res.results[0].geometry))
+  .catch(err => console.log(err.message))
+
+const darkSkyOptions = {
+  url: `https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/51.5074,0.1278`,
+  qs: {
+    units: 'uk2',
+    exclude: 'currently,minutely,hourly,alerts,flags'
   }
 }
 
-rp(options)
-  .then(res => console.log(JSON.parse(res).results[0].geometry))
-
+rp(darkSkyOptions)
+  .then(res => console.log(JSON.parse(res).daily))
+  .catch(err => console.log(err.message))
 
 app.listen(4000, () => console.log('Express running on 4000'))
