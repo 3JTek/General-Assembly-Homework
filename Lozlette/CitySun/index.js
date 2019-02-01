d83b0235805214c1d522e6f1f46da56991d4affc
Lauren Bell <laurengilbert0@gmail.com>
2019-02-01 08:36:40 +0000

    last commit

--- Lozlette/CitySun/index.js
+++ Lozlette/CitySun/index.js
@@ -2,12 +2,10 @@ require('dotenv').config()
 const rp = require('request-promise')
 const express = require('express')
 const app = express()
-// const bodyParser = require('body-parser')
-//
-// app.use(bodyParser.json())
 
 
-app.get('/forecast', (req,res) => {
+
+app.get('/weather', (req,res) => {
   rp.post(`https://api.darksky.net/forecast/${process.env.DARKSKY_KEY}/${req.body.latitude},${req.body.longitude}`)
     .then(data => res.status(200).json(JSON.parse(data)))
     .catch(err => console.log(err))
