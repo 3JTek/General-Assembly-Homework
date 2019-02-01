12fa7ff1eb31c526562633cb87e84dec4518c899
edwardlea <leaedward87@gmail.com>
2019-02-01 07:39:38 +0000

    Comments added to code

--- edwardlea/city-sun/index.js
+++ edwardlea/city-sun/index.js
@@ -1,14 +1,16 @@
 require('dotenv').config()
 
 const express = require('express')
-const bodyParser = require('body-parser')
 const rp = require('request-promise')
+// const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(bodyParser.json())
+// app.use(bodyParser.json())
 
+// HTTP route for GET forecast, which uses the city query to string
 app.get('/forecast', (req, res ) =>{
+  // HTTP request made to access lat and lng data based on city query string included in URL
   rp({
     uri: 'https://api.opencagedata.com/geocode/v1/json?',
     qs: {
@@ -17,24 +19,31 @@ app.get('/forecast', (req, res ) =>{
     },
     json: true
   })
+  // Returned data from first HTTP requested in then used make a second HTTP request using the lat and lng data.
     .then(data => {
       const {lat, lng} = data.results[0].geometry
       const token = process.env.DARKSKY_TOKEN
-      rp(`https://api.darksky.net/forecast/${token}/${lat},${lng}`)
+      rp({
+        uri: `https://api.darksky.net/forecast/${token}/${lat},${lng}`,
+        json: true
+      })
         .then((data) => {
-          const weatherData = JSON.parse(data).daily
-
+          // returned data from HTTP requested is used to extract daily weather report data
+          const weatherData = data.daily
+          // Summary and icon data made variables
           const {summary, icon } = weatherData
 
-          const dataNew = weatherData.data.map(date => {
+          // Data object mapped over to keep only required fields, saved to new variable
+          const reducedData = weatherData.data.map(date => {
             const {time, summary, icon, temperatureHigh, temperatureLow} = date
             return {time, summary, icon, temperatureHigh, temperatureLow}
           })
-          const displayData = { summary, icon, dataNew }
-
+          // new object is created using the summary, icon and reducedData objects
+          const displayData = { summary, icon, reducedData }
+          // data is returned to user in new format
           res.status(200).json( displayData )
         })
-        .catch(err => console.log(err))
+        .catch(() => res.status(400).json( {message: 'Error, please try again'} ))
     })
 
 })
