36cb12e725f3a5fad1d0c9e8188c84188eca86d5
Begona Fernandez <boadicea1979@yahoo.es>
2019-02-01 22:24:20 +0000

    added a hero image with a title

--- begonafernandez/react-sms-translate/src/app.js
+++ begonafernandez/react-sms-translate/src/app.js
@@ -7,7 +7,15 @@ import 'bulma'
 class App extends React.Component{
   render() {
     return(
-      <h1 className="title is-1">Aqui</h1>
+      <section className="hero is-dark">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title is-1">The SMS translater</h1>
+            <p>Want to send a message to a friend that speaks another language?</p>
+            <p>Let us help</p>
+          </div>
+        </div>
+      </section>
     )
   }
 }
