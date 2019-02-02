821e0dfa8b6720d865ef1a9fb87e959568c2ac91
Gessy90 <gessicasantoro90@hotmail.it>
2019-02-02 15:54:33 +0000

    all packages installed and localhost8000 all working

---
+++ Gessy90/react-sms-translate/src/app.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// import axios from 'axios'
+
+import 'bulma'
+class App extends React.Component {
+  constructor(){
+    super()
+
+  }
+  render(){
+    return (
+      <main>
+        <div>
+
+
+        </div>
+      </main>
+    )
+  }
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
