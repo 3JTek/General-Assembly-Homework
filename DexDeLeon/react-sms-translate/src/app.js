a18a87e197db6f2d54cea90e71403a9d20dcf58d
Dexter De Leon <dexdeleon@gmail.com>
2019-02-02 16:29:26 +0000

    Axios used to get list of languages from Yandex and saved in state.

--- DexDeLeon/react-sms-translate/src/app.js
+++ DexDeLeon/react-sms-translate/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import axios from 'axios'
 
 import './style.scss'
 
@@ -7,25 +8,44 @@ class App extends React.Component {
   constructor(){
     super()
 
-    this.state = {}
+    this.state = {
+      message: ''
+    }
 
     this.changeHandler = this.changeHandler.bind(this)
+    this.submitHandler = this.submitHandler.bind(this)
+  }
+
+  componentDidMount(){
+    axios.get('https://translate.yandex.net/api/v1.5/tr.json/getLangs', {
+      params: {
+        key: process.env.YANDEX_KEY,
+        ui: 'en'
+      }
+    })
+      .then(res => this.setState({ langs: res.data.langs }))
+      .catch(err => console.error(err.message))
   }
 
   changeHandler({ target: { value } }){
-    this.setState({ value })
+    this.setState({ message: value })
+  }
+
+  submitHandler(e){
+    e.preventDefault()
+    console.log(this.state.message)
   }
 
   render(){
     return (
       <div>
         <h1>React SMS</h1>
-        <form>
+        <form onSubmit={this.submitHandler}>
           <input
-            name="message"
             value={this.state.message}
             onChange={this.changeHandler}
           />
+          <button>Send Message</button>
         </form>
       </div>
     )
--- DexDeLeon/react-sms-translate/webpack.config.js
+++ DexDeLeon/react-sms-translate/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   entry: './src/app.js',
@@ -23,6 +24,7 @@ module.exports = {
     watchContentBase: true
   },
   plugins: [
+    new Dotenv(),
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
