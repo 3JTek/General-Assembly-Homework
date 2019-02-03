67ae09b4dc99c8b3525b72136210662c3eba6ac6
Gessy90 <gessicasantoro90@hotmail.it>
2019-02-03 14:58:40 +0000

    finished

--- Gessy90/react-sms-translate/src/components/Form.js
+++ Gessy90/react-sms-translate/src/components/Form.js
@@ -2,22 +2,32 @@ import React from 'react'
 
 const Form = ({ handleSubmit, handleChange }) => {
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label>Number</label>
-        <input name="to" placeholder="Number" onChange={handleChange} />
-      </div>
-      <div>
-        <label>Enter text here</label>
-        <input name="message" placeholder="Type your text" onChange={handleChange} />
-      </div>
-      <div>
-        <label>Enter language</label>
-        <input name="lang" placeholder="Language" onChange={handleChange} />
-      </div>
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label className="label">Number</label>
+          <input className="input is-danger" name="to" placeholder="Number" onChange={handleChange} />
+        </div>
+        <div>
+          <label className="label">Enter text here</label>
+          <input className="input is-danger" name="message" placeholder="Type your text" onChange={handleChange} />
+        </div>
+        <div>
+          <label onChange={handleChange}>
+          Pick the Language:
+            <select name="lang" >
+              <option value="en">English</option>
+              <option value="fr">French</option>
+              <option value="it">Italian</option>
+              <option value="es">Spanish</option>
+              <option value="ru">Russian</option>
+            </select>
+          </label>
+        </div>
 
-      <button>Submit</button>
-    </form>
+        <button className="button is-primary">Submit</button>
+      </form>
+    </div>
   )
 }
 export default Form
