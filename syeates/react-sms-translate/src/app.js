b21518b58cb2465d1297c57007aacb1109f87e81
seb yeates <yeatessa@gmail.com>
2019-02-03 19:33:36 +0000

    It's alive and all hail bulma with it's framework

--- syeates/react-sms-translate/src/app.js
+++ syeates/react-sms-translate/src/app.js
@@ -1,36 +1,42 @@
-require('dotenv').config()
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './style.scss'
-
-app.use(bodyPaser.json())
+import Form from './components/Form'
 import axios from 'axios'
+import './style.scss'
+import 'bulma'
+
+
 
 class App extends React.Component {
   constructor(){
     super()
 
     this.state = {}
+
+
+    this.handlechange= this.handlechange.bind(this)
+    this.handlesubmit= this.handlesubmit.bind(this)
+  }
+  handlesubmit() {
+    axios.post('/api/message', this.state)
+  }
+  handlechange({ target: {name, value} }){
+    this.setState({ [name]: value })
   }
 
-  componentDidMount(){
-   axios.get('/api/message',{
-   })
-     .then(res => this.setState({ message: res.data }))
- }
 
+  render(){
+    return (
+      <div>
+        <Form
+          handlechange={this.handlechange}
+          handlesubmit={this.handlesubmit}
+        />
+      </div>
+    )
+  }
 }
- render() {
- if(!this.state.forecast) return null
-     return (
-       <div>
-      
-       </div>
-     )
-   }
- }
-
- ReactDOM.render(
-   <App />,
-   document.getElementById('root')
- )
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
