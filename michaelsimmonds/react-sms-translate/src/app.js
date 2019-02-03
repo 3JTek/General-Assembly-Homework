91e77852b877c5f26fec29e8e3575987c8bdb1c5
Michael Simmonds <michaelsimmonds92@hotmail.com>
2019-02-03 14:51:28 +0000

    Have the setup done

---
+++ michaelsimmonds/react-sms-translate/src/app.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import moment from 'moment'
+import bulma from 'bulma'
+
+// import './style.css'
+
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {}
+  }
+
+  render() {
+    return(
+      <h1>Hi</h1>
+    )
+  }
+
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
