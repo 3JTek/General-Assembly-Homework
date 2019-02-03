abd9abd0cdc26010ba108c7b6f35943e1659559b
seb yeates <yeatessa@gmail.com>
2019-02-03 17:45:42 +0000

    new components added

--- syeates/react-sms-translate/src/app.js
+++ syeates/react-sms-translate/src/app.js
@@ -1,38 +1,36 @@
-const express = require('express')
-const bodyPaser = require('body-parser')
-const rp = require('request-promise')
-const app = express()
-
-
-const accountSid = process.env.accountSid
-const authToken = process.env.authToken
-const twilio = require('twilio')(accountSid, authToken)
-
+require('dotenv').config()
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.scss'
 
 app.use(bodyPaser.json())
+import axios from 'axios'
 
-app.route('/message')
-  .post((req, res) => {
-    rp({
-      url: 'https://translate.yandex.net/api/v1.5/tr.json/translate',
-      qs: {
-        key: process.env.yandexKey,
-        text: req.body.text,
-        lang: req.body.lang
-      },
-      json: true
-    })
-      .then(response => {
-        console.log(response)
-        return twilio.messages
-          .create({
-            body: response.text[0],
-            from: '+447480485825',
-            to: '+447747034495'
-          })
-          .then(() => res.json({ message: 'SMS sent' }))
-          .catch(err => console.log(err))
-      })
-  })
+class App extends React.Component {
+  constructor(){
+    super()
 
-app.listen(4000, () => console.log('server up'))
+    this.state = {}
+  }
+
+  componentDidMount(){
+   axios.get('/api/message',{
+   })
+     .then(res => this.setState({ message: res.data }))
+ }
+
+}
+ render() {
+ if(!this.state.forecast) return null
+     return (
+       <div>
+      
+       </div>
+     )
+   }
+ }
+
+ ReactDOM.render(
+   <App />,
+   document.getElementById('root')
+ )
