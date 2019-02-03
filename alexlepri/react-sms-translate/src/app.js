4c09b481e5b2dc54ee4ed3515e7a9fcb0f7d4c1c
Alessandro Lepri <alex.lepri84@gmail.com>
2019-02-03 12:12:42 +0000

    All working, message has been sent and traslation is working

--- alexlepri/react-sms-translate/src/app.js
+++ alexlepri/react-sms-translate/src/app.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Form from './components/form'
 
-// import axios from 'axios'
+import axios from 'axios'
 
 
 
@@ -10,17 +10,36 @@ class App extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      message: 'Ciao World'
-    }
+    this.state = {}
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
   }
 
-  render() {
+  handleChange({ target: { value } }) {
+    this.setState({ message: value })
+  }
 
+  handleSubmit(e) {
+    e.preventDefault()
+    this.setState({ message: null })
+    axios.post('/api/message', {
+      message: this.state.message,
+      lang: 'it',
+      to: '+447450643047'
+    })
+
+  }
+
+
+  render() {
     return (
+
       <Form
         handleChange={this.handleChange}
         handleSubmit={this.handleSubmit}
+        // message={this.state.message}
+        // phone={this.state.to}
       />
     )
   }
