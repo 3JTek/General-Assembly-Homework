7d0c4354c5c87cd9072c0bc65b4ad9964fd55066
Ben Lander <ben.l.lander@googlemail.com>
2019-02-05 16:03:39 +0000

    Create route working with secureRoute

--- BenLeo/embedded-reference-lab/config/routes.js
+++ BenLeo/embedded-reference-lab/config/routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 
+const secureRoute = require('../lib/secureRoute')
+
 const authController = require('../controllers/authController')
 const blogPostsController = require('../controllers/blogPostsController')
 
@@ -11,7 +13,7 @@ router.post('/login', authController.login)
 //GET ALL BLOG POSTS
 router.get('/blog', blogPostsController.index)
 //CREATE A NEW BLOG POSTS >>> SECURE
-router.post('/blog', blogPostsController.create)
+router.post('/blog', secureRoute, blogPostsController.create)
 
 //CREATE NEW BLOG COMMENT >>> SECURE
 router.post('/blog/comments')
