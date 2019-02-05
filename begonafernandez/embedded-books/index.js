be924f6f6ba021860992e209aee33e682389706a
Begona Fernandez <boadicea1979@yahoo.es>
2019-02-05 22:58:26 +0000

    added frontend index page

--- begonafernandez/embedded-books/index.js
+++ begonafernandez/embedded-books/index.js
@@ -10,6 +10,9 @@ const routes = require('./config/routes')
 
 mongoose.connect(process.env.MONGODB_URI)
 
+app.use(express.static(`${__dirname}/dist`))
+
 app.use(bodyParser.json())
 app.use('/api', routes)
+app.get('/*',  (req, res) => res.sendFile(`${__dirname}/dist/index.html`))
 app.listen(process.env.PORT, () => console.log(`Express is serving the dist folder on port ${process.env.PORT}`))
