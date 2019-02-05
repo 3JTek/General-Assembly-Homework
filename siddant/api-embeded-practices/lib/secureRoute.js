8997a891d1738cfefd83fe26f27689d58d92ba1a
Siddant <siddantgurung@hotmail.co.uk>
2019-02-05 17:03:21 +0000

    authorization completed

--- siddant/api-embeded-practices/lib/secureRoute.js
+++ siddant/api-embeded-practices/lib/secureRoute.js
@@ -4,10 +4,10 @@ const User = require('../models/user')
 
 Promise.promisifyAll(jwt)
 
-function secureRoute(req,res,next){
-  if(!req.header.authorization) res.status(401).json({ message: 'No token set' })
+function secureRoute(req, res, next){
+  if(!req.headers.authorization) res.status(401).json({ message: 'No token set' })
 
-  const token = req.header.authorization.replace('Bearer ', '')
+  const token = req.headers.authorization.replace('Bearer ', '')
 
   jwt
     .verifyAsync(token, process.env.SECRET)
@@ -18,6 +18,7 @@ function secureRoute(req,res,next){
     })
     .then(user => {
       if(!user)throw new Error('Invalid token')
+      req.currentUser = user
       next()
     })
     .catch(err => res.status(401).json({ message: err.message }))
--- siddant/api-embeded-practices/models/user.js
+++ siddant/api-embeded-practices/models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  password: { type: Number, required: true },
+  password: { type: String, required: true },
   email: { type: String, required: true }
 })
 
