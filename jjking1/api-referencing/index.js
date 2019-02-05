db160f682d212686b5a01a1b33623f70396406b5
joshuaking06 <joshua.king0692@gmail.com>
2019-02-05 17:22:57 +0000

    got comments working

---
+++ jjking1/api-referencing/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const app = express()
+const routes = require('./config/routes')
+
+mongoose.connect(process.env.MONGODB_URI)
+
+app.use(bodyParser.json())
+
+app.use('/api', routes)
+
+app.listen(process.env.PORT, () => console.log(`express up on Port ${process.env.PORT}`))
