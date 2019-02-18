5ff99185e01cc2d034b1fb3a2a9ddc49f8db5e42
Alessandro Lepri <alex.lepri84@gmail.com>
2019-02-18 16:14:54 +0000

    Passed first test

--- alexlepri/python-problems-101/problems.py
+++ alexlepri/python-problems-101/problems.py
@@ -1,7 +1,7 @@
 # write a function that returns "Hello World!" if no argument is given, or "Hello <argument>!" otherwise
 # eg: hello() => "Hello World!"; hello("Mike") => "Hello Mike!"
 def hello(string='World'):
-    pass
+    return f'Hello {string}!'
 
 
 # write a function that will calculate the area of a circle, given the radius
