6b4cec0b5f0577ffde24861f2066cda06ea077c8
James Benson <you@example.com>
2019-02-18 18:29:45 +0000

    Done some problems a different way

--- jjbenson/python-problems-101/problems.py
+++ jjbenson/python-problems-101/problems.py
@@ -1,14 +1,18 @@
 import math
+import re
 
 # write a function that returns "Hello World!" if no argument is given, or "Hello <argument>!" otherwise
 # eg: hello() => "Hello World!"; hello("Mike") => "Hello Mike!"
 def hello(string='World'):
-    return f'Hello {string}!'
+    # return f'Hello {string}!'
+    return 'Hello %s!' % string
 
 
 # write a function that will calculate the area of a circle, given the radius
 def area_of_circle(radius):
-    return math.pi*radius*radius
+    # return math.pi*radius*radius
+    # return math.pi*math.pow(radius, 2)
+    return math.pi*radius**2
 
 
 # write a function to convert celcius to farenheit
@@ -19,24 +23,28 @@ def celcius_to_farenheit(celcius):
 
 # write a function that will reverse a number (eg. 456733 become 337654)
 def number_reverse(number):
-    output = ''
-    for char in reversed(str(number)):
-        output = output + char
+    # output = ''
+    # for char in reversed(str(number)):
+    #     output = output + char
+    #
+    # return float(output)
 
-    return float(output)
-    # return reversed(''+str(number))
-
+    return float(''.join(reversed(str(number))))
 
 # write a function to check if a word or phrase is a palindrome returning a boolean
 # eg. palindrome_check('dad') => True, palindrome('nonsense') => False
 def palindrome_check(string):
+    # string = string.replace(' ','')
+    # rev_string = ''
+    #
+    # for char in reversed(string):
+    #     rev_string = rev_string + char
+    #
+    # return string == rev_string
+
     string = string.replace(' ','')
-    rev_string = ''
 
-    for char in reversed(string):
-        rev_string = rev_string + char
-
-    return string == rev_string
+    return string == ''.join(reversed(string))
 
 
 # write a function that returns the letters of a word or phrase in alphabetical order case insensitive
@@ -55,13 +63,18 @@ def title_case(string):
 # 'y' should not be considered a vowel
 # eg: num_of_vowels('Yellow Submarine') => 6
 def num_of_vowels(string):
-    count = 0
+    # count = 0
+    #
+    # for char in string.lower():
+    #     if char in 'aeiou':
+    #         count += 1
+    #
+    # return count
 
-    for char in string.lower():
-        if char in 'aeiou':
-            count += 1
+    #REGEX!
+    match = re.findall(r'[aeiou]', string)
+    return len(match)
 
-    return count
 
 # write a function that frames a string in asterisks (*)
 #                             ***************
