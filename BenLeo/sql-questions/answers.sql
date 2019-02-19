e66345f450a2e6e3922ed198f121c929f0b9b058
Ben Lander <ben.l.lander@googlemail.com>
2019-02-19 16:34:06 +0000

    Completed 1-10

--- BenLeo/sql-questions/answers.sql
+++ BenLeo/sql-questions/answers.sql
@@ -1,25 +1,42 @@
 -- 1. Select the names of all products that are not on sale.
-SELECT * FROM products WHERE on_sale=False
+SELECT name FROM products WHERE on_sale=False;
 
 -- 2. Select the names of all products that cost less than £20.
+SELECT name FROM products WHERE price<20;
 
 -- 3. Select the name and price of the most expensive product.
+SELECT name, price FROM products ORDER BY price DESC LIMIT 1;
 
 -- 4. Select the name and price of the second most expensive product.
+SELECT name, price FROM products ORDER BY price DESC LIMIT 1 OFFSET 1;
 
 -- 5. Select the name and price of the least expensive product.
+SELECT name, price FROM products ORDER BY price LIMIT 1;
 
 -- 6. Select the names and prices of all products, ordered by price in descending order.
+SELECT name, price FROM products ORDER BY price DESC;
 
 -- 7. Select the average price of all products.
+SELECT
+  ROUND(AVG(price)) avg_price
+FROM products;
 
 -- 8. Select the sum of the price of all products.
+SELECT
+  ROUND(SUM(price)) total_price
+FROM products;
 
 -- 9. Select the sum of the price of all products whose prices is less than £20.
+SELECT
+  ROUND(SUM(price)) total_price
+FROM products
+WHERE price<20;
 
 -- 10. Select the id of the user with the name "Alex Chin".
+SELECT id FROM users WHERE name='Alex Chin';
 
 -- 11. Select the names of all users whose names start with the letter "E".
+SELECT name FROM users WHERE name LIKE 'E%';
 
 -- 12. Select the number of users whose first names are "Benjamin".
 
