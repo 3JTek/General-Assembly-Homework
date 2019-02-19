60fc6e3771049eb3587a7e4768ecaac374cf5e6d
Ben Lander <ben.l.lander@googlemail.com>
2019-02-19 16:00:31 +0000

    Initial commit

---
+++ BenLeo/sql-questions/answers.sql
@@ -0,0 +1,42 @@
+-- 1. Select the names of all products that are not on sale.
+SELECT * FROM products WHERE on_sale=False
+
+-- 2. Select the names of all products that cost less than £20.
+
+-- 3. Select the name and price of the most expensive product.
+
+-- 4. Select the name and price of the second most expensive product.
+
+-- 5. Select the name and price of the least expensive product.
+
+-- 6. Select the names and prices of all products, ordered by price in descending order.
+
+-- 7. Select the average price of all products.
+
+-- 8. Select the sum of the price of all products.
+
+-- 9. Select the sum of the price of all products whose prices is less than £20.
+
+-- 10. Select the id of the user with the name "Alex Chin".
+
+-- 11. Select the names of all users whose names start with the letter "E".
+
+-- 12. Select the number of users whose first names are "Benjamin".
+
+-- 13. Select the number of users who want a "Teddy Bear".
+
+-- 14. Select the count of items on the wishlish of the user with your name.
+
+-- 15. Select the count and name of all products on the wishlist, ordered by count in descending order.
+
+-- 16. Select the count and name of all products that are not on sale on the wishlist, ordered by count in descending order.
+
+-- 17. Insert a user with the name "Jonathan Anderson" into the users table.
+
+-- 19. Insert a wishlist entry for the user with the name "Jonathan Anderson" for the product "The Ruby Programming Language" using sub queries to find the user id and the product id.
+
+--20. Update the name of the "Jonathan Anderson" user to be "Jon Anderson".
+
+-- 21. Delete the user with the name "Jon Anderson".
+
+-- 22. Delete the wishlist item for the user you just deleted.
