a4c3f4e74a12e2279f683213c02f3eee8a2a1f4b
edwardlea <leaedward87@gmail.com>
2019-02-20 16:59:47 +0000

    initial commit

---
+++ edwardlea/python-cakes-sql/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost:5432/flask-homework'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+class Cake(db.Model):
+
+    __tablename__ = 'cakes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    flavour = db.Column(db.String(40), nullable=False)
+    slices = db.Column(db.Integer, nullable=True)
+    type = db.Column(db.String(40), nullable=False)
+
+
+class CakeSchema(ma.ModelSchema):
+
+  class Meta:
+    model = Cake
+
+
+@app.route('/')
+def home():
+  return 'Hello Cakes!', 200
+
+
+@app.route('/cakes', methods=['GET'])
+def cakes_index():
+    cakes = Cake.query.all() #gets all donut
+    cakes_schema = CakeSchema(many=True)
+
+    return cakes_schema.jsonify(cakes), 200
+
+@app.route('/cakes/<int:id>', methods=['GET'])
+def cakes_show(id):
+    cake = Cake.query.get(id) #get a donut by id
+    cake_schema = CakeSchema()
+
+    return cake_schema.jsonify(cake), 200
