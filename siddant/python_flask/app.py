af8aacd5a55782b37894147cf6bba17964986b81
Siddant <siddantgurung@hotmail.co.uk>
2019-02-20 16:32:54 +0000

    database work

--- siddant/python_flask/app.py
+++ siddant/python_flask/app.py
@@ -4,6 +4,7 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__) #file name main
 
+app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://localhost:5432/flask-homework'
 
 @app.route('/')
 def home():
