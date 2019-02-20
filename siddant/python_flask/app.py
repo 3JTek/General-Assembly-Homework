f6156591cc87d57743a707ccd073b93c77fe575f
Siddant <siddantgurung@hotmail.co.uk>
2019-02-20 16:37:36 +0000

    model complte

--- siddant/python_flask/app.py
+++ siddant/python_flask/app.py
@@ -5,6 +5,17 @@ from flask_marshmallow import Marshmallow
 app = Flask(__name__) #file name main
 
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgres://localhost:5432/flask-homework'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+class Teams(db.Model):
+
+    __tablename__='teams'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), nullable= False)
+    location = db.Column(db.String(40), nullable=True) #not needed
+    established = db.Column(db.String(40), nullable=True)
+    league = db.Column(db.Integer,, default= False)
 
 @app.route('/')
 def home():
