f62f61f70156b5a2fe48752a4a20210cbf4a8ee3
Alessandro Lepri <alex.lepri84@gmail.com>
2019-02-20 18:54:22 +0000

    Added some more seeds and fixed typo on seeds.add

--- alexlepri/python-pizzas/seeds.py
+++ alexlepri/python-pizzas/seeds.py
@@ -7,19 +7,27 @@ with app.app_context():
 
 # ============ CREATING PIZZAS ==============
 
-    margherita = Pizza(name='Margherita', toppings='tomato sauce, mozzarella, oregano, basil', dough='Thin Crust', price=8)
+    margherita = Pizza(name='Margherita', toppings='Tomato Sauce, Mozzarella, Oregano, Basil', dough='Thin Crust', price=8)
 
-    super = Pizza(name='Bufala', toppings='tomato sauce, Bufala mozzarella, oregano, basil', dough='Thin Crust', price=11)
+    super = Pizza(name='Bufala', toppings='Tomato Sauce, Bufala Mozzarella, Oregano, Basil', dough='Neapolitan Crust', price=11)
 
-    wild = Pizza(name='Country', toppings='Tomato Sauce, Mozzarella, Mushroom, Pancetta, Egg, Sausage', dough='Thin Crust', price=12)
+    wild = Pizza(name='Country', toppings='Tomato Sauce, Mozzarella, Mushroom, Pancetta, Egg, Sausage', dough='Flatbread', price=12)
 
     slurp = Pizza(name='Pear and Gorgonzola', toppings='Tomato Sauce, Pear, Gorgonzola, Chilly, Oregano', dough='Thin Crust', price=11)
 
     cool = Pizza(name='Fiorentina', toppings='Tomato Sauce, Mozzarella, Spinach, Eggs, Parmesan', dough='Thin Crust', price=13)
 
-    alex = Pizza(name='Alex', toppings='Tomato Sauce, Mozzarella, Sausage, Onion', dough='Thin Crust', price=12)
+    alex = Pizza(name='Alex', toppings='Tomato Sauce, Mozzarella, Sausage, Onion', dough='Flatbread', price=12)
 
-    cheese = Pizza(name='Four Cheeses', toppings='Tomato Sauce, Mozzarella, Gorgonzola, Parmesan, Gouda', dough='Thin Crust', price=13)
+    cheese = Pizza(name='Four Cheeses', toppings='Tomato Sauce, Mozzarella, Gorgonzola, Parmesan, Gouda', dough='Neapolitan Crust', price=13)
+
+    napoli = Pizza(name='Napoli', toppings='Tomato Sauce, Mozzarella, Anchovies, Capers, Oregano', dough='Neapolitan Crust', price=10)
+
+    rich = Pizza(name='Deluxe', toppings='Tomato Sauce,Mascarpone Cheese, Prawns, Rocket, Chilli, Onion', dough='Flatbread', price=15)
+
+    potato = Pizza(name='Potato Pizza', toppings='Mozzarella, Pancetta, Potato, Onion, Rosemary', dough='Thin Crust', price=9)
+
+    
 
 
         # ADD THE DONUTS TO THE DATABASE SESSION
