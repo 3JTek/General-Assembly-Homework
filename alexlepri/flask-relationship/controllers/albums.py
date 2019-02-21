438e7b8be9d5d7dbc6373e9cc04082df82737d59
Alessandro Lepri <alex.lepri84@gmail.com>
2019-02-21 16:42:26 +0000

    Added Album and Photo show

--- alexlepri/flask-relationship/controllers/albums.py
+++ alexlepri/flask-relationship/controllers/albums.py
@@ -3,6 +3,7 @@ from models.photo import Photo, PhotoSchema
 from models.album import Album, AlbumSchema
 
 albums_schema = AlbumSchema(many=True)
+album_schema = AlbumSchema()
 
 api = Blueprint('albums', __name__)
 
@@ -10,3 +11,9 @@ api = Blueprint('albums', __name__)
 def  index():
     albums = Album.query.all()
     return albums_schema.jsonify(albums)
+
+
+@api.route('/albums/<int:album_id>', methods=['GET'])
+def  show(album_id):
+    album = Album.query.get(album_id)
+    return album_schema.jsonify(album)
--- alexlepri/flask-relationship/controllers/photos.py
+++ alexlepri/flask-relationship/controllers/photos.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from models.photo import Photo, PhotoSchema
 
 photos_schema = PhotoSchema(many=True)
+photo_schema = PhotoSchema()
 
 api = Blueprint('photos', __name__)
 
@@ -9,3 +10,9 @@ api = Blueprint('photos', __name__)
 def  index():
     photos = Photo.query.all()
     return photos_schema.jsonify(photos)
+
+
+@api.route('/photos/<int:photo_id>', methods=['GET'])
+def  show(photo_id):
+    photo = Photo.query.get(photo_id)
+    return photo_schema.jsonify(photo)
