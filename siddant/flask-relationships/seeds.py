41b81c0595e552c4d756b7b583399b982243ca4d
Siddant <siddantgurung@hotmail.co.uk>
2019-02-21 15:05:46 +0000

    seeds file done

---
+++ siddant/flask-relationships/seeds.py
@@ -0,0 +1,13 @@
+from app import app, db
+from models.author import Author
+from models.book import Book
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    tolkien = User(author_first_name='J. R. R.',author_last_name='Tolkien', nationality='English', age=81)
+    tolkien.save()
+
+    lord_of_the_ring = Video(title="The Lord of the Rings", rating=5, author=tolkien, published_year=1954)
+    lord_of_the_ring.save()
