87c18ce489e08ae1163fa85cf13f36e2ec5057d4
Dexter De Leon <dexdeleon@gmail.com>
2019-02-22 09:30:13 +0000

    Fixed id variable on user controller.

--- DexDeLeon/sql-relationships/controllers/users.py
+++ DexDeLeon/sql-relationships/controllers/users.py
@@ -11,7 +11,7 @@ def index():
     users = User.query.all()
     return users_schema.jsonify(users)
 
-@api.route('/users/<int:id>', methods=['GET'])
+@api.route('/users/<int:user_id>', methods=['GET'])
 def show(user_id):
     user = User.query.get(user_id)
     return user_schema.jsonify(user)
